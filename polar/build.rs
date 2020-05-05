2abd607016a6aed4a3945411dc4ecb52b6d791cc
David Hatch <david@osohq.com>
2020-05-05 09:57:02 -0400

    Fix build swallowing errors.

--- polar/build.rs
+++ polar/build.rs
@@ -5,12 +5,21 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    // cbindgen::Builder::new()
-    //     .with_crate(crate_dir)
-    //     .with_language(cbindgen::Language::C)
-    //     .with_no_includes()
-    //     .with_item_prefix("polar_")
-    //     .generate()
-    //     .expect("Unable to generate bindings")
-    //     .write_to_file("polar.h");
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_language(cbindgen::Language::C)
+        .with_no_includes()
+        .with_item_prefix("polar_")
+        .generate()
+        .map(|res| Some(res))
+        .or_else(|err| {
+            // Continue on syntax errors
+            if let cbindgen::Error::ParseSyntaxError { .. } = err {
+                Ok(None)
+            } else {
+                Err(err)
+            }
+        })
+        .expect("Unable to generate bindings")
+        .and_then(|res| Some(res.write_to_file("polar.h")));
 }
