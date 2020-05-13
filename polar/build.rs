797a1bbdabbfd1919ad8ef8184fba2747429c769
David Hatch <david@osohq.com>
2020-05-13 08:47:38 -0400

    Fix RLS build with cargo metadata errors.

--- polar/build.rs
+++ polar/build.rs
@@ -19,6 +19,8 @@ fn main() {
             // Continue on syntax errors
             if let cbindgen::Error::ParseSyntaxError { .. } = err {
                 Ok(None)
+            } else if let cbindgen::Error::CargoMetadata { .. } = err {
+                Ok(None)
             } else {
                 Err(err)
             }
