4f82b4c3f429176754c0f9c81574f6a69dcb906d
steve olsen <dev@steve.computer>
2020-05-13 17:19:56 -0500

    wip, start on rewrites

--- polar/src/rewrites.rs
+++ polar/src/rewrites.rs
@@ -11,8 +11,12 @@ use super::types::*;
 // => foo(?temp) := .(a,b,?temp), hi;
 
 // for queries
-// foo(a.b) => .(a,b,x), foo(?x)
+// foo(a.b) => .(a,b,?x), foo(?x)
+// foo(a.b(i)) => .(a,b(i),?x), foo(?x)
 //
+
+// a.b(c,d,e) => .(a,b(c,d,e))
+// a,b(c,d,e)
 
 // the two cases
 // rewrite the head of a rule,
@@ -20,13 +24,71 @@ use super::types::*;
 //}
 
 pub fn rewrite_term(term: Term) -> Term {
-    term
+    // walk the term
+    // if you hit a . expression
+    //     generate a new symbol
+    //     replace it with the symbol
+    //     push a 3 dot expression into the nearest enclosing and
+    //     or if there's an or or not, we add a new and of it and the origional arg.
+    let mut i = 0;
+    fn gen_var(i: &mut i32) -> Term {
+        let t = Term {
+            id: 0,
+            offset: 0,
+            value: Value::Symbol(Symbol(format!("_value_{}", i))),
+        };
+        *i += 1;
+        t
+    }
+
+    let mut enclosing_term: Option<&mut Operation> = None;
+    let enclosing_arg: usize = 0;
+
+    let rewrite_term_helper = move |value: &Value| -> Value {
+        match value {
+            Value::Expression(Operation {
+                operator: Operator::Dot,
+                mut args,
+            }) => {
+                let symbol = gen_var(&mut i);
+                args.push(symbol.clone());
+                let lookup = Term {
+                    value: Value::Expression(Operation {
+                        operator: Operator::Dot,
+                        args,
+                    }),
+                    id: 0,
+                    offset: 0,
+                };
+
+                if let Some(enclosing_term) = &mut enclosing_term {
+                    let and_wrap = Term {
+                        value: Value::Expression(Operation {
+                            operator: Operator::And,
+                            args: vec![lookup, enclosing_term.args[enclosing_arg].clone()],
+                        }),
+                        id: 0,
+                        offset: 0,
+                    };
+                    enclosing_term.args[enclosing_arg] = and_wrap;
+                    symbol.value
+                } else {
+                    Value::Expression(Operation {
+                        operator: Operator::And,
+                        args: vec![lookup, symbol],
+                    })
+                }
+            }
+            _ => value.clone(),
+        }
+    };
+
+    term.map(&mut rewrite_term_helper)
 }
 
 pub fn rewrite_rule(rule: Rule) -> Rule {
-    //let Rule { name, params, body } = rule;
-
-    //let body = rewrite_term(body);
-    //Rule { name, params, body }
-    rule
+    let Rule { name, params, body } = rule;
+    let body = rewrite_term(body);
+    // rewrite params
+    Rule { name, params, body }
 }
