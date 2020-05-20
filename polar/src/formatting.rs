9146e043a1c4d261f615f7d43606b4afb0aee3e0
Sam Scott <sam@osohq.com>
2020-05-20 10:07:08 -0400

    Update comments.

--- polar/src/formatting.rs
+++ polar/src/formatting.rs
@@ -52,7 +52,8 @@ pub mod display {
 pub mod to_polar {
     use crate::types::*;
 
-    /// Helper method: uses the operator precedence to
+    /// Helper method: uses the operator precedence to determine if `t`
+    /// has a lower precendence that `op`.
     fn has_lower_pred(op: Operator, t: &Term) -> bool {
         match t.value {
             Value::Expression(Operation {
@@ -128,7 +129,7 @@ pub mod to_polar {
     impl ToPolarString for Operation {
         fn to_polar(&self) -> String {
             use Operator::*;
-            // Adds parenthesis when sub expressions have lower precidence (which is what you would have had to have during inital parse)
+            // Adds parentheses when sub expressions have lower precidence (which is what you would have had to have during inital parse)
             // Lets us spit out strings that would reparse to the same ast.
             match self.operator {
                 // `Make` formats as a predicate
