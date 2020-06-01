5722ec6f63cfcd470e7742619853935502eaf18c
David Hatch <david@osohq.com>
2020-06-01 13:29:07 -0400

    fmt

--- languages/python/docs/examples/user_types/01-user_classes.py
+++ languages/python/docs/examples/user_types/01-user_classes.py
@@ -1,25 +1,27 @@
 # classes-start
 from oso import polar_class
 
+
 @polar_class
 class Customer:
     def __init__(self, id):
         self.id = id
 
+
 @polar_class
 class InternalUser:
     def __init__(self, id):
         self.id = id
+
+
 # classes-end
 
 # app-start
-def customer_dashboard_handler(request, ...):
+def customer_dashboard_handler(request):
     oso = get_oso()
     actor = user_from_id(request.id)
-    allowed = oso.allow(
-        actor=actor,
-        action="view",
-        resource="customer_dashboard")
+    allowed = oso.allow(actor=actor, action="view", resource="customer_dashboard")
+
 
 def user_from_id(id):
     user_type = db.query("SELECT type FROM users WHERE id = ?", request.id)
@@ -27,4 +29,6 @@ def user_from_id(id):
         return InternalUser(request.id)
     elif user_type == "customer":
         return CustomerUser(request.id)
+
+
 # app-end
