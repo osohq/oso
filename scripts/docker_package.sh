d68ce61c01b84b6656add3d9ea1ab6f986dc9009
steve olsen <dev@steve.computer>
2020-06-03 14:21:51 -0500

    directorz

--- scripts/docker_package.sh
+++ scripts/docker_package.sh
@@ -2,6 +2,8 @@
 rustup target add x86_64-unknown-linux-musl
 cargo build --target x86_64-unknown-linux-musl --release
 
+cd languages/python
+
 docker pull quay.io/pypa/manylinux1_x86_64
 mkdir -p native
 cp ../../target/x86_64-unknown-linux-musl/release/libpolar.a native/
