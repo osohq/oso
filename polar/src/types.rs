bbe3315403605dd90510f53d1928a1dea42436bd
Leina McDermott <leina05@gmail.com>
2020-06-03 15:28:57 -0400

    Dot Op Refactor (#88)
    * moved dict-specific lookup logic into dict-specific lookup fn
    
    dict lookups with unbound variable fields working
    
    rewriting dot lookups in specializers,
    
    Update polar/src/types.rs
    
    Co-authored-by: Sam Scott <sam@osohq.com>
    
    Simplify rewriting logic. (#92)
    
    * Simplify rewriting logic.
    
    * Rework for rebased.
    
    * fix nested lookups rewrites
    
    * add docs-test target to makefile

--- polar/src/types.rs
+++ polar/src/types.rs
@@ -4,6 +4,7 @@
 
 use serde::{Deserialize, Serialize};
 
+use crate::ToPolarString;
 use std::collections::{BTreeMap, HashMap};
 use std::fmt;
 use std::hash::{Hash, Hasher};
@@ -108,6 +109,14 @@ impl Dictionary {
         }
     }
 
+    /// Apply `f` to value and return a new term.
+    pub fn map_in_place<F>(&mut self, f: &mut F)
+    where
+        F: FnMut(&mut Value),
+    {
+        self.fields.iter_mut().for_each(|(_, v)| f(&mut v.value));
+    }
+
     pub fn is_empty(&self) -> bool {
         self.fields.is_empty()
     }
@@ -171,6 +180,14 @@ impl Predicate {
             name: self.name.clone(),
             args: self.args.iter().map(|term| term.map(f)).collect(),
         }
+    }
+
+    /// Apply `f` to value and return a new term.
+    pub fn map_in_place<F>(&mut self, f: &mut F)
+    where
+        F: FnMut(&mut Value),
+    {
+        self.args.iter_mut().for_each(|a| f(&mut a.value));
     }
 }
 
@@ -267,10 +284,34 @@ impl Value {
         f(&mapped)
     }
 
-    pub fn symbol(self) -> Result<Symbol, Self> {
+    pub fn map_in_place<F>(&mut self, f: &mut F)
+    where
+        F: FnMut(&mut Value),
+    {
+        f(self);
+        // the match does the recursive calling of map
+        match self {
+            Value::Integer(_) | Value::String(_) | Value::Boolean(_) | Value::Symbol(_) => {}
+            Value::List(terms) => {
+                terms.iter_mut().for_each(|term| f(&mut term.value));
+            }
+            Value::Call(predicate) => predicate.map_in_place(f),
+            Value::Expression(Operation { args, .. }) => {
+                args.iter_mut().for_each(|term| term.map_in_place(f));
+                // args.iter_mut().for_each(|term| f(&mut term.value));
+            }
+            Value::InstanceLiteral(InstanceLiteral { fields, .. }) => fields.map_in_place(f),
+            Value::ExternalInstance(_) => {}
+            Value::Dictionary(dict) => dict.map_in_place(f),
+        };
+    }
+
+    pub fn symbol(self) -> Result<Symbol, RuntimeError> {
         match self {
             Value::Symbol(name) => Ok(name),
-            _ => Err(self),
+            _ => Err(RuntimeError::TypeError {
+                msg: format!("Expected symbol, got: {}", self.to_polar()),
+            }),
         }
     }
 }
@@ -314,6 +355,14 @@ impl Term {
             value: self.value.map(f),
         }
     }
+
+    /// Apply `f` to value and return a new term.
+    pub fn map_in_place<F>(&mut self, f: &mut F)
+    where
+        F: FnMut(&mut Value),
+    {
+        self.value.map_in_place(f);
+    }
 }
 
 pub fn unwrap_and(term: Term) -> TermList {
@@ -352,6 +401,13 @@ impl Parameter {
             specializer: self.specializer.clone().map(|t| t.map(f)),
         }
     }
+
+    pub fn map_in_place<F>(&mut self, f: &mut F)
+    where
+        F: FnMut(&mut Value),
+    {
+        self.specializer.iter_mut().for_each(|a| f(&mut a.value));
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
@@ -371,6 +427,17 @@ impl Rule {
             params: self.params.iter().map(|param| param.map(f)).collect(),
             body: self.body.map(f),
         }
+    }
+
+    /// Apply `f` to value and return a new term.
+    pub fn map_in_place<F>(&mut self, f: &mut F)
+    where
+        F: FnMut(&mut Value),
+    {
+        self.params
+            .iter_mut()
+            .for_each(|param| param.map_in_place(f));
+        self.body.map_in_place(f);
     }
 }
 
