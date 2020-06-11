1f421b91c4506b7c528a63db8e8e2a04092e527a
Gabe Jackson <gj@mail.co.de>
2020-06-11 19:27:52 -0400

    Generators!

--- languages/ruby/polar/spec/osohq/polar_spec.rb
+++ languages/ruby/polar/spec/osohq/polar_spec.rb
@@ -1,9 +1,23 @@
-RSpec.describe Osohq::Oso do
-  it "has a version number" do
-    expect(Osohq::Oso::VERSION).not_to be nil
+# frozen_string_literal: true
+
+RSpec.describe Osohq::Polar do
+  it 'has a version number' do
+    expect(Osohq::Polar::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  it 'does something useful' do
     expect(false).to eq(true)
+  end
+
+  it 'works' do
+    p = Osohq::Polar::Polar.new
+    p.load_str('f(1);')
+    p.query_str('f(x)')
+
+    results = list(p._query_str('f(x)'))
+    assert results[0]['x'] == 1
+    results = list(p._query_str('f(y)'))
+    assert results[0]['y'] == 1
+    del p
   end
 end
