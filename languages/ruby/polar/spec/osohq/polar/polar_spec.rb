ab7d6086700e4839bf87200371193d9d70c22422
Gabe Jackson <gj@mail.co.de>
2020-06-12 12:07:10 -0400

    Fix Term.to_ruby bug + add second test case

--- languages/ruby/polar/spec/osohq/polar/polar_spec.rb
+++ languages/ruby/polar/spec/osohq/polar/polar_spec.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
+require_relative './helpers'
+
+RSpec.configure do |c|
+  c.include Helpers
+end
+
 RSpec.describe Osohq::Polar::Polar do
-  let(:polar) { subject }
-
   it 'works' do
-    polar.load_str('f(1);')
-    results = polar.query_str('f(x)')
-    expect(results.next.transform_values(&:to_ruby)).to eq({ 'x' => 1 })
+    subject.load_str('f(1);')
+    results = subject.query_str('f(x)')
+    expect(next_result(results)).to eq({ 'x' => 1 })
     expect { results.next }.to raise_error StopIteration
+  end
 
-    results = polar.query_str('f(y)')
-    expect(results.next.transform_values(&:to_ruby)).to eq({ 'y' => 1 })
-    expect { results.next }.to raise_error StopIteration
+  it 'converts Polar values into Ruby values' do
+    subject.load_str('f({x: [1, "two", true], y: {z: false}});')
+    expect(qvar(subject, 'f(x)', 'x')).to eq({ 'x' => [1, 'two', true], 'y' => { 'z' => false } })
   end
 end
