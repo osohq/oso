13488239805f6764c3622805cc4a14ffd75f7b23
Leina McDermott <leina05@gmail.com>
2020-06-12 14:54:48 -0400

    update test helpers for new to_ruby semantics

--- languages/ruby/polar/spec/osohq/polar/helpers.rb
+++ languages/ruby/polar/spec/osohq/polar/helpers.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 module Helpers
-  def next_result(results)
-    results.next.transform_values(&:to_ruby)
-  end
-
   def qvar(polar, query, var)
-    next_result(polar.query_str(query))[var]
+    results = polar.query_str(query)
+    results.next[var]
   end
 end
