a651152b557fa2f8c9116f6ea685345aa623bf51
Sam Scott <sam@osohq.com>
2020-06-17 16:23:39 -0400

    Try `cibuildwheel` python package for cross-OS builds (#118)
    * Try `cibuildwheel` python package for cross-OS builds
    
    * Ready to go.

--- languages/python/package.sh
+++ languages/python/package.sh
@@ -2,13 +2,13 @@
 cd io
 
 rm -rf build
-ENV=LINUX /opt/python/cp36-cp36m/bin/python setup.py build
-ENV=LINUX /opt/python/cp36-cp36m/bin/python setup.py sdist bdist_wheel
+ENV=CI /opt/python/cp36-cp36m/bin/python setup.py build
+ENV=CI /opt/python/cp36-cp36m/bin/python setup.py sdist bdist_wheel
 
 rm -rf build
-ENV=LINUX /opt/python/cp37-cp37m/bin/python setup.py build
-ENV=LINUX /opt/python/cp37-cp37m/bin/python setup.py sdist bdist_wheel
+ENV=CI /opt/python/cp37-cp37m/bin/python setup.py build
+ENV=CI /opt/python/cp37-cp37m/bin/python setup.py sdist bdist_wheel
 
 rm -rf build
-ENV=LINUX /opt/python/cp38-cp38/bin/python setup.py build
-ENV=LINUX /opt/python/cp38-cp38/bin/python setup.py sdist bdist_wheel
+ENV=CI /opt/python/cp38-cp38/bin/python setup.py build
+ENV=CI /opt/python/cp38-cp38/bin/python setup.py sdist bdist_wheel
