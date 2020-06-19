94e215126ffb33e778263c4a3a78e9a4928dd8c9
Gabe Jackson <gj@mail.co.de>
2020-06-19 13:45:54 -0400

    Support Ruby 2.4, 2.5, 2.6 (#141)

--- languages/ruby/polar/spec/osohq/polar/polar_spec.rb
+++ languages/ruby/polar/spec/osohq/polar/polar_spec.rb
@@ -118,6 +118,68 @@ RSpec.describe Osohq::Polar::Polar do
     end
   end
 
+  context '#make_instance' do
+    context 'when using the default constructor' do
+      it 'handles keyword args' do
+        stub_const('Foo', Class.new do
+          attr_reader :bar, :baz
+          def initialize(bar:, baz:)
+            @bar = bar
+            @baz = baz
+          end
+        end)
+        subject.register_class(Foo)
+        one = subject.to_polar_term(1)
+        two = subject.to_polar_term(2)
+        id = subject.make_instance('Foo', fields: { 'bar' => one, 'baz' => two }, id: 1)
+        instance = subject.get_instance(id)
+        expect(instance.class).to eq(Foo)
+        expect(instance.bar).to eq(1)
+        expect(instance.baz).to eq(2)
+      end
+
+      it 'handles no args' do
+        stub_const('Foo', Class.new do
+          def initialize; end
+        end)
+        subject.register_class(Foo)
+        id = subject.make_instance('Foo', fields: {}, id: 1)
+        instance = subject.get_instance(id)
+        expect(instance.class).to eq(Foo)
+      end
+    end
+
+    context 'when using a custom constructor' do
+      it 'handles keyword args' do
+        stub_const('Foo', Class.new do
+          attr_reader :bar, :baz
+          def initialize(bar:, baz:)
+            @bar = bar
+            @baz = baz
+          end
+        end)
+        subject.register_class(Foo) { |**args| Foo.new(**args) }
+        one = subject.to_polar_term(1)
+        two = subject.to_polar_term(2)
+        id = subject.make_instance('Foo', fields: { 'bar' => one, 'baz' => two }, id: 1)
+        instance = subject.get_instance(id)
+        expect(instance.class).to eq(Foo)
+        expect(instance.bar).to eq(1)
+        expect(instance.baz).to eq(2)
+      end
+
+      it 'handles no args' do
+        stub_const('Foo', Class.new do
+          def initialize; end
+        end)
+        subject.register_class(Foo) { Foo.new }
+        id = subject.make_instance('Foo', fields: {}, id: 1)
+        instance = subject.get_instance(id)
+        expect(instance.class).to eq(Foo)
+      end
+    end
+  end
+
   context '#register_class' do
     it 'registers a Ruby class with Polar' do
       pending 'Instance literal parsing updates'
