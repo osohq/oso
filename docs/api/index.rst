404c4b7fea94ecf332dd3b3856b01d76ea1518d3
David Hatch <david@osohq.com>
2020-06-30 14:17:17 -0400

    Ruby doc link in docs.

--- docs/api/index.rst
+++ docs/api/index.rst
@@ -7,3 +7,7 @@ API Documentation
    :caption: Contents:
 
    oso
+
+   # The html_extra_paths conf directive copies files into this directory.
+   # The ruby directory in the /docs folder exists only to generate this link.
+   Ruby library documentation </ruby/index>
--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,9 @@ pygments_style = "sphinx"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+#html_static_path = ["_static"]
+
+html_extra_path = ["_api_docs"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
