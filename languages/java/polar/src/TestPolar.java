108c468c67d951d6d0274ff99a2531a67b5c6b45
Leina McDermott <leina05@gmail.com>
2020-07-01 18:26:42 -0400

    updated tests

--- languages/java/polar/src/TestPolar.java
+++ languages/java/polar/src/TestPolar.java
@@ -15,135 +15,186 @@ public class TestPolar {
         }
     }
 
-    public static void testToJava() {
+    public static void testLoadAndQueryStr() {
+        String name = "testLoadAndQueryStr";
         Boolean passed = true;
-        List<String> failures = new ArrayList<String>();
-        Polar p = new Polar();
-        // Test boolean
-        p.load_str("a(x) := x = true;");
-        HashMap<String, Object> a = p.query_str("a(x)").nextElement();
-        if (!a.equals(Map.of("x", true))) {
-            passed = false;
-            failures.add("Failed to convert boolean to java.");
-        }
-        // Test dictionary
-        p.load_str("b(x) := x = {a: 1};");
-        HashMap<String, Object> b = p.query_str("b(x)").nextElement();
-        if (!b.equals(Map.of("x", Map.of("a", 1)))) {
-            passed = false;
-            failures.add("Failed to convert dictionary to java.");
-        }
-        // Test list
-        p.load_str("c(x) := x = [\"a\", \"b\", \"c\"];");
-        HashMap<String, Object> c = p.query_str("c(x)").nextElement();
-        if (!c.equals(Map.of("x", List.of("a", "b", "c")))) {
-            passed = false;
-            failures.add("Failed to convert list to java.");
-        }
+        String msg = null;
+        try {
+            Polar p = new Polar();
 
-        printResults(passed, failures, "testToJava");
+            p.load_str("f(1);");
+            Enumeration<HashMap<String, Object>> results = p.query_str("f(x)");
+            if (!results.hasMoreElements() || results.nextElement().get("x") != Integer.valueOf(1)) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            passed = false;
+            msg = e.toString();
+        } finally {
+            printResults(passed, msg, name);
+        }
     }
 
-    public static void testToPolarTerm() {
+    public static void testInlineQueries() {
+        String name = "testInlineQueries";
         Boolean passed = true;
-        List<String> failures = new ArrayList<String>();
-        Polar p = new Polar();
+        String msg = null;
+        try {
+            Polar p = new Polar();
+            p.load_str("f(1); ?= f(1);");
+            try {
+                p.load_str("?= f(2);");
+            } catch (Error e) {
+                return;
+            }
+            throw new Exception("Expected inline query to fail but it didn't.");
 
-        // Test Boolean
-        Boolean b = true;
-        JSONObject polar = p.toPolarTerm(b);
-        Object java = p.toJava(polar);
-        if (java.getClass() != Boolean.class || java != b) {
+        } catch (Exception e) {
             passed = false;
-            failures.add("Failed to convert Boolean to Polar.");
+            msg = e.toString();
+        } finally {
+            printResults(passed, msg, name);
         }
-        // Test Int
-        int i = 3;
-        polar = p.toPolarTerm(i);
-        java = p.toJava(polar);
-        if (java.getClass() != Integer.class || (Integer) java != i) {
-            passed = false;
-            failures.add("Failed to convert Integer to Polar.");
-        }
-        // Test Float
-        float f = (float) 3.50;
-        polar = p.toPolarTerm(f);
-        java = p.toJava(polar);
-        if (java.getClass() != Float.class || (Float) java != f) {
-            passed = false;
-            failures.add("Failed to convert Float to Polar.");
-        }
-        // Test String
-        String s = "oso!";
-        polar = p.toPolarTerm(s);
-        java = p.toJava(polar);
-        if (java.getClass() != String.class || (String) java != s) {
-            passed = false;
-            failures.add("Failed to convert String to Polar.");
-        }
-        // Test List
-        List<Integer> l = List.of(1, 2, 3, 4);
-        polar = p.toPolarTerm(l);
-        java = p.toJava(polar);
-        if (!(java instanceof List) || !((List<Object>) java).equals(l)) {
-            passed = false;
-            failures.add("Failed to convert List to Polar.");
-        }
-        // Test Dict
-        Map<String, Integer> m = Map.of("a", 1, "b", 2);
-        polar = p.toPolarTerm(m);
-        java = p.toJava(polar);
-        if (!(java instanceof Map) || !((Map<String, Object>) java).equals(m)) {
-            passed = false;
-            failures.add("Failed to convert Map to Polar.");
-        }
-        // Test ExternalInstance
-        MyClass instance = new MyClass("test");
-        polar = p.toPolarTerm(instance);
-        java = p.toJava(polar);
-        if (java.getClass() != MyClass.class || !((MyClass) java).equals(instance)) {
-            passed = false;
-            failures.add("Failed to convert ExternalInstance to Polar.");
-        }
+    }
 
-        printResults(passed, failures, "testToPolarTerm");
+    public static void testToJava() {
+        String name = "testToJava";
+        Boolean passed = true;
+        String msg = null;
+        try {
+            Polar p = new Polar();
 
+            // Test boolean
+            p.load_str("a(x) := x = true;");
+            HashMap<String, Object> a = p.query_str("a(x)").nextElement();
+            if (!a.equals(Map.of("x", true))) {
+                throw new Exception("Failed to convert boolean to java.");
+            }
+            // Test dictionary
+            p.load_str("b(x) := x = {a: 1};");
+            HashMap<String, Object> b = p.query_str("b(x)").nextElement();
+            if (!b.equals(Map.of("x", Map.of("a", 1)))) {
+                throw new Exception("Failed to convert dictionary to java.");
+            }
+            // Test list
+            p.load_str("c(x) := x = [\"a\", \"b\", \"c\"];");
+            HashMap<String, Object> c = p.query_str("c(x)").nextElement();
+            if (!c.equals(Map.of("x", List.of("a", "b", "c")))) {
+                throw new Exception("Failed to convert list to java.");
+            }
+        } catch (Exception e) {
+            passed = false;
+            msg = e.toString();
+        } finally {
+            printResults(passed, msg, name);
+
+        }
+    }
+
+    public static void testFFIRoundTrip() {
+        String name = "testFFIRoundTrip";
+        Boolean passed = true;
+        String msg = null;
+        try {
+            Polar p = new Polar();
+
+            // Test Boolean
+            Boolean b = true;
+            JSONObject polar = p.toPolarTerm(b);
+            Object java = p.toJava(polar);
+            if (java.getClass() != Boolean.class || java != b) {
+                throw new Exception("Failed to convert Boolean to Polar");
+            }
+            // Test Int
+            int i = 3;
+            polar = p.toPolarTerm(i);
+            java = p.toJava(polar);
+            if (java.getClass() != Integer.class || (Integer) java != i) {
+                throw new Exception("Failed to convert Integer to Polar");
+            }
+            // Test Float
+            float f = (float) 3.50;
+            polar = p.toPolarTerm(f);
+            java = p.toJava(polar);
+            if (java.getClass() != Float.class || (Float) java != f) {
+                throw new Exception("Failed to convert Float to Polar");
+            }
+            // Test String
+            String s = "oso!";
+            polar = p.toPolarTerm(s);
+            java = p.toJava(polar);
+            if (java.getClass() != String.class || (String) java != s) {
+                throw new Exception("Failed to convert String to Polar");
+            }
+            // Test List
+            List<Integer> l = List.of(1, 2, 3, 4);
+            polar = p.toPolarTerm(l);
+            java = p.toJava(polar);
+            if (!(java instanceof List) || !((List<Object>) java).equals(l)) {
+                throw new Exception("Failed to convert List to Polar");
+            }
+            // Test Dict
+            Map<String, Integer> m = Map.of("a", 1, "b", 2);
+            polar = p.toPolarTerm(m);
+            java = p.toJava(polar);
+            if (!(java instanceof Map) || !((Map<String, Object>) java).equals(m)) {
+                throw new Exception("Failed to convert Map to Polar");
+            }
+            // Test ExternalInstance
+            MyClass instance = new MyClass("test");
+            polar = p.toPolarTerm(instance);
+            java = p.toJava(polar);
+            if (java.getClass() != MyClass.class || !((MyClass) java).equals(instance)) {
+                throw new Exception("Failed to convert Java Object to Polar");
+            }
+        } catch (Exception e) {
+            passed = false;
+            msg = e.toString();
+        } finally {
+            printResults(passed, msg, name);
+
+        }
     }
 
     public static void testRegisterAndMakeClass() {
-        Polar p = new Polar();
+        String name = "testRegisterAndMakeClass";
         Boolean passed = true;
-        ArrayList<String> failures = new ArrayList<String>();
-        p.registerClass(MyClass.class, m -> new MyClass((String) m.get("name")));
+        String msg = null;
+        try {
+            Polar p = new Polar();
+            p.registerClass(MyClass.class, m -> new MyClass((String) m.get("name")));
 
-        Map<String, String> testArg = Map.of("name", "testName");
-        MyClass instance = (MyClass) p.makeInstance(MyClass.class, testArg, Long.valueOf(0));
-        if (instance.name != "testName") {
+            Map<String, String> testArg = Map.of("name", "testName");
+            MyClass instance = (MyClass) p.makeInstance(MyClass.class, testArg, Long.valueOf(0));
+            if (instance.name != "testName") {
+                throw new Exception();
+            }
+        } catch (Exception e) {
             passed = false;
+            msg = e.toString();
+        } finally {
+            printResults(passed, msg, name);
+
         }
-
-        printResults(passed, failures, "testRegisterAndMakeClass");
-
     }
 
-    private static void printResults(Boolean passed, List<String> failures, String name) {
-        if (!passed) {
-            System.out.println(name + ANSI_RED + " FAILED:" + ANSI_YELLOW);
-            for (String e : failures) {
-                System.out.println("\t" + e);
-            }
-            System.out.print(ANSI_RESET);
-        } else {
+    private static void printResults(Boolean passed, String message, String name) {
+        if (passed) {
             System.out.println(name + ANSI_GREEN + " PASSED." + ANSI_RESET);
+        } else {
+            System.out.println(name + ANSI_RED + " FAILED:" + ANSI_YELLOW);
+            if (message != null)
+                System.out.println("\t" + message + ANSI_RESET);
         }
-
     }
 
     public static void main(String[] args) {
         System.out.println("\nRunning tests...\n");
         testToJava();
-        testToPolarTerm();
+        testFFIRoundTrip();
         testRegisterAndMakeClass();
+        testLoadAndQueryStr();
+        testInlineQueries();
     }
 
 }
