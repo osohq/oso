df69fc5053058961efcf1f54412f343bd6f1c94b
Leina McDermott <leina05@gmail.com>
2020-07-15 10:00:44 -0400

    Packaged up Java (Maven) (#190)
    * WIP set-up java
    
    * query_str working
    
    * clean up enumeration interface
    
    * to_java
    
    * add finalize method to Polar class
    
    * rename to_java and add support for Floats
    
    * toPolarTerm
    
    * tests
    
    * repl
    
    * WIP register class
    
    * class registration with lambda WIP
    
    * instance caching & creation, + fun colors
    
    * inline queries
    
    * updated tests
    
    * clean up main from Polar
    
    * check inline queries in function
    
    * print exception stack trace in tests
    
    * load_str -> loadStr
    
    * loadFile WIP
    
    * camel case
    
    * set the queries free
    
    * more camel casing
    
    * loadFile + loadQueuedFiles
    
    * clean up Query class + some camelcasing
    
    * javadoc
    
    * more camel casing
    
    * clean up Query interface a bit
    
    * javadoc
    
    * oops forgot to clear the loadQueue
    
    * load queued files before querying
    
    * test loadFile
    
    * finish loadFile tests + clear
    
    * spiff up tests
    
    * prep for rebase
    
    * makeExternal query event handler + test
    
    * add polar_call_result to FFI class
    
    * external call
    
    * Predicate class + FFI round trip
    
    * queryPred + some test cleanup
    
    * query for a variable
    
    * types -> argTypes
    
    * externalisa + externalissubspecializer
    
    * exceptions
    
    * better memory management for polar + query ptrs
    
    * split out query class
    
    * split out predicate + variable classes
    
    * go back to using finalize for memory mgmt
    
    * some cleanup + reorder args for consistency
    
    * map args to argTypes for brevity
    
    * reorganize methods + edit method visibility
    
    * reorganized + renamed Ffi to match Ruby
    
    * fix bug with operational error parsing
    
    * add debug handling + debugger test
    
    * lil bug fix in the debugger
    
    * update rust lib
    
    * small cleanup
    
    * clippy + update java rust library
    
    * oso class
    
    * change all methods in Polar to be protected
    
    * Oso, Http, and PathMapper
    
    * cleanup
    
    * a few more tests
    
    * start of maven project
    
    * successfully builds
    
    * start of maven test file
    
    * makefiles
    
    * switch tests to use junit
    
    * goodbye old tests
    
    * fix small exceptions bug
    
    * add rest of polar tests
    
    * add oso tests
    
    * wrap Error and QueryEvent pointers
    
    * update visibility for Ffi wrapper classes
    
    * small rename
    
    * clean up subspecializer logic
    
    * Update languages/java/oso/src/test/java/com/osohq/oso/PolarTest.java
    
    Co-authored-by: David Hatch <david@osohq.com>
    
    * update syntax
    
    * include mvn clean in makefile
    
    * bug fix for query_free
    
    * bug fix for Symbol -> Variable
    
    * fix outstanding conflicts from merge
    
    * add one test for builtin strings
    
    * add register_constant
    
    * refactored the tests
    
    * fix class methods
    
    * rename ffi -> polarFfi and queryFfi
    
    * make Ffi singleton
    
    Co-authored-by: David Hatch <david@osohq.com>

---
+++ languages/java/oso/src/main/java/com/osohq/oso/Http.java
@@ -0,0 +1,13 @@
+package com.osohq.oso;
+
+public class Http {
+    public String hostname, path, query;
+
+    public Http(String hostname, String path, String query) {
+        this.hostname = hostname;
+        this.path = path;
+        this.query = query;
+
+    }
+
+}
