f0e65993b98241c6e378f427fdb711df2b8b94aa
Leina McDermott <leina05@gmail.com>
2020-07-15 10:00:44 -0400

    Packaged up Java (Maven) (#190)
    * WIP set-up java
    
    * query_str working
    
    * clean up enumeration interface
    
    * to_java
    
    * add finalize method to Polar class
    
    * rename to_java and add support for Floats
    
    * toPolarTerm
    
    * tests
    
    * repl
    
    * WIP register class
    
    * class registration with lambda WIP
    
    * instance caching & creation, + fun colors
    
    * inline queries
    
    * updated tests
    
    * clean up main from Polar
    
    * check inline queries in function
    
    * print exception stack trace in tests
    
    * load_str -> loadStr
    
    * loadFile WIP
    
    * camel case
    
    * set the queries free
    
    * more camel casing
    
    * loadFile + loadQueuedFiles
    
    * clean up Query class + some camelcasing
    
    * javadoc
    
    * more camel casing
    
    * clean up Query interface a bit
    
    * javadoc
    
    * oops forgot to clear the loadQueue
    
    * load queued files before querying
    
    * test loadFile
    
    * finish loadFile tests + clear
    
    * spiff up tests
    
    * prep for rebase
    
    * makeExternal query event handler + test
    
    * add polar_call_result to FFI class
    
    * external call
    
    * Predicate class + FFI round trip
    
    * queryPred + some test cleanup
    
    * query for a variable
    
    * types -> argTypes
    
    * externalisa + externalissubspecializer
    
    * exceptions
    
    * better memory management for polar + query ptrs
    
    * split out query class
    
    * split out predicate + variable classes
    
    * go back to using finalize for memory mgmt
    
    * some cleanup + reorder args for consistency
    
    * map args to argTypes for brevity
    
    * reorganize methods + edit method visibility
    
    * reorganized + renamed Ffi to match Ruby
    
    * fix bug with operational error parsing
    
    * add debug handling + debugger test
    
    * lil bug fix in the debugger
    
    * update rust lib
    
    * small cleanup
    
    * clippy + update java rust library
    
    * oso class
    
    * change all methods in Polar to be protected
    
    * Oso, Http, and PathMapper
    
    * cleanup
    
    * a few more tests
    
    * start of maven project
    
    * successfully builds
    
    * start of maven test file
    
    * makefiles
    
    * switch tests to use junit
    
    * goodbye old tests
    
    * fix small exceptions bug
    
    * add rest of polar tests
    
    * add oso tests
    
    * wrap Error and QueryEvent pointers
    
    * update visibility for Ffi wrapper classes
    
    * small rename
    
    * clean up subspecializer logic
    
    * Update languages/java/oso/src/test/java/com/osohq/oso/PolarTest.java
    
    Co-authored-by: David Hatch <david@osohq.com>
    
    * update syntax
    
    * include mvn clean in makefile
    
    * bug fix for query_free
    
    * bug fix for Symbol -> Variable
    
    * fix outstanding conflicts from merge
    
    * add one test for builtin strings
    
    * add register_constant
    
    * refactored the tests for literally no reason
    
    * fix class methods
    
    * rename ffi -> polarFfi and queryFfi
    
    * make Ffi singleton
    
    Co-authored-by: David Hatch <david@osohq.com>

---
+++ languages/java/oso/src/main/java/com/osohq/oso/Ffi.java
@@ -0,0 +1,211 @@
+package com.osohq.oso;
+
+import jnr.ffi.LibraryLoader;
+import jnr.ffi.Pointer;
+
+public class Ffi {
+    // singleton variable
+    private static Ffi ffi = null;
+
+    private PolarLib polarLib;
+
+    protected class Polar {
+        private Pointer ptr;
+
+        private Polar(Pointer ptr) {
+            this.ptr = ptr;
+        }
+
+        protected Pointer get() {
+            return ptr;
+        }
+
+        protected long newId() throws Exceptions.OsoException {
+            return checkResult(polarLib.polar_get_external_id(ptr));
+        }
+
+        protected int loadStr(String src, String filename) throws Exceptions.OsoException {
+            return checkResult(polarLib.polar_load(ptr, src, filename));
+        }
+
+        protected Query newQueryFromStr(String queryStr) throws Exceptions.OsoException {
+            return new Query(checkResult(polarLib.polar_new_query(ptr, queryStr)));
+        }
+
+        protected Query newQueryFromTerm(String queryTerm) throws Exceptions.OsoException {
+            return new Query(checkResult(polarLib.polar_new_query_from_term(ptr, queryTerm)));
+        }
+
+        protected Query nextInlineQuery() throws Exceptions.OsoException {
+            // Don't check result here because the returned Pointer is null to indicate
+            // termination
+            Pointer p = polarLib.polar_next_inline_query(ptr);
+            if (p == null) {
+                return null;
+            } else {
+                return new Query(p);
+            }
+        }
+
+        protected Query newQueryFromRepl() throws Exceptions.OsoException {
+            return new Query(checkResult(polarLib.polar_query_from_repl(ptr)));
+        }
+
+        protected int registerConstant(String name, String value) throws Exceptions.OsoException {
+            return checkResult(polarLib.polar_register_constant(ptr, name, value));
+        }
+
+        @Override
+        protected void finalize() {
+            polarLib.polar_free(ptr);
+        }
+    }
+
+    protected class Query {
+        private Pointer ptr;
+
+        private Query(Pointer ptr) {
+            this.ptr = ptr;
+        }
+
+        protected Pointer get() {
+            return ptr;
+        }
+
+        protected int questionResult(long call_id, int result) throws Exceptions.OsoException {
+            return checkResult(polarLib.polar_question_result(ptr, call_id, result));
+        }
+
+        protected int callResult(long call_id, String value) throws Exceptions.OsoException {
+            return checkResult(polarLib.polar_call_result(ptr, call_id, value));
+        }
+
+        protected QueryEvent nextEvent() throws Exceptions.OsoException {
+            return new QueryEvent(checkResult(polarLib.polar_next_query_event(ptr)));
+        }
+
+        protected int debugCommand(String value) throws Exceptions.OsoException {
+            return checkResult(polarLib.polar_debug_command(ptr, value));
+        }
+
+        @Override
+        protected void finalize() {
+            polarLib.query_free(ptr);
+        }
+
+    }
+
+    protected class QueryEvent {
+        private Pointer ptr;
+
+        private QueryEvent(Pointer ptr) {
+            this.ptr = ptr;
+        }
+
+        public String get() {
+            return ptr.getString(0);
+        }
+
+        @Override
+        protected void finalize() {
+            polarLib.string_free(ptr);
+        }
+
+    }
+
+    protected class Error {
+        private Pointer ptr;
+
+        protected Error() {
+            ptr = polarLib.polar_get_error();
+        }
+
+        private Exceptions.OsoException get() {
+            return Exceptions.getJavaError(ptr.getString(0));
+        }
+
+        @Override
+        protected void finalize() {
+            polarLib.string_free(ptr);
+        }
+
+    }
+
+    protected static interface PolarLib {
+        int polar_debug_command(Pointer query_ptr, String value);
+
+        int polar_free(Pointer polar);
+
+        Pointer polar_get_error();
+
+        long polar_get_external_id(Pointer polar_ptr);
+
+        int polar_load(Pointer polar_ptr, String src, String filename);
+
+        Pointer polar_new();
+
+        Pointer polar_new_query(Pointer polar_ptr, String query_str);
+
+        Pointer polar_new_query_from_term(Pointer polar_ptr, String query_term);
+
+        Pointer polar_next_inline_query(Pointer polar_ptr);
+
+        Pointer polar_next_query_event(Pointer query_ptr);
+
+        Pointer polar_query_from_repl(Pointer polar_ptr);
+
+        int polar_question_result(Pointer query_ptr, long call_id, int result);
+
+        int polar_call_result(Pointer query_ptr, long call_id, String value);
+
+        int query_free(Pointer query);
+
+        int string_free(Pointer s);
+
+        int polar_register_constant(Pointer polar_ptr, String name, String value);
+
+    }
+
+    private Ffi() {
+        polarLib = LibraryLoader.create(PolarLib.class).load("../../../target/debug/libpolar.dylib");
+    }
+
+    protected static Ffi get() {
+        if (ffi == null) {
+            ffi = new Ffi();
+        }
+        return ffi;
+    }
+
+    protected Polar polarNew() throws Exceptions.OsoException {
+        return new Polar(checkResult(polarLib.polar_new()));
+    }
+
+    protected int stringFree(Pointer s) throws Exceptions.OsoException {
+        return checkResult(polarLib.string_free(s));
+    }
+
+    private int checkResult(int i) throws Exceptions.OsoException {
+        if (i == 0) {
+            throw new Error().get();
+        } else {
+            return i;
+        }
+    }
+
+    private long checkResult(long i) throws Exceptions.OsoException {
+        if (i == 0) {
+            throw new Error().get();
+        } else {
+            return i;
+        }
+    }
+
+    private Pointer checkResult(Pointer p) throws Exceptions.OsoException {
+        if (p == null) {
+            throw new Error().get();
+        } else {
+            return p;
+        }
+    }
+}
