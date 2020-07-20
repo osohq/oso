82341801ec4bf88d04809fb6d8bf46445dd6382b
Gabe Jackson <gj@mail.co.de>
2020-07-20 17:45:56 -0400

    Add Ruby test of aliasing + custom constructor for register_class

--- test/test.rb
+++ test/test.rb
@@ -10,7 +10,22 @@ class A
   end
 end
 
+module B
+  class C
+    attr_reader :y
+
+    def initialize(y)
+      @y = y
+    end
+  end
+end
+
 oso.register_class A
+oso.register_class(B::C, name: "C") { |y:| B::C.new(y) }
+oso.load_str <<~POLAR
+  c(instance, y) if instance = new C{y: y};
+  ?= c(instance, "hello") and instance.y = "hello";
+POLAR
 oso.load_file __dir__ + '/test.polar'
 oso.load_queued_files
 puts 'Tests Pass'
