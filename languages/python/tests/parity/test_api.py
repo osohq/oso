bc8859ed36d1bc1467ff043068db8d31b83e4584
Alex Plotnick <alex@osohq.com>
2020-07-21 13:27:10 -0600

    Restore parity tests
    This reverts commit b6d853bb33b6870be71b7093c9908b61d401f48b.

---
+++ languages/python/tests/parity/test_api.py
@@ -0,0 +1,330 @@
+"""Tests the Polar API as an external consumer"""
+import os
+import pytest
+
+from pathlib import Path
+
+from polar import Polar
+from polar.exceptions import PolarRuntimeException, PolarApiException
+from oso import Http, PathMapper
+
+from test_api_externals import Widget, DooDad, Actor, Company, get_frobbed, set_frobbed
+
+try:
+    # This import is required when running the rust version of the library
+    # so that the fixture is registered with pytest.
+    from polar.test_helpers import polar
+except ImportError:
+    pass
+
+from polar.test_helpers import tell, qvar, query, oso_monkeypatch as polar_monkeypatch
+
+# WOWHACK until API stabilizes.
+if not os.getenv("OSO_COMPAT"):
+    from polar import Predicate
+else:
+    from polar.api import Query as Predicate
+
+    setattr(Polar, "register_class", Polar.register_python_class)
+    setattr(Polar, "_to_python", Polar.to_python)
+    setattr(Polar, "_to_polar_term", Polar.to_polar)
+    setattr(Polar, "_load_queued_files", Polar._kb_load)
+    setattr(Polar, "_query_pred", Polar.query)
+
+
+# Set if running tests against old code
+EXPECT_XFAIL_PASS = not bool(os.getenv("EXPECT_XFAIL_PASS", False))
+
+## FIXTURES ##
+
+
+@pytest.fixture
+def load_policy(polar):
+    polar.register_class(Http)
+    polar.register_class(PathMapper)
+    polar.load_file(Path(__file__).parent / "policies" / "test_api.polar")
+
+
+default_company = Company(id="1", default_role="admin")
+
+
+@pytest.fixture
+def widget_in_company(polar_monkeypatch):
+    return polar_monkeypatch.patch(Widget, "company", default_company)
+
+
+@pytest.fixture
+def actor_in_role(polar_monkeypatch):
+    def _patch(role):
+        return polar_monkeypatch.patch(Company, "role", role)
+
+    return _patch
+
+
+## TESTS ##
+
+
+def test_register_class(polar, load_policy):
+    actor = Actor(name="guest")
+    resource = Widget(id="1")
+    action = "get"
+    assert polar.query(
+        Predicate(name="allow", args=(actor, action, resource))
+    ).success
+
+
+def test_allow(polar, load_policy):
+    actor = Actor(name="guest")
+    resource = Widget(id="1")
+    action = "get"
+    assert polar.query(
+        Predicate(name="allow", args=[actor, action, resource])
+    ).success
+    actor = Actor(name="president")
+    assert polar.query(
+        Predicate(name="actorInRole", args=[actor, "admin", resource])
+    ).success
+    assert polar.query(
+        Predicate(name="allowRole", args=["admin", "create", resource])
+    ).success
+
+
+def test_method_resolution_order(polar, load_policy):
+    set_frobbed([])
+    actor = Actor(name="guest")
+    resource = Widget(id="1")
+    action = "get"
+    assert polar.query(
+        Predicate(name="allow", args=[actor, action, resource])
+    ).success
+    assert get_frobbed() == ["Widget"]
+    set_frobbed([])
+    resource = DooDad(id="2")
+    assert polar.query(
+        Predicate(name="allow", args=[actor, action, resource])
+    ).success
+    assert get_frobbed() == ["DooDad", "Widget"]
+
+
+def test_cut(polar, load_policy):
+    set_frobbed([])
+    actor = Actor(name="guest")
+    resource = Widget(id="1")
+    action = "get"
+    assert polar.query(
+        Predicate(name="allow_with_cut", args=[actor, action, resource])
+    ).success
+    assert get_frobbed() == ["Widget"]
+    set_frobbed([])
+    resource = DooDad(id="2")
+    assert polar.query(
+        Predicate(name="allow_with_cut", args=[actor, action, resource])
+    ).success
+    assert get_frobbed() == ["DooDad"]
+
+
+def test_querystring_resource_map(polar, load_policy):
+    assert polar.query(
+        Predicate(
+            name="allow",
+            args=[
+                Actor(name="sam"),
+                "what",
+                Http(path="/widget/12", query={"param": "foo"}),
+            ],
+        )
+    ).success
+    assert not polar.query(
+        Predicate(
+            name="allow", args=[Actor(name="sam"), "what", Http(path="/widget/12")]
+        )
+    ).success
+
+
+def test_resource_mapping(polar, load_policy):
+    # from flask import Flask, request, Response, g
+    try:
+        from flask import Flask, request, Response, g
+    except ImportError:
+        return pytest.skip("Flask not available in environment.")
+
+    def set_user():
+        g.user = Actor(name=request.headers["username"])
+
+    app = Flask(__name__)
+    app.before_request(set_user)
+
+    @app.route("/widget/<int:id>")
+    def get_widget(id):
+        if not polar.query(
+            Predicate(
+                name="allow",
+                args=[g.user, request.method.lower(), Http(path=request.path)],
+            )
+        ).success:
+            return Response("Denied", status=403)
+        return Response("Ok", status=204)
+
+    @app.route("/widget/", methods=["POST"])
+    def create_widget():
+        if not polar.query(
+            Predicate(
+                name="allow",
+                args=[g.user, request.method.lower(), Http(path=request.path)],
+            )
+        ).success:
+            return Response("Denied", status=403)
+        return Response("Ok", status=204)
+
+    with app.test_client() as client:
+        resp = client.get("/widget/1", headers=[("username", "guest")])
+        assert resp.status_code == 204
+
+        resp = client.post("/widget/", headers=[("username", "guest")])
+        assert resp.status_code == 403
+
+        resp = client.post("/widget/", headers=[("username", "president")])
+        assert resp.status_code == 204
+
+
+def test_patching(polar, widget_in_company, actor_in_role, load_policy):
+    user = Actor("test")
+    assert not polar.query(
+        Predicate(name="actorInRole", args=[user, "admin", Widget(id="1")])
+    ).success
+    with widget_in_company:
+        with actor_in_role("admin"):
+            assert polar.query(
+                Predicate(name="actorInRole", args=[user, "admin", Widget(id="1")])
+            ).success
+    assert not polar.query(
+        Predicate(name="actorInRole", args=[user, "admin", Widget(id="1")])
+    ).success
+
+
+## Instance Caching tests (move these somewhere else eventually)
+def test_instance_round_trip(polar, query, qvar):
+    # direct round trip
+    user = Actor("sam")
+    assert polar.host.to_python(polar.host.to_polar_term(user)) is user
+
+
+@pytest.mark.xfail(
+    EXPECT_XFAIL_PASS,
+    reason="Instance literals are not instantiated for unify right now.",
+)
+def test_instance_initialization(polar, query, qvar):
+    # test round trip through kb query
+    user = Actor("sam")
+    env = query('new Actor{name:"sam"} = returned_user')[0]
+    # Note this is not API compatible. It seems like
+    # _query_str on the python version will return uninstantiated
+    # external instances so another _to_python call is needed.
+    # Might need a fix in test_helpers or somewhere esle.
+    assert polar.host.to_python(env["returned_user"]) == user
+
+
+def test_instance_from_external_call(polar, load_policy):
+    user = Actor(name="guest")
+    resource = Widget(id="1", name="name")
+    assert polar.query(
+        Predicate(name="allow", args=[user, "frob", resource])
+    ).success
+
+    resource = Widget(id="2", name="name")
+    assert not polar.query(
+        Predicate(name="allow", args=[user, "frob", resource])
+    ).success
+
+
+def test_load_input_checking(polar):
+    with pytest.raises(PolarApiException):
+        polar.load_file("unreal.py")
+    with pytest.raises(PolarApiException):
+        polar.load_file(Path(__file__).parent / "unreal.py")
+    with pytest.raises(PolarApiException):
+        polar.load_file(Path(__file__).parent / "unreal.polar")
+    with pytest.raises(PolarApiException):
+        polar.load_file(Path(__file__).parent / "unreal.pol")
+
+    polar.load_file(Path(__file__).parent / "policies" / "test_api.polar")
+
+
+@pytest.mark.xfail(
+    EXPECT_XFAIL_PASS,
+    reason="Lists are no longer converted to generators, but are returned as true lists.",
+)
+def test_return_list(polar, load_policy):
+    actor = Actor(name="guest")
+    resource = Widget(id="1")
+    action = "invite"
+    assert polar.query(
+        Predicate(name="allow", args=[actor, action, resource])
+    ).success
+
+
+def test_type_fields(polar, load_policy):
+    resource = Widget(id=1, name="goldfish")
+    actor = Actor(name="elmo", id=1, widget=resource)
+    assert polar.query(
+        Predicate(name="allow", args=[actor, "keep", resource])
+    ).success
+
+
+def test_iter_fields(polar, load_policy):
+    resource = Widget(id=1, name="stapler")
+    actor = Actor(name="milton", id=1)
+    assert polar.query(
+        Predicate(name="allow", args=[actor, "can_have", resource])
+    ).success
+
+
+@pytest.mark.xfail(EXPECT_XFAIL_PASS, reason="Test relies on internal classes.")
+def test_clear(polar, load_policy):
+    old = Path(__file__).parent / "policies" / "load.pol"
+    fails = Path(__file__).parent / "policies" / "reload_fail.pol"
+    new = Path(__file__).parent / "policies" / "reload.pol"
+
+    polar.clear()
+    polar.load_file(old)
+
+    actor = Actor(name="milton", id=1)
+    resource = Widget(id=1, name="thingy")
+    assert polar.query(
+        Predicate(name="allow", args=[actor, "make", resource])
+    ).success
+    assert polar.query(
+        Predicate(name="allow", args=[actor, "get", resource])
+    ).success
+    assert polar.query(
+        Predicate(name="allow", args=[actor, "edit", resource])
+    ).success
+    assert polar.query(
+        Predicate(name="allow", args=[actor, "delete", resource])
+    ).success
+
+    # raises exception because new policy file specifies on a class defined in the old file,
+    # but not in the new file
+    polar.clear()
+    with pytest.raises(PolarRuntimeException):
+        polar.load_file(fails)
+        polar._load_queued_files()
+
+    polar.clear()
+    polar.load_file(new)
+    assert polar.query(
+        Predicate(name="allow", args=[actor, "make", resource])
+    ).success
+    assert not polar.query(
+        Predicate(name="allow", args=[actor, "get", resource])
+    ).success
+    assert not polar.query(
+        Predicate(name="allow", args=[actor, "edit", resource])
+    ).success
+    assert not polar.query(
+        Predicate(name="allow", args=[actor, "delete", resource])
+    ).success
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
---
+++ languages/python/tests/parity/test_polar.py
@@ -0,0 +1,413 @@
+# Various polar tests. More integration less unit. Could move this into the module
+# later if we wish to.
+# Main things to test.
+# - Direct tests of polar code.
+# - Property tests for polar parser.
+# - Property tests for polar semantics.
+# - External functions / python binding tests of some kind, maybe.
+from pathlib import Path
+
+import os
+import shutil
+import tempfile
+
+from polar.exceptions import PolarRuntimeException
+
+from test_polar_externals import Qux, Bar, Foo, MyClass, YourClass, OurClass
+
+try:
+    # This import is required when running the rust version of the library
+    # so that the fixture is registered with pytest.
+    from polar.test_helpers import polar
+except ImportError:
+    pass
+
+from polar.test_helpers import load_file, tell, query, qeval, qvar
+
+import pytest
+
+EXPECT_XFAIL_PASS = not bool(os.getenv("EXPECT_XFAIL_PASS", False))
+
+
+@pytest.fixture
+def externals(polar):
+    polar.register_class(Qux)
+    polar.register_class(Bar)
+    polar.register_class(Foo)
+    polar.register_class(MyClass)
+    polar.register_class(YourClass)
+    polar.register_class(OurClass)
+
+
+def test_load_file(load_file, tell, qeval, qvar):
+    load_file(Path(__file__).parent / "policies/test.pol")
+    assert qeval('test("true")')
+    tell('b("foo")')
+    assert qvar("a(x)", "x", one=True) == "foo"
+
+
+def test_query_multiple(tell, qvar):
+    tell('a("foo")')
+    tell('a("bar")')
+    tell('a("baz")')
+    results = qvar("a(x)", "x")
+    assert results == ["foo", "bar", "baz"]
+
+
+def test_define_rule(tell, qeval):
+    tell("a(x) if b(x) and c(x);")
+    tell('b("apple")')
+    tell('c("apple")')
+    assert qeval('a("apple")')
+
+
+def test_missing_rule(tell, qeval):
+    tell("a(x) if b(x) and c(x);")
+    tell('b("apple")')
+    tell('c("apple")')
+    assert not qeval('d("apple")')
+
+
+def test_negation(tell, qeval):
+    tell('b("apple")')
+    assert qeval('b("apple")')
+    assert not qeval('not (b("apple"))')
+    assert qeval('not (b("notanapple"))')
+
+
+def test_recursive_rule(tell, qeval, qvar):
+    tell('derive("apple", "orange")')
+    tell('derive("orange", "avacado")')
+    tell('derive("avacado", "juniper_berry")')
+    results = qvar('derive("apple", x)', "x")
+    assert results == ["orange"]
+    tell("derives(a, b) if derive(a, b);")
+    tell("derives(a, b) if derive(a, z) and derives(z, b);")
+    assert qeval('derives("apple", "juniper_berry")')
+    results = qvar('derives("apple", x)', "x")
+    assert results == ["orange", "avacado", "juniper_berry"]
+
+
+def test_disjunctive_rule(tell, qeval):
+    tell("or_eq(a, b) if 1 = 0 or a = b;")
+    assert qeval("or_eq(1, 1)")
+
+    tell("and_or_eq(a, b, c) if (a = b and b = c) or 1 = 0")
+    assert not qeval("and_or_eq(1, 1, 0)")
+    assert qeval("and_or_eq(1, 1, 1)")
+
+    assert qeval("1=0 or (1=1 and 1=1)")
+    assert not qeval("1=0 or (1=0 and 1=1)")
+
+    # not sure if these test anything but :)
+    assert qeval("1=0 or (1=0 or 1=1)")
+    assert not qeval("1=0 or (1=0 or 1=0)")
+
+    assert qeval("1=1 and (1=0 or 1=1)")
+    assert not qeval("1=0 and (1=0 or 1=1)")
+
+
+def test_parens(tell, qeval):
+    tell("paren1(a, b, c) if (a = b and b = c);")
+    tell("paren2(a, b, c) if ((a = b and b = c));")
+    tell("paren3(a, b, c) if (a = b) and (b = c);")
+    tell("paren4(a, b, c, d) if (a = b and b = c and c = d);")
+    tell("paren5(a, b, c) if ((a = b) and (b = c));")
+
+    assert qeval("paren1(1, 1, 1)")
+    assert not qeval("paren1(0, 1, 1)")
+    assert not qeval("paren1(1, 1, 0)")
+    assert not qeval("paren1(1, 0, 1)")
+
+    assert qeval("paren2(1, 1, 1)")
+    assert not qeval("paren2(0, 1, 1)")
+    assert not qeval("paren2(1, 1, 0)")
+    assert not qeval("paren2(1, 0, 1)")
+
+    assert qeval("paren3(1, 1, 1)")
+    assert not qeval("paren3(0, 1, 1)")
+    assert not qeval("paren3(1, 1, 0)")
+    assert not qeval("paren3(1, 0, 1)")
+
+    assert qeval("paren4(1, 1, 1, 1)")
+    assert not qeval("paren4(0, 1, 1, 1)")
+    assert not qeval("paren4(1, 1, 0, 1)")
+    assert not qeval("paren4(1, 1, 1, 0)")
+
+    assert qeval("paren5(1, 1, 1)")
+    assert not qeval("paren5(0, 1, 1)")
+    assert not qeval("paren5(1, 1, 0)")
+    assert not qeval("paren5(1, 0, 1)")
+
+
+def test_defining_things(tell, qeval):
+    facts = [
+        'apple("orange")',
+        'thing("with", "two")',
+        'thing("with", "a", "lot", "of", "arguments", 1, 2, 3, 4, 5)',
+        'thing(with("nested"), "stuff")',
+        "dream(within(a(dream(within(a(dream(within(a(_dream)))))))))",
+        'embedded("strings")',
+    ]
+    for f in facts:
+        tell(f)
+    for f in facts:
+        assert qeval(f)
+
+
+def test_dictionaries(tell, qeval, qvar):
+    # basic dictionary lookup
+    tell('dict({hello: "world", foo: "bar"})')
+    assert qeval('dict(d) and d.hello = "world"')
+
+    ### dictionary lookups with variable fields ###
+    tell("attr(d, k, d.(k))")
+
+    # k = "hello", {hello: "steve"}.(k) = "steve"
+    assert qeval('attr({hello: "steve"}, "hello", "steve")')
+
+    # k = "hello", {hello: "steve"}.(k) = value, value = "steve"
+    assert qvar('attr({hello: "steve"}, "hello", value)', "value", one=True) == "steve"
+
+    # k = key, {hello: "steve"}.(k) = "steve", key = "hello"
+    assert qvar('attr({hello: "steve"}, key, "steve")', "key", one=True) == "hello"
+
+    ### nested lookups ###
+    assert qeval(
+        'attr({hello: {this: {is: "nested"}}}, "hello", {this: {is: "nested"}})'
+    )
+
+    tell("deepget(d, d.hello.this.is)")
+    assert qeval('deepget({hello: {this: {is: "nested"}}}, "nested")')
+
+    tell("myget(d, d.get.inner)")
+    assert qeval('myget({get: {inner: "nested"}}, "nested")')
+
+    tell("x({a: {b:{c:123}}})")
+    tell("x({a: {y:{c:456}}})")
+    assert qvar("x(d) and d.a.(k).c = value", "value") == [123, 456]
+
+    tell("lookup(dict, result) if result = dict.a.b.c;")
+    assert qeval('lookup({a: {b: {c: "nested"}}}, "nested")')
+
+    ### more basic lookup tests ###
+    tell('user({name: "steve", job: "programmer", state: "NY"})')
+    tell('user({name: "alex", job: "programmer", state: "CO"})')
+    tell('user({name: "graham", job: "business", state: "NY"})')
+    assert qeval('user(d) and d.name = "steve"')
+    assert qvar('user({job: "programmer", name: name, state: state})', "name") == [
+        "steve",
+        "alex",
+    ]
+
+
+def test_external_classes(tell, qeval, qvar, externals):
+    assert qeval("new Bar{} isa Foo")
+    assert not qeval("new Qux{} isa Foo")
+    assert qeval('new Foo{}.foo = "Foo!"')
+    assert qeval('new Bar{}.foo = "Bar!"')
+
+
+@pytest.mark.xfail(
+    reason="Doesn't work right now since we don't implement external instance unification."
+)
+def test_unify_class_fields(tell, qeval, qvar, externals):
+    tell("check(name, new Foo{name: name})")
+
+    assert qeval('check("sam", new Foo{name: "sam"})')
+    assert not qeval('check("alex", new Foo{name: "sam"})')
+
+
+def test_argument_patterns(tell, qeval, qvar, externals):
+    tell("isaFoo(name, foo: Foo) if name = foo.name")
+
+    assert qeval('isaFoo(sam, new Foo{name: "sam"})')
+    assert qeval('isaFoo(sam, new Bar{name: "sam"})')
+    assert not qeval('isaFoo("alex", new Foo{name: "sam"})')
+    assert not qeval('isaFoo("alex", new Bar{name: "sam"})')
+    assert not qeval('isaFoo("alex", new Qux{})')
+
+
+@pytest.mark.skip(reason="No longer support external instance unification")
+# TODO: update to use internal classes (depends on instantiation bug fix)
+def test_keys_are_confusing(tell, qeval, qvar, externals):
+    assert qeval("new MyClass{x: 1, y: 2} = new MyClass{y: 2, x: 1}")
+    assert qeval("new MyClass{x: 1, y: 2} = new MyClass{x: 1, y: 2}")
+    assert not qeval("new MyClass{x: 1, y: 2} = new MyClass{x: 2, y: 1}")
+    assert not qeval("new MyClass{x: 1, y: 2} = new MyClass{y: 1, x: 2}")
+    assert not qeval("new MyClass{x: 1} = new MyClass{x: 1, y: 2}")
+    assert not qeval("new MyClass{x: 1, y: 2} = new MyClass{y: 2}")
+
+
+def test_isa(qeval, qvar, externals):
+    assert qeval("{} isa {}")
+    assert qeval("{x: 1} isa {}")
+    assert qeval("{x: 1} isa {x: 1}")
+    assert qeval("{x: 1, y: 2} isa {x: 1}")
+    assert qeval("{x: 1, y: 2} isa {x: 1, y: 2}")
+    assert qeval("{a: {x: 1, y: 2}} isa {a: {y: 2}}")
+    assert not qeval("{a: {x: 1, y: 2}} isa {b: {y: 2}}")
+    assert not qeval("{x: 1} isa {x: 1, y: 2}")
+    assert not qeval("{y: 2} isa {x: 1, y: 2}")
+    assert not qeval("{} isa {x: 1, y: 2}")
+    assert not qeval("{} isa {x: 1}")
+
+    assert qeval("new MyClass{x: 1, y: 2} isa {}")
+    assert qeval("new MyClass{x: 1, y: 2} isa {x: 1, y: 2}")
+    assert not qeval("{x: 1, y: 2} isa MyClass{x: 1, y: 2}")
+
+    assert qeval("new MyClass{x: 1, y: 2} isa MyClass{x: 1}")
+    assert not qeval("new MyClass{y: 2} isa MyClass{x: 1, y: 2}")
+
+    assert qeval("new OurClass{x: 1, y: 2} isa YourClass")
+    assert qeval("new OurClass{x: 1, y: 2} isa MyClass{x: 1}")
+    assert qeval("new OurClass{x: 1, y: 2} isa MyClass{x: 1, y: 2}")
+    assert not qeval("new MyClass{x: 1, y: 2} isa OurClass{x: 1}")
+    assert not qeval("new MyClass{x: 1, y: 2} isa YourClass")
+    assert not qeval("new MyClass{x: 1, y: 2} isa YourClass{}")
+
+
+def test_nested_isa(qeval, qvar, externals):
+    # TODO Need instance literal pattern here.
+    assert qeval(
+        "new MyClass{x: new MyClass{x: 1, y: 2}, y: 2} isa MyClass{x: MyClass{x: 1}}"
+    )
+    assert not qeval(
+        "new MyClass{x: new MyClass{x: 1}, y: 2} isa MyClass{x: MyClass{y: 2}}"
+    )
+
+
+def test_field_unification(qeval):
+    # test dictionary field unification
+    assert qeval("{} = {}")
+    assert qeval("{x: 1} = {x: 1}")
+    assert not qeval("{x: 1} = {x: 2}")
+    assert not qeval("{x: 1} = {y: 1}")
+    assert not qeval("{x: 1, y: 2} = {y: 1, x: 2}")
+    assert qeval("{x: 1, y: 2} = {y: 2, x: 1}")
+
+
+def test_field_unification_external(qeval, externals):
+    # test instance field unification
+    assert qeval("new MyClass{x: 1, y: 2} = new MyClass{y: 2, x: 1}")
+    assert not qeval("new MyClass{x: 1, y: 2} = {y: 2, x: 1}")
+    assert qeval("new MyClass{x: 1, y: 2} = new OurClass{y: 2, x: 1}")
+
+
+@pytest.mark.xfail(EXPECT_XFAIL_PASS, reason="Internal classes not implemented yet.")
+def test_class_definitions(tell, qeval, load_file):
+    # Contains test queries.
+    load_file(Path(__file__).parent / "policies/classes.pol")
+
+    # Test instantiation errors.
+    with pytest.raises(PolarRuntimeException):
+        qeval("NotADefinedClassName{foo: 1}")
+    with pytest.raises(PolarRuntimeException):
+        qeval("Three{foo: One{}}")
+    with pytest.raises(PolarRuntimeException):
+        qeval("Three{unit: Two{}}")
+    with pytest.raises(PolarRuntimeException):
+        qeval("Three{unit: One{}, pair: One{}}")
+
+
+@pytest.mark.xfail(EXPECT_XFAIL_PASS, reason="Classes not implemented yet.")
+def test_field_specializers(load_file, qvar):
+    # Contains test queries.
+    load_file(Path(__file__).parent / "policies/people.pol")
+
+    # Test method ordering w/field specializers.
+    assert qvar('froody(Manager{name: "Sam"}, x)', "x") == [1]
+    assert qvar('froody(Manager{name: "Sam", id: 1}, x)', "x") == [2, 1]
+    assert qvar(
+        'froody(Manager{name: "Sam", id: 1, manager: Person{name: "Sam"}}, x)', "x"
+    ) == [3, 2, 1]
+
+
+@pytest.mark.xfail(EXPECT_XFAIL_PASS, reason="Groups not implemented yet.")
+def test_groups(load_file, qeval, query):
+    # Contains test queries.
+    load_file(Path(__file__).parent / "policies/groups.pol")
+
+    # Check that we can't instantiate groups.
+    with pytest.raises(PolarRuntimeException):
+        qeval("G{}")
+
+    # Test rule ordering with groups.
+    results = query("check_order(A{}, action)")
+    expected = ["A", "G", "H"]
+    assert expected == [result["action"] for result in results]
+
+
+# TODO: Fix with
+# https://www.notion.so/osohq/Internal-classes-cannot-be-instantiated-9554c7298feb4842b5448e7edf1d8b8b
+@pytest.mark.skip("Skipped because of bug in class init.")
+def test_group_field_access(load_file, qvar):
+    load_file(Path(__file__).parent / "policies/groups.pol")
+
+    assert qvar('get_bar(Baz{bar: "test"}, val)', "val", one=True) == "test"
+
+
+def test_booleans(qeval):
+    assert qeval("true = true")
+    assert qeval("false = false")
+    assert not qeval("true = false")
+
+
+def test_comparisons(tell, qeval, qvar, query):
+    assert qeval("3 == 3")
+    assert qeval("3 != 2")
+    assert qeval("2 <= 2")
+    assert qeval("2 <= 3")
+    assert qeval("2 < 3")
+    assert qeval("3 >= 3")
+    assert qeval("3 >= 2")
+    assert qeval("3 > 2")
+    assert qeval("x = 1 and x == 1")
+
+
+def test_bool_from_external_call(polar, qeval, qvar, query):
+    class Booler:
+        def whats_up(self):
+            yield True
+
+    polar.register_class(Booler)
+
+    result = qvar("new Booler{}.whats_up() = var", "var", one=True)
+    assert qeval("new Booler{}.whats_up() = true")
+
+
+def test_numbers_from_external_call(polar, qeval, qvar, query):
+    class Numberer:
+        def give_me_an_int(self):
+            yield 1
+
+        def give_me_a_float(self):
+            yield 1.234
+
+    polar.register_class(Numberer)
+
+    result = qvar("new Numberer{}.give_me_an_int() = var", "var", one=True)
+    assert result == 1
+    assert qeval("new Numberer{}.give_me_an_int() = 1")
+
+    result = qvar("new Numberer{}.give_me_a_float() = var", "var", one=True)
+    assert result == 1.234
+    assert qeval("new Numberer{}.give_me_a_float() = 1.234")
+
+
+def test_arities(tell, qeval):
+    tell("f(1);")
+    tell("f(x, y);")
+    assert qeval("f(1)")
+    assert not qeval("f(2)")
+    assert qeval("f(2, 3)")
+
+
+def test_rule_ordering(tell, qeval, externals):
+    tell("f(_: Foo{}) if cut() and 1 = 2;")
+    tell('f(_: Foo{name: "test"});')
+
+    assert qeval('f(new Foo{ name: "test" }) ')
+    assert qeval('x = new Foo{ name: "test" } and f(x) ')
+    assert not qeval('f(new Foo{ name: "nope" }) ')
+    assert not qeval('x = new Foo{ name: "nope" } and f(x) ')
