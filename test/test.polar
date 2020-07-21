5f33199eb214c7e6e98d76ebe741b87b9a949d6d
Gabe Jackson <gj@mail.co.de>
2020-07-21 14:32:50 -0400

    Test all the things in Ruby

--- test/test.polar
+++ test/test.polar
@@ -3,5 +3,62 @@ allow("a","b","c");
 a(a_var, x_val) if a_var = new A{x: x_val};
 ?= a(a_instance, "hello") and a_instance.x = "hello";
 
-predicate(a: A, c: C) if
+c(instance, y) if instance = new C{y: y};
+?= c(instance, "hello") and instance.y = "hello";
+
+specializers(a: A, c: C) if
     a.x = c.y;
+
+floatLists() if 3.14159 in ["pi", 3.14159];
+
+intDicts() if {a: 42}.a = 42;
+
+comparisons() if
+    2 > 1
+    and not (1 > 2)
+    and 1 < 2
+    and not (2 < 1)
+    and 1 >= 1
+    and 2 >= 1
+    and not (0 >= 1)
+    and 1 <= 1
+    and 1 <= 2
+    and not (3 <= 2)
+    and 1 == 1
+    # and not (-1 == 1)
+    # and 1 != -1
+    and not (1 != 1);
+
+testForall() if
+    forall(x in [1, 1], x = 1)
+    and not forall(x in [1, 2], x = 1);
+
+testRest() if
+    [_, *tail] = [1, 2, 3]
+    and tail = [2, 3];
+
+testMatches(a) if
+    a matches {x: "hello"}
+    and a matches A{x: "hello"}
+    and a matches A
+    and {x: 1, y: 2} matches {x: 1}
+    and {x: 1, y: 3} matches {y: 3}
+    and {x: 1, y: 3} matches {x:1, y: 3}
+    and not {x: 1, y: 3} matches {x:1, y: 4}
+    and new A{x: "hello"} matches A
+    and new A{x: "hello"} matches A{x: "hello"};
+
+testMethodCalls(a, c) if
+    a.foo() == c.foo();
+
+testOr() if false or true;
+
+testCut() if
+    x in [1, 2, 3]
+    and x > 1
+    and cut
+    and x == 3;
+
+testHttpAndPathMapper() if
+    new Http{hostname: "foo", path: "/", query: {x: 1}}.send("hostname") = "foo"
+    and new PathMapper{template: "/foo/{id}/bar/{ego}"}.map("/foo/1/bar/2") = {id: "1", ego: "2"};
--- test/test.rb
+++ test/test.rb
@@ -2,14 +2,24 @@ require 'oso'
 
 oso = Oso.new
 
+# Application class with default kwargs constructor.
 class A
   attr_reader :x
 
   def initialize(x:)
     @x = x
   end
-end
 
+  def foo
+    -1
+  end
+end
+oso.register_class A
+
+class D < A; end
+
+# Namespaced application class (to be aliased) with custom
+# constructor.
 module B
   class C
     attr_reader :y
@@ -17,21 +27,51 @@ module B
     def initialize(y)
       @y = y
     end
+
+    def foo
+      -1
+    end
   end
 end
+oso.register_class(B::C, name: 'C') { |y:| B::C.new(y) }
 
-oso.register_class A
 oso.load_file __dir__ + '/test.polar'
 oso.load_queued_files
 
-oso.allow(actor: 'a', action: 'b', resource: 'c')
+raise unless oso.allow(actor: 'a', action: 'b', resource: 'c')
 
-oso.register_class(B::C, name: 'C') { |y:| B::C.new(y) }
 oso.load_str <<~POLAR
-  c(instance, y) if instance = new C{y: y};
-  ?= c(instance, "hello") and instance.y = "hello";
+  ?= x = "hello world!" and x.end_with?("world!");
 POLAR
 
-oso.query_predicate('predicate', A.new(x: 'hello'), B::C.new('hello')).next
+exception_thrown = false
+begin
+  oso.load_str 'missingSemicolon()'
+rescue Oso::Polar::ParseError::UnrecognizedEOF => e
+  exception_thrown = true
+  raise unless e.message == 'hit the end of the file unexpectedly. Did you forget a semi-colon at line 1, column 19'
+end
+raise unless exception_thrown
 
-puts 'Tests Pass'
+oso.query_predicate('specializers', D.new(x: 'hello'), B::C.new('hello')).next
+oso.query_predicate('floatLists').next
+oso.query_predicate('intDicts').next
+oso.query_predicate('comparisons').next
+oso.query_predicate('testForall').next
+oso.query_predicate('testRest').next
+oso.query_predicate('testMatches', A.new(x: 'hello')).next
+oso.query_predicate('testMethodCalls', A.new(x: 'hello'), B::C.new('hello')).next
+oso.query_predicate('testOr').next
+
+exception_thrown = false
+begin
+  oso.query_predicate('testCut').next
+rescue StopIteration
+  exception_thrown = true
+end
+raise unless exception_thrown
+
+oso.register_class String
+oso.load_str '?= x = "" and x.length == String.send("new").length;'
+
+oso.query_predicate('testHttpAndPathMapper').next
