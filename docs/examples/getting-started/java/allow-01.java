aac5eea564801b839f83a729b2680d2972ff53ab
Sam Scott <sam@osohq.com>
2020-07-22 21:27:29 -0400

    Update docs/examples/getting-started/java/allow-01.java
    Co-authored-by: David Hatch <david@osohq.com>

--- docs/examples/getting-started/java/allow-01.java
+++ docs/examples/getting-started/java/allow-01.java
@@ -2,7 +2,7 @@ import com.osohq.oso.Oso;
 
 class Allow {
     public static void main(String[] args) {
-        Oso oso = Oso();
+        Oso oso = new Oso();
         String actor = "alice@example.com";
         Expense resource = EXPENSES[1];
         boolean allowed = oso.allow(actor, "view", resource);
