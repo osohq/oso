8649ffa515a52a1bac88703110e6a0c17018d3d9
Sam Scott <sam@osohq.com>
2020-08-03 17:16:47 -0400

    Add fibonacci bench, and minor perf opt.

--- polar-core/src/vm.rs
+++ polar-core/src/vm.rs
@@ -1032,7 +1032,6 @@ impl PolarVirtualMachine {
         self.push_goal(Goal::PopQuery { term: term.clone() })?;
         self.trace.push(Rc::new(Trace {
             node: Node::Term(term.clone()),
-            polar_str: term.to_string(),
             children: vec![],
         }));
 
@@ -1833,7 +1832,6 @@ impl PolarVirtualMachine {
                 goals.push(Goal::TraceRule {
                     trace: Rc::new(Trace {
                         node: Node::Rule(rule.clone()),
-                        polar_str: rule.to_string(),
                         children: vec![],
                     }),
                 });
