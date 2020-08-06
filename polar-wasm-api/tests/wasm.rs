97e6402a8ce4c3677ed5a6cc4d8a5de064ce8b3e
Gabe Jackson <gj@mail.co.de>
2020-08-06 18:22:00 -0400

    Add additional tests of Polar wasm interface

--- polar-wasm-api/tests/wasm.rs
+++ polar-wasm-api/tests/wasm.rs
@@ -1,5 +1,5 @@
-use js_sys::Error;
-use wasm_bindgen::JsCast;
+use js_sys::{Error, JsString, Map, Object, Reflect};
+use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_test::*;
 
 #[wasm_bindgen_test]
@@ -12,20 +12,41 @@ fn polar_load_file_succeeds() {
 #[wasm_bindgen_test]
 fn polar_load_file_errors() {
     let polar = polar_wasm_api::Polar::wasm_new();
-    match polar.wasm_load_file(";", None) {
-        Err(e) => {
-            if let Some(e) = &e.dyn_ref::<Error>() {
-                assert_eq!(e.name(), "ParseError::UnrecognizedToken");
-                assert_eq!(
-                    e.message(),
-                    "did not expect to find the token ';' at line 1, column 1"
-                );
-            } else {
-                panic!();
-            }
-        }
-        _ => panic!(),
-    }
+    let err = polar.wasm_load_file(";", None).unwrap_err();
+    let err: Error = err.dyn_into().unwrap();
+    assert_eq!(err.name(), "ParseError::UnrecognizedToken");
+    assert_eq!(
+        err.message(),
+        "did not expect to find the token ';' at line 1, column 1"
+    );
+}
+
+#[wasm_bindgen_test]
+fn polar_next_inline_query_succeeds() {
+    let polar = polar_wasm_api::Polar::wasm_new();
+    let res = polar.wasm_load_file("?= 1 = 1;", None);
+    assert!(matches!(res, Ok(())));
+    let mut query = polar.wasm_next_inline_query().unwrap();
+    let event: Object = query.wasm_next_event().unwrap().dyn_into().unwrap();
+    let event_kind: JsValue = "Result".into();
+    let event_data = Reflect::get(&event, &event_kind).unwrap();
+    let data_key: JsValue = "bindings".into();
+    let bindings = Reflect::get(&event_data, &data_key).unwrap();
+    assert_eq!(bindings.dyn_into::<Map>().unwrap().size(), 0);
+    let event: JsString = query.wasm_next_event().unwrap().dyn_into().unwrap();
+    assert_eq!(event, "Done");
+    assert!(polar.wasm_next_inline_query().is_none());
+}
+
+#[wasm_bindgen_test]
+fn polar_next_inline_query_errors() {
+    let polar = polar_wasm_api::Polar::wasm_new();
+    let res = polar.wasm_load_file("?= 1 = 2;", None);
+    assert!(matches!(res, Ok(())));
+    let mut query = polar.wasm_next_inline_query().unwrap();
+    let event: JsString = query.wasm_next_event().unwrap().dyn_into().unwrap();
+    assert_eq!(event, "Done");
+    assert!(polar.wasm_next_inline_query().is_none());
 }
 
 #[wasm_bindgen_test]
@@ -41,18 +62,78 @@ fn polar_register_constant_succeeds() {
 #[wasm_bindgen_test]
 fn polar_register_constant_errors() {
     let mut polar = polar_wasm_api::Polar::wasm_new();
-    match polar.wasm_register_constant("mathematics", "") {
-        Err(e) => {
-            if let Some(e) = &e.dyn_ref::<Error>() {
-                assert_eq!(e.name(), "RuntimeError::Serialization");
-                assert_eq!(
-                    e.message(),
-                    "Serialization error: EOF while parsing a value at line 1 column 0"
-                );
-            } else {
-                panic!();
-            }
-        }
-        _ => panic!(),
+    let err = polar.wasm_register_constant("mathematics", "").unwrap_err();
+    let err: Error = err.dyn_into().unwrap();
+    assert_eq!(err.name(), "RuntimeError::Serialization");
+    assert_eq!(
+        err.message(),
+        "Serialization error: EOF while parsing a value at line 1 column 0"
+    );
+}
+
+#[wasm_bindgen_test]
+fn polar_new_query_from_str_succeeds() {
+    let polar = polar_wasm_api::Polar::wasm_new();
+    let mut query = polar.wasm_new_query_from_str("x()").unwrap();
+    let event: JsString = query.wasm_next_event().unwrap().dyn_into().unwrap();
+    assert_eq!(event, "Done");
+}
+
+#[wasm_bindgen_test]
+fn polar_new_query_from_str_errors() {
+    let polar = polar_wasm_api::Polar::wasm_new();
+    let mut query = polar.wasm_new_query_from_str("x").unwrap();
+    let err: Error = query.wasm_next_event().unwrap_err().dyn_into().unwrap();
+    assert_eq!(err.name(), "RuntimeError::TypeError");
+    assert_eq!(err.message(), "trace (most recent evaluation last):\n  in query at line 1, column 1\n    x\nType error: can't query for: x at line 1, column 1");
+}
+
+#[wasm_bindgen_test]
+fn polar_new_query_from_term_succeeds() {
+    let polar = polar_wasm_api::Polar::wasm_new();
+    let term = r#"{"value":{"Call":{"name":"x","args":[]}}}"#;
+    let mut query = polar.wasm_new_query_from_term(term).unwrap();
+    let event: JsString = query.wasm_next_event().unwrap().dyn_into().unwrap();
+    assert_eq!(event, "Done");
+}
+
+#[wasm_bindgen_test]
+fn polar_new_query_from_term_errors() {
+    let polar = polar_wasm_api::Polar::wasm_new();
+    let res = polar.wasm_new_query_from_term("");
+    if let Err(err) = res {
+        let err: Error = err.dyn_into().unwrap();
+        assert_eq!(err.name(), "RuntimeError::Serialization");
+        assert_eq!(
+            err.message(),
+            "Serialization error: EOF while parsing a value at line 1 column 0"
+        );
+    } else {
+        panic!();
     }
+}
+
+#[wasm_bindgen_test]
+fn polar_get_external_id_succeeds() {
+    let polar = polar_wasm_api::Polar::wasm_new();
+    assert_eq!(polar.wasm_get_external_id(), 1);
+    assert_eq!(polar.wasm_get_external_id(), 2);
+}
+
+#[wasm_bindgen_test]
+fn works() {
+    let polar = polar_wasm_api::Polar::wasm_new();
+    let res = polar.wasm_load_file("x() if 1 == 1;\n", None);
+    assert!(matches!(res, Ok(())));
+
+    let mut query = polar.wasm_new_query_from_str("x()").unwrap();
+    let event: Object = query.wasm_next_event().unwrap().dyn_into().unwrap();
+    let event_kind: JsValue = "Result".into();
+    let event_data = Reflect::get(&event, &event_kind).unwrap();
+    let data_key: JsValue = "bindings".into();
+    let bindings = Reflect::get(&event_data, &data_key).unwrap();
+    assert_eq!(bindings.dyn_into::<Map>().unwrap().size(), 0);
+
+    let event: JsString = query.wasm_next_event().unwrap().dyn_into().unwrap();
+    assert_eq!(event, "Done");
 }
