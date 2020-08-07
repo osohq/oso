740560ef61cec7a26ac0d5ddddb2f231d8b0f379
steve olsen <dev@steve.computer>
2020-08-07 08:31:52 -0500

    alpine sdk

--- build_musl.sh
+++ build_musl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 cd oso
-apk add musl-dev rustup
+apk add apline-sdk rustup
 rustup-init -y
 $HOME/.cargo/bin/rustup target add x86_64-unknown-linux-musl
 RUSTFLAGS='-C target-feature=-crt-static -C relocation-model=pic' $HOME/.cargo/bin/cargo build --target x86_64-unknown-linux-musl
