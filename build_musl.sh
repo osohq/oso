6198381f9287d80f52df0add3534b9a52fe6e11d
steve olsen <dev@steve.computer>
2020-08-07 09:02:30 -0500

    try and just build the wheel

--- build_musl.sh
+++ build_musl.sh
@@ -3,9 +3,13 @@ cd oso
 apk add build-base rustup
 rustup-init -y
 $HOME/.cargo/bin/rustup target add x86_64-unknown-linux-musl
-RUSTFLAGS='-C target-feature=-crt-static -C relocation-model=pic' $HOME/.cargo/bin/cargo build --target x86_64-unknown-linux-musl --release
-python -m pip install cibuildwheel==1.4.2
+RUSTFLAGS='-C target-feature=-crt-static -C relocation-model=pic' $HOME/.cargo/bin/cargo build --target x86_64-unknown-linux-musl
 mkdir -p languages/python/native
-cp polar-c-api/polar.h langauges/python/native/
-cp target/x86_64-unknown-linux-musl/release/libpolar.a languages/python/native/
-python -m cibuildwheel --output-dir wheelhouse
+cp polar-c-api/polar.h languages/python/native/
+cp target/x86_64-unknown-linux-musl/debug/libpolar.a languages/python/native/
+cd languages/python
+python -m pip install -r requirements.txt
+python -m pip install -r requirements-tests.txt
+python -m pip install wheel
+python setup.py sdist bdist_wheel
+ls
