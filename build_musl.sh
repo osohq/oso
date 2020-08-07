432495822fbd9c15cb8546f7dd11abeb735b1552
steve olsen <dev@steve.computer>
2020-08-07 08:13:52 -0500

    build it

--- build_musl.sh
+++ build_musl.sh
@@ -2,3 +2,6 @@
 
 apk add musl-dev rustup
 rustup-init -y
+source $HOME/.cargo/env
+rustup target add x86_64-unknown-linux-musl
+RUSTFLAGS='-C target-feature=-crt-static -C relocation-model=pic' cargo build cargo build --target x86_64-unknown-linux-musl
