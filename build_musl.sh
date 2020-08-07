a74878dd7f34452fd1ce178c2de5927f27516885
steve olsen <dev@steve.computer>
2020-08-07 08:19:34 -0500

    just add to path?

--- build_musl.sh
+++ build_musl.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 apk add musl-dev rustup
 rustup-init -y
-source $HOME/.cargo/env
+PATH = $HOME/.cargo/bin:$PATH
 rustup target add x86_64-unknown-linux-musl
 RUSTFLAGS='-C target-feature=-crt-static -C relocation-model=pic' cargo build cargo build --target x86_64-unknown-linux-musl
