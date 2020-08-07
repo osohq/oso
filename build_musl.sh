c9c8d8c322c4ce9917d4233ac5527483686ce4c7
steve olsen <dev@steve.computer>
2020-08-07 08:25:56 -0500

    only one cargo bro

--- build_musl.sh
+++ build_musl.sh
@@ -3,4 +3,4 @@
 apk add musl-dev rustup
 rustup-init -y
 $HOME/.cargo/bin/rustup target add x86_64-unknown-linux-musl
-RUSTFLAGS='-C target-feature=-crt-static -C relocation-model=pic' $HOME/.cargo/bin/cargo build cargo build --target x86_64-unknown-linux-musl
+RUSTFLAGS='-C target-feature=-crt-static -C relocation-model=pic' $HOME/.cargo/bin/cargo build --target x86_64-unknown-linux-musl
