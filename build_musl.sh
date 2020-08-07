c89a4689dbb202d3b27e595cd64e48fae5d5d020
steve olsen <dev@steve.computer>
2020-08-07 08:33:01 -0500

    build base?

--- build_musl.sh
+++ build_musl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 cd oso
-apk add apline-sdk rustup
+apk add build-base rustup
 rustup-init -y
 $HOME/.cargo/bin/rustup target add x86_64-unknown-linux-musl
 RUSTFLAGS='-C target-feature=-crt-static -C relocation-model=pic' $HOME/.cargo/bin/cargo build --target x86_64-unknown-linux-musl
