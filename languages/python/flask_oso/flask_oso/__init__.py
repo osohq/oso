4980de85b13ebb572d433220178cc4a70c703e83
David Hatch <david@osohq.com>
2020-08-10 16:34:46 -0400

    Rename oso.py in flask oso.

--- languages/python/flask_oso/flask_oso/__init__.py
+++ languages/python/flask_oso/flask_oso/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.0.0'
 
-from .oso import FlaskOso
+from .flask_oso import FlaskOso
 from .decorators import authorize, skip_authorization
 
 __all__ = ['FlaskOso', 'authorize', 'skip_authorization']
