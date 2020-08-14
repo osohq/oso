49c6e4e4f01e26bcf54cdc847ae3161fcfbb07f8
steve olsen <dev@steve.computer>
2020-08-14 12:17:59 -0500

    fix build for new python directory change and pre-releases

--- scripts/build_musl.sh
+++ scripts/build_musl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 apk add build-base libffi-dev
-cd oso/languages/python
+cd oso/languages/python/oso
 python -m pip install -r requirements.txt
 python -m pip install -r requirements-tests.txt
 python -m pip install wheel
