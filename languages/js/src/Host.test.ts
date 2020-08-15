ee81406e79b15420c9abe4ab170201f7f5200b9d
Gabe Jackson <gj@mail.co.de>
2020-08-15 00:21:23 -0400

    Add Promises + Functions to conversion test

--- languages/js/src/Host.test.ts
+++ languages/js/src/Host.test.ts
@@ -8,18 +8,33 @@ describe('conversions between JS + Polar values', () => {
   test('converts Polar values into JS values', () => {
     const h = new Host(new FfiPolar());
     const int = 1;
-    const float = 3.14159;
+    const float = Math.PI;
     const str = '2';
     const bool = true;
     const list = [int, str, bool];
-    const set = new Set([Math.PI, float, Infinity, NaN]);
+    const set = new Set([Math, float, Infinity, NaN, undefined, null]);
     const map = new Map([[str, set]]);
     const obj = { [str]: map };
+    const promises = {
+      resolved: Promise.resolve(int),
+      pending: Promise.reject(str).catch(() => {}),
+      constructor: Promise.prototype,
+    };
+    const functions = {
+      arrow: () => () => 1,
+      expr: function () {
+        return function () {
+          return 2;
+        };
+      },
+    };
     const value = {
       a: list,
       b: obj,
       c: pred('a', new Actor('b'), new User('c'), new Widget(str)),
       d: new Variable('x'),
+      e: promises,
+      f: functions,
     };
     expect(h.toJs(h.toPolarTerm(value))).toStrictEqual(value);
   });
