b7b374c19d1fdcfcf4b9ba865d381273e18ec1d8
Leina McDermott <leina05@gmail.com>
2020-08-31 17:23:17 -0400

    remove support for instance literals in constructors

--- languages/java/oso/src/main/java/com/osohq/oso/Query.java
+++ languages/java/oso/src/main/java/com/osohq/oso/Query.java
@@ -114,13 +114,12 @@ public class Query implements Enumeration<HashMap<String, Object>> {
                     }
                     JSONObject constructor = data.getJSONObject("constructor").getJSONObject("value");
                     JSONArray initargs;
-                    if (constructor.has("InstanceLiteral")) {
-                        // Keyword initargs are not supported in Java.
-                        className = constructor.getJSONObject("InstanceLiteral").getString("tag");
-                        throw new Exceptions.InstantiationError(className);
-                    } else if (constructor.has("Call")) {
+                    if (constructor.has("Call")) {
                         className = constructor.getJSONObject("Call").getString("name");
                         initargs = constructor.getJSONObject("Call").getJSONArray("args");
+                        if (!(constructor.getJSONObject("Call").get("kwargs") == JSONObject.NULL)) {
+                            throw new Exceptions.InstantiationError(className);
+                        }
                     } else {
                         throw new Exceptions.InvalidConstructorError("Bad constructor");
                     }
@@ -186,10 +185,8 @@ public class Query implements Enumeration<HashMap<String, Object>> {
      *                      instance of a built-in type.
      */
     public void registerCall(String attrName, Optional<List<Object>> args, long callId, JSONObject polarInstance)
-            throws Exceptions.InvalidAttributeError,
-                   Exceptions.InvalidCallError,
-                   Exceptions.UnregisteredInstanceError,
-                   Exceptions.UnexpectedPolarTypeError {
+            throws Exceptions.InvalidAttributeError, Exceptions.InvalidCallError, Exceptions.UnregisteredInstanceError,
+            Exceptions.UnexpectedPolarTypeError {
         if (calls.containsKey(callId)) {
             return;
         }
@@ -205,10 +202,9 @@ public class Query implements Enumeration<HashMap<String, Object>> {
         Object result = null;
         try {
             Class<?> cls = instance instanceof Class ? (Class<?>) instance : instance.getClass();
-            if(args.isPresent()) {
+            if (args.isPresent()) {
                 Class<?>[] argTypes = args.get().stream().map(a -> a.getClass())
-                    .collect(Collectors.toUnmodifiableList())
-                    .toArray(new Class[0]);
+                        .collect(Collectors.toUnmodifiableList()).toArray(new Class[0]);
                 Method method = MethodUtils.getMatchingAccessibleMethod(cls, attrName, argTypes);
                 if (method == null) {
                     throw new Exceptions.InvalidCallError(cls.getName(), attrName, argTypes);
