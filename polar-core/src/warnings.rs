66473088ccab9ebe3b270bbdc3d25e679c4fb512
steve olsen <dev@steve.computer>
2020-09-03 16:12:46 -0500

    Add some common misspellings to unkown specializer message

--- polar-core/src/warnings.rs
+++ polar-core/src/warnings.rs
@@ -5,6 +5,39 @@ use super::terms::*;
 
 use std::collections::{hash_map::Entry, HashMap};
 
+pub fn common_misspellings(t: &str) -> Option<String> {
+    let misspelled_type = match t {
+        "int" => Some("Number"),
+        "i32" => Some("Number"),
+        "i64" => Some("Number"),
+        "u32" => Some("Number"),
+        "u64" => Some("Number"),
+        "size_t" => Some("Number"),
+        "usize" => Some("Number"),
+        "float" => Some("Number"),
+        "f32" => Some("Number"),
+        "double" => Some("Number"),
+        "f64" => Some("Number"),
+        "char" => Some("String"),
+        "str" => Some("String"),
+        "string" => Some("String"),
+        "list" => Some("List"),
+        "array" => Some("List"),
+        "Array" => Some("List"),
+        "dict" => Some("Dictionary"),
+        "dictionary" => Some("Dictionary"),
+        "hash" => Some("Dictionary"),
+        "Hash" => Some("Dictionary"),
+        "map" => Some("Dictionary"),
+        "Map" => Some("Dictionary"),
+        "HashMap" => Some("Dictionary"),
+        "hashmap" => Some("Dictionary"),
+        "hash_map" => Some("Dictionary"),
+        _ => None,
+    };
+    misspelled_type.map(str::to_owned)
+}
+
 /// Warn about singleton variables and unknown specializers in a rule,
 /// except those whose names start with `_`.
 pub fn check_singletons(rule: &Rule, kb: &KnowledgeBase) -> Vec<String> {
@@ -44,7 +77,11 @@ pub fn check_singletons(rule: &Rule, kb: &KnowledgeBase) -> Vec<String> {
     for (sym, singleton) in singletons {
         if let Some(term) = singleton {
             let mut msg = if let Value::Pattern(..) = term.value() {
-                format!("Unknown specializer {}", sym)
+                let mut msg = format!("Unknown specializer '{}'", sym);
+                if let Some(t) = common_misspellings(&sym.0) {
+                    msg.push_str(&format!(", did you mean '{}'?", t));
+                }
+                msg
             } else {
                 format!("Singleton variable {} is unused or undefined, see <https://docs.oso.dev/using/polar-syntax.html#variables>", sym)
             };
