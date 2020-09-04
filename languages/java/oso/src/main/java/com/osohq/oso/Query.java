1d98463c2fc653f910747e68149f319c19058b94
Leina McDermott <leina05@gmail.com>
2020-09-04 14:06:22 -0400

    update instance init test

--- languages/java/oso/src/main/java/com/osohq/oso/Query.java
+++ languages/java/oso/src/main/java/com/osohq/oso/Query.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.concurrent.ForkJoinWorkerThread;
 import java.util.stream.Collectors;
 import org.json.JSONObject;
 import org.json.JSONException;
