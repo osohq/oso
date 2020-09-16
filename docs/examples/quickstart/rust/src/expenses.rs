a5990c0f2e6e84f7f22d3d518e3b415c38976a8e
steve olsen <dev@steve.computer>
2020-09-16 13:26:59 -0500

    this doesn't work

---
+++ docs/examples/quickstart/rust/src/expenses.rs
@@ -0,0 +1,29 @@
+use lazy_static::lazy_static;
+use std::string::ToString;
+
+use oso_derive::*;
+
+lazy_static! {
+    pub static ref EXPENSES: [Expense; 3] = [
+        Expense::new(500, "coffee", "alice@example.com"),
+        Expense::new(5000, "software", "alice@example.com"),
+        Expense::new(50000, "flight", "bhavik@example.com"),
+    ];
+}
+
+#[derive(PolarClass, Debug, Clone)]
+pub struct Expense {
+    pub amount: i32,
+    pub description: String,
+    pub submitted_by: String,
+}
+
+impl Expense {
+    pub fn new(amount: i32, description: &str, submitted_by: &str) -> Self {
+        Self {
+            amount,
+            description: description.to_string(),
+            submitted_by: submitted_by.to_string(),
+        }
+    }
+}
