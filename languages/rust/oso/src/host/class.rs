7190db4fe6059a4d9a1ebec8b366b4f778dbf6d9
steve olsen <dev@steve.computer>
2020-09-17 08:29:29 -0500

    fmt

--- languages/rust/oso/src/host/class.rs
+++ languages/rust/oso/src/host/class.rs
@@ -217,7 +217,7 @@ pub struct Instance {
 }
 
 // @TODO: This is very unsafe.
-// Temporary workaround. We need to differentiate between instances which 
+// Temporary workaround. We need to differentiate between instances which
 // _do_ need to be `Send` (e.g. registered as constants on the base `Oso` objects)
 // and instances which don't need to be Send (e.g. created/accessed on a single thread for
 // just one query).
