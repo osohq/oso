74984f1caccb18badad7940311db412f49ce6e37
steve olsen <dev@steve.computer>
2020-09-18 16:15:25 -0500

    comment

--- languages/rust/oso/src/macros.rs
+++ languages/rust/oso/src/macros.rs
@@ -10,7 +10,6 @@ macro_rules! lazy_error {
 macro_rules! check_messages {
     ($core_obj:expr) => {
         while let Some(message) = $core_obj.next_message() {
-            //: polar_core::messages::Message
             match message.kind {
                 ::polar_core::messages::MessageKind::Print => ::tracing::debug!("{}", &message.msg),
                 ::polar_core::messages::MessageKind::Warning => {
