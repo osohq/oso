1ccecd2bd1b121aee3cb54860593f7f5079cf9d6
Gabe Jackson <gj@mail.co.de>
2020-10-06 14:22:29 -0400

    Update Python quickstart tests

--- docs/examples/quickstart/python/test_quickstart.py
+++ docs/examples/quickstart/python/test_quickstart.py
@@ -10,21 +10,10 @@ from expense import *
 from oso import Oso
 
 
-def test_quickstart_policy_2():
-    oso = Oso()
-    alice = "alice@example.com"
-    expense = EXPENSES[1]
-    assert not oso.is_allowed(alice, "GET", expense)
-    oso.register_class(Expense)
-    oso.load_file("../polar/expenses-02.polar")
-    assert oso.is_allowed(alice, "GET", expense)
-    assert not oso.is_allowed("bhavik@example.com", "GET", expense)
-
-
-def test_quickstart_policy_3():
+def test_quickstart_policy_1():
     oso = Oso()
     oso.register_class(Expense)
-    oso.load_file("../polar/expenses-03-py.polar")
+    oso.load_file("../polar/expenses-01-python.polar")
     expense = EXPENSES[1]
     assert oso.is_allowed("bhavik@example.com", "GET", expense)
     assert not oso.is_allowed("bhavik@foo.com", "GET", expense)
@@ -33,7 +22,7 @@ def test_quickstart_policy_3():
 def test_quickstart_policy_4():
     oso = Oso()
     oso.register_class(Expense)
-    oso.load_file("../polar/expenses-04.polar")
+    oso.load_file("../polar/expenses-02-python.polar")
     assert oso.is_allowed("alice@example.com", "GET", EXPENSES[1])
     assert not oso.is_allowed("alice@example.com", "GET", EXPENSES[3])
     assert not oso.is_allowed("bhavik@example.com", "GET", EXPENSES[1])
