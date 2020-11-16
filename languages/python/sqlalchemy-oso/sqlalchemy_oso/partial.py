e9a75a561982bca42c4cfeb50eb64ffe392c544f
David Hatch <david@osohq.com>
2020-11-16 12:29:51 -0500

    Functional programming FTW.

--- languages/python/sqlalchemy-oso/sqlalchemy_oso/partial.py
+++ languages/python/sqlalchemy-oso/sqlalchemy_oso/partial.py
@@ -1,3 +1,6 @@
+import functools
+from typing import Any, Callable, List
+
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.query import Query
 from sqlalchemy.orm import RelationshipProperty, ColumnProperty
@@ -7,6 +10,9 @@ from polar.partial import Partial
 from polar.expression import Expression
 from polar.variable import Variable
 from polar.exceptions import UnsupportedError
+
+# TODO (dhatch) Better types here, first any is model, second any is a sqlalchemy expr.
+EmitFunction = Callable[[Session, Any], Any]
 
 def partial_to_query(expression: Expression, session: Session, model) -> Query:
     """Convert constraints in ``partial`` to a query over ``model``."""
@@ -25,7 +31,7 @@ def partial_to_query(expression: Expression, session: Session, model) -> Query:
 def translate_expr(expression: Expression, session: Session, model):
     assert isinstance(expression, Expression)
     if expression.operator == 'Eq' or expression.operator == 'Unify':
-        return compare_expr(expression, session, model)
+        return translate_compare(expression, session, model)
     elif expression.operator == 'Isa':
         assert expression.args[1].tag == model.__name__
         return None
@@ -36,7 +42,7 @@ def translate_expr(expression: Expression, session: Session, model):
     else:
         raise UnsupportedError(f"Unsupported {expression}")
 
-def translate_and_expr(expression: Expression, session: Session, model) -> BinaryExpression:
+def translate_and_expr(expression: Expression, session: Session, model):
     expr = and_()
     assert expression.operator == "And"
     for expression in expression.args:
@@ -48,7 +54,7 @@ def translate_and_expr(expression: Expression, session: Session, model) -> Binar
 
     return expr
 
-def compare_expr(expression: Expression, session: Session, model) -> BinaryExpression:
+def translate_compare(expression: Expression, session: Session, model):
     left = expression.args[0]
     right = expression.args[1]
 
@@ -60,55 +66,86 @@ def compare_expr(expression: Expression, session: Session, model) -> BinaryExpre
         assert path
         value = left
 
-    return translate_comparison(path, value, model)
+    path, field_name = path[:-1], path[-1]
+    return translate_dot_op(
+        path,
+        session,
+        model,
+        functools.partial(emit_compare, field_name, value))
+
+def translate_in(expression, session, model):
+    assert expression.operator == 'In'
+    left = expression.args[0]
+    right = expression.args[1]
+
+    # IN means at least something must be contained in the property.
+
+    # There are two possible types of in operations. In both, the right hand side
+    # should be a dot op.
+
+    # Partial In: LHS is an expression
+    if isinstance(left, Expression):
+        path = dot_op_path(right)
+        assert path
+
+        return translate_dot_op(
+            path,
+            session,
+            model,
+            functools.partial(emit_subexpression, left))
+    else:
+        # TODO (dhatch) Missing check, left type must match type of the target?
+        path = dot_op_path(right)
+        assert path
+        path, field_name = path[:-1], path[-1]
+        return translate_dot_op(
+            path,
+            session,
+            model,
+            functools.partial(emit_contains, field_name, left))
+
+    # Contains: LHS is not an expression.
 
 
-
-def translate_sub_expression(path, sub_expression, session: Session, model):
-    """Translate a sub_expression path on model."""
-    if len(path) == 1:
-        # Complicated... there are multiple base cases depending on the type of
-        # sub_expression... see if we can improve this. Er I guess these should be diff functions.
-        if sub_expression.op in ['in'
-
+def translate_dot_op(path: List[str], session: Session, model, func: EmitFunction):
     if len(path) == 0:
-        return translate_expr(sub_expression, session, model)
+        return func(session, model)
     else:
-        property = getattr(model, path[0])
-        assert isinstance(property.property, RelationshipProperty)
-        relationship = property.property
-        model = property.entity.class_
-
-        if not relationship.uselist:
-            return property.has(
-                translate_sub_expression(path[1:], sub_expression, session, model))
+        property, model, is_multi_valued = get_relationship(model, path[0])
+        if not is_multi_valued:
+            return property.has(translate_dot_op(path[1:], session, model, func))
         else:
-            return property.any(translate_sub_expression(path[1:], sub_expression, session, model))
+            return property.any(translate_dot_op(path[1:], session, model, func))
 
 
+def get_relationship(model, field_name: str):
+    """Get the property object for field on model. field must be a relationship field.
 
-def translate_comparison(path, value, model):
-    """Translate a comparison operation of ``path`` = ``value`` on ``model``."""
-    if len(path) == 1:
-        property = getattr(model, path[0])
-        return property == value
-    else:
-        # TODO this has assumes that nested relationships are always
-        # a scalar attribute... it also probably isn't as efficient as a
-        # join usually, so we may want to translate differently.
-        property = getattr(model, path[0])
-        assert isinstance(property.property, RelationshipProperty)
-        relationship = property.property
+    :returns: (property, model, is_multi_valued)
+    """
+    property = getattr(model, field_name)
+    assert isinstance(property.property, RelationshipProperty)
+    relationship = property.property
+    model = property.entity.class_
 
-        if not relationship.uselist:
-            return property.has(
-                translate_comparison(path[1:], value, property.entity.class_))
-        else:
-            # TODO (dhatch): Should this assert? This would come from comparing
-            # something against a multi-valued property
-            return property.any(
-                translate_comparison(path[1:], value, property.entity.class_))
+    return (property, model, relationship.uselist)
 
+def emit_compare(field_name, value, session, model):
+    """Emit a comparison operation comparing the value of ``field_name`` on ``model`` to ``value``."""
+    property = getattr(model, field_name)
+    return property == value
+
+def emit_subexpression(sub_expression, session: Session, model):
+    """Emit a sub-expression on ``model``."""
+    return translate_expr(sub_expression, session, model)
+
+def emit_contains(field_name, value, session, model):
+    """Emit a contains operation, checking that multi-valued relationship field ``field_name`` contains ``value``."""
+    # TODO (dhatch): Could this be valid for fields that are not relationship fields?
+    property, model, is_multi_valued = get_relationship(model, field_name)
+    assert is_multi_valued
+
+    return property.contains(value)
 
 # TODO (dhatch): Move this helper into base.
 def dot_op_path(expr):
@@ -133,79 +170,3 @@ def dot_op_path(expr):
         return [expr.args[1]]
 
     return dot_op_path(expr.args[0]) + [expr.args[1]]
-
-def translate_in(expression, session, model):
-    assert expression.operator == 'In'
-    left = expression.args[0]
-    right = expression.args[1]
-
-    # IN means at least something must be contained in the property.
-
-    # There are two possible types of in operations. In both, the right hand side
-    # should be a dot op.
-
-    # Partial In: LHS is an expression
-    if isinstance(left, Expression):
-        path = dot_op_path(right)
-        assert path
-
-        # (_this.is_public = true) in (_this.tags)
-        # [tags], model = Post
-        return translate_in_path(path, left, session, model)
-    else:
-        # TODO (dhatch) Missing check, left type must match type of the target?
-        path = dot_op_path(right)
-        assert path
-        return translate_contains(path, left, session, model)
-
-    # Contains: LHS is not an expression.
-
-# TODO (dhatch): Test multiple levels w/ this function.
-
-def translate_in_path(path, sub_expression, session, model):
-    # TODO use this for dot op path, but have the sub expression be the lookup / comparison.
-    """Translate an in op like (EXPR) in PATH"""
-    if len(path) == 0:
-        # _this.is_public = true
-        # model: Tag
-        return translate_expr(sub_expression, session, model)
-    else:
-        property = getattr(model, path[0])
-        assert isinstance(property.property, RelationshipProperty)
-        relationship = property.property
-        model = property.entity.class_
-
-        # model = Tag
-
-        if not relationship.uselist:
-            return property.has(
-                translate_in_path(path[1:], sub_expression, session, model))
-        else:
-            # post.tags.any()
-            return property.any(translate_in_path(path[1:], sub_expression, session, model))
-
-def translate_contains(path, value, session, model):
-    # TODO use this for dot op path, but have the sub expression be the lookup / comparison.
-    """Translate an in op like (EXPR) in PATH"""
-    if len(path) == 1:
-        # _this.is_public = true
-        # model: Tag
-        property = getattr(model, path[0])
-        model = property.entity.class_
-        assert isinstance(value, model)
-        return property.contains(value)
-    else:
-        # TODO I DON"T THINK THIS IS RIGHT
-        property = getattr(model, path[0])
-        assert isinstance(property.property, RelationshipProperty)
-        relationship = property.property
-        model = property.entity.class_
-
-        # model = Tag
-
-        if not relationship.uselist:
-            return property.has(
-                translate_contains(path[1:], sub_expression, session, model))
-        else:
-            # post.tags.any()
-            return property.any(translate_contains(path[1:], sub_expression, session, model))
