df98305e011bef90373be578690ec78aabbe2199
David Hatch <david@osohq.com>
2020-11-17 08:59:31 -0500

    Format.

--- polar-core/src/partial/partial.rs
+++ polar-core/src/partial/partial.rs
@@ -158,7 +158,10 @@ impl Partial {
         let name = match other.value() {
             Value::Partial(constraints) => constraints.name().clone(),
             Value::Variable(sym) => sym.clone(),
-            _ => panic!("Unexpected in LHS value {:?}, maybe you meant to call Constraints::contains()", other.value()),
+            _ => panic!(
+                "Unexpected in LHS value {:?}, maybe you meant to call Constraints::contains()",
+                other.value()
+            ),
         };
 
         let in_op = op!(In, term!(name.clone()), self.variable_term());
