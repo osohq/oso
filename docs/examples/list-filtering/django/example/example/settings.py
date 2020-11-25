b30bea1bfe5dc6e66ae9cf2d76b3d58f8d6cb11d
Gabe Jackson <gj@mail.co.de>
2020-11-25 16:49:01 -0500

    Remove additional cruft

--- docs/examples/list-filtering/django/example/example/settings.py
+++ docs/examples/list-filtering/django/example/example/settings.py
@@ -31,43 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    # "django.contrib.sessions",
-    # "django.contrib.messages",
-    # "django.contrib.staticfiles",
     "django_oso",
     "app",
 ]
 
-# MIDDLEWARE = [
-#     "django.middleware.security.SecurityMiddleware",
-#     "django.contrib.sessions.middleware.SessionMiddleware",
-#     "django.middleware.common.CommonMiddleware",
-#     "django.middleware.csrf.CsrfViewMiddleware",
-#     "django.contrib.auth.middleware.AuthenticationMiddleware",
-#     "django.contrib.messages.middleware.MessageMiddleware",
-#     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-# ]
-
 ROOT_URLCONF = "example.urls"
-
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         "DIRS": [],
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             "context_processors": [
-#                 "django.template.context_processors.debug",
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.contrib.messages.context_processors.messages",
-#             ],
-#         },
-#     },
-# ]
 
 WSGI_APPLICATION = "example.wsgi.application"
 
@@ -81,43 +51,5 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-# # Password validation
-# # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-#
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-#     },
-# ]
-
-
-# # Internationalization
-# # https://docs.djangoproject.com/en/3.1/topics/i18n/
-#
-# LANGUAGE_CODE = "en-us"
-#
-# TIME_ZONE = "UTC"
-#
-# USE_I18N = True
-#
-# USE_L10N = True
-#
-# USE_TZ = True
-
-
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/3.1/howto/static-files/
-#
-# STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "app.User"
