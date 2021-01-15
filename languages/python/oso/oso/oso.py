cbb08f28576892c75c2aeafd762da0588ef080d0
Leina McDermott <leina05@gmail.com>
2021-01-15 17:22:19 -0500

    Expose `get_allowed_actions` method in the Python API (#642)
    * add get_allowed_actions and test
    
    * allow_unbound -> allow_wildcard
    
    * lint
    
    * fix tests
    
    * changelog
    
    * add docstring to get_allowed_actions
    
    * remove todo
    
    * docstring formatting

--- languages/python/oso/oso/oso.py
+++ languages/python/oso/oso/oso.py
@@ -2,7 +2,7 @@
 
 __version__ = "0.9.0"
 
-from polar import Polar
+from polar import Polar, Variable, exceptions
 
 
 class Oso(Polar):
@@ -32,9 +32,48 @@ class Oso(Polar):
         :param resource: The resource being accessed.
 
         :return: ``True`` if the request is allowed, ``False`` otherwise.
+
         """
         try:
             next(self.query_rule("allow", actor, action, resource))
             return True
         except StopIteration:
             return False
+
+    def get_allowed_actions(self, actor, resource, allow_wildcard=False) -> list:
+        """Determine the actions ``actor`` is allowed to take on ``resource``.
+
+        Collects all actions allowed by allow rules in the Polar policy for the
+        given combination of actor and resource.
+
+        :param actor: The actor for whom to collect allowed actions
+
+        :param resource: The resource being accessed
+
+        :param allow_wildcard: Flag to determine behavior if the policy \
+        includes a wildcard action. E.g., a rule allowing any action: \
+        ``allow(_actor, _action, _resource)``. If ``True``, the method will \
+        return ``["*"]``, if ``False``, the method will raise an exception.
+
+        :type allow_wildcard: bool
+
+        :return: A list of the unique allowed actions.
+        """
+        results = self.query_rule("allow", actor, Variable("action"), resource)
+        actions = set()
+        for result in results:
+            action = result.get("bindings").get("action")
+            if isinstance(action, Variable):
+                if not allow_wildcard:
+                    raise exceptions.OsoError(
+                        """The result of get_allowed_actions() contained an
+                        "unconstrained" action that could represent any
+                        action, but allow_wildcard was set to False. To fix,
+                        set allow_wildcard to True and compare with the "*"
+                        string."""
+                    )
+                else:
+                    return ["*"]
+            actions.add(action)
+
+        return list(actions)
