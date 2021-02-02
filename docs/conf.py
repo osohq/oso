2a35bdf78d50a50b89ff7f3018ea29699bd5bbc6
David Hatch <david@osohq.com>
2021-02-02 13:31:39 -0500

    Bump versions for 0.11.0-beta

--- docs/conf.py
+++ docs/conf.py
@@ -41,8 +41,8 @@ django.setup()
 project = "oso"
 copyright = "2020 Oso Security, Inc"
 author = "oso"
-version = "0.10"
-release = "0.10.2"
+version = "0.11"
+release = "0.11.0-beta"
 
 
 # -- General configuration ---------------------------------------------------
--- languages/python/django-oso/django_oso/__init__.py
+++ languages/python/django-oso/django_oso/__init__.py
@@ -2,6 +2,6 @@ import oso
 
 from .oso import Oso
 
-__version__ = "0.6.0"
+__version__ = "0.7.0-beta"
 
 default_app_config = "django_oso.apps.DjangoOsoConfig"
