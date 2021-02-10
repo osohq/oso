42008f35b9f2dcb51205dbd4fca1ad1a4139fc2c
David Hatch <david@osohq.com>
2021-02-09 22:23:12 -0500

    Move most of unify_vars logic into bind.

--- polar-core/src/bindings.rs
+++ polar-core/src/bindings.rs
@@ -3,10 +3,10 @@
 /// Bindings associate variables in the VM with constraints or values.
 use std::collections::{HashMap, HashSet};
 
-use crate::terms::{Symbol, Term, Value, has_rest_var, Operation, Operator};
 use crate::error::PolarResult;
-use crate::folder::{Folder, fold_term};
+use crate::folder::{fold_term, Folder};
 use crate::formatting::ToPolarString;
+use crate::terms::{has_rest_var, Operation, Operator, Symbol, Term, Value};
 use crate::vm::cycle_constraints;
 
 #[derive(Clone, Debug)]
@@ -27,21 +27,119 @@ pub enum VariableState {
     Partial(Operation),
 }
 
-
 #[derive(Clone, Debug)]
 pub struct BindingManager {
-    bindings: BindingStack
+    bindings: BindingStack,
 }
-
 
 impl BindingManager {
     pub fn new() -> Self {
-        Self {
-            bindings: vec![]
+        Self { bindings: vec![] }
+    }
+
+    /// Bind `var` to `val`, overwriting any already bound value.
+    pub fn rebind(&mut self, var: &Symbol, val: Term) {
+        self.add_binding(var, val);
+    }
+
+    /// Bind `var` to `val`.
+    ///
+    ///
+    /// If `var` is already bound or constrained, the
+    /// binding or constraints are replaced with `val`.
+    pub fn bind(&mut self, var: &Symbol, val: Term) {
+        if let Ok(symbol) = val.value().as_symbol() {
+            self.bind_variables(var, symbol);
+        } else {
+            self.add_binding(var, val);
         }
     }
 
-    pub fn bind(&mut self, var: &Symbol, val: Term) {
+    /// Bind two variables together.
+    fn bind_variables(&mut self, x: &Symbol, y: &Symbol) {
+        match (self.variable_state(x), self.variable_state(y)) {
+            (VariableState::Bound(_), VariableState::Unbound) => {
+                // Replace binding.
+                self.add_binding(x, term!(y.clone()));
+            },
+            (VariableState::Unbound, VariableState::Bound(_)) => {
+                // Bind variables in cycle.
+                if x != y {
+                    self.add_binding(x, term!(y.clone()));
+                    self.add_binding(y, term!(x.clone()));
+                }
+            }
+
+            // Cycles: one or more variables are bound together.
+            (VariableState::Unbound, VariableState::Unbound) => {
+                // Both variables are unbound. Bind them in a new cycle,
+                // but do not create 1-cycles.
+                if x != y {
+                    self.add_binding(x, term!(y.clone()));
+                    self.add_binding(y, term!(x.clone()));
+                }
+            }
+            (VariableState::Cycle(c), VariableState::Unbound) => {
+                // Left is in a cycle. Extend it to include right.
+                let p = c.last().unwrap();
+                assert_ne!(p, x);
+                self.add_binding(p, term!(y.clone()));
+                self.add_binding(y, term!(x.clone()));
+            }
+            (VariableState::Unbound, VariableState::Cycle(d)) => {
+                // Right is in a cycle. Extend it to include left.
+                let q = d.last().unwrap();
+                assert_ne!(q, y);
+                self.add_binding(q, term!(x.clone()));
+                self.add_binding(x, term!(y.clone()));
+            }
+            (VariableState::Cycle(c), VariableState::Cycle(d)) => {
+                // Both variables are in cycles.
+                let h = c.iter().collect::<HashSet<&Symbol>>();
+                let i = d.iter().collect::<HashSet<&Symbol>>();
+                if h.intersection(&i).next().is_some() {
+                    // The cycles must be the same. Do nothing.
+                    assert_eq!(h, i);
+                } else {
+                    // Join the two cycles.
+                    let p = c.last().unwrap();
+                    let q = d.last().unwrap();
+                    assert_ne!(p, x);
+                    assert_ne!(q, y);
+                    self.add_binding(p, term!(y.clone()));
+                    self.add_binding(q, term!(x.clone()));
+                }
+            }
+            (VariableState::Cycle(_), VariableState::Bound(y)) => {
+                // Ground out the cycle.
+                self.add_binding(x, y);
+            }
+            (VariableState::Bound(_), VariableState::Cycle(d)) => {
+                // Left is currently bound. Instead, rebind it by adding it to
+                // the cycle.
+                let q = d.last().unwrap();
+                assert_ne!(q, y);
+                self.add_binding(q, term!(x.clone()));
+                self.add_binding(x, term!(y.clone()));
+            },
+            (VariableState::Bound(_), VariableState::Bound(y)) => {
+                self.add_binding(x, y);
+            },
+            (VariableState::Bound(l), VariableState::Partial(_)) => {
+                // Left is bound, right has constraints.
+                // TODO (dhatch): No unwrap.
+                self.add_constraint(&op!(Unify, l.clone(), term!(y.clone())).into_term()).unwrap();
+            },
+            (VariableState::Partial(_), VariableState::Bound(right)) => {
+                self.add_constraint(&op!(Unify, term!(x.clone()), right).into_term()).unwrap();
+            },
+            (VariableState::Partial(_), _) | (_, VariableState::Partial(_)) => {
+                self.add_constraint(&op!(Unify, term!(x.clone()), term!(y.clone())).into_term()).unwrap();
+            },
+        }
+    }
+
+    fn add_binding(&mut self, var: &Symbol, val: Term) {
         self.bindings.push(Binding(var.clone(), val));
     }
 
@@ -144,7 +242,6 @@ impl BindingManager {
         VariableState::Unbound
     }
 
-
     pub fn add_constraint(&mut self, term: &Term) -> PolarResult<()> {
         let Operation { operator: op, args } = term.value().as_expression().unwrap();
         assert!(
@@ -154,7 +251,10 @@ impl BindingManager {
         assert!(args.len() >= 2);
 
         let (left, right) = (&args[0], &args[1]);
-        match (extract_variable(left.value()), extract_variable(right.value())) {
+        match (
+            extract_variable(left.value()),
+            extract_variable(right.value()),
+        ) {
             (Value::Variable(l), Value::Variable(r)) => {
                 match (self.variable_state(l), self.variable_state(r)) {
                     (VariableState::Unbound, VariableState::Unbound) => {
@@ -221,7 +321,11 @@ impl BindingManager {
                     );
                 }
             },
-            (_, _) => panic!("At least one side of a constraint expression must be a variable. This is {} {}", left.to_polar(), right.to_polar()),
+            (_, _) => panic!(
+                "At least one side of a constraint expression must be a variable. This is {} {}",
+                left.to_polar(),
+                right.to_polar()
+            ),
         }
 
         Ok(())
@@ -233,7 +337,7 @@ impl BindingManager {
         for var in o.variables() {
             match self.variable_state(&var) {
                 VariableState::Bound(_) => (),
-                _ => self.bind(&var, o.clone().into_term()),
+                _ => self.add_binding(&var, o.clone().into_term()),
             }
         }
         Ok(())
@@ -294,12 +398,68 @@ impl BindingManager {
 /// Otherwise, the term is returned.
 fn extract_variable(value: &Value) -> &Value {
     match value {
-        Value::Variable(_) => {
-            value
-        },
+        Value::Variable(_) => value,
         Value::Expression(expr) if expr.operator == Operator::Dot => {
             extract_variable(expr.args[0].value())
-        },
-        _ => value
+        }
+        _ => value,
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn variable_state() {
+        let mut bindings = BindingManager::new();
+
+        let x = sym!("x");
+        let y = sym!("y");
+        let z = sym!("z");
+
+        // Unbound.
+        assert_eq!(bindings.variable_state(&x), VariableState::Unbound);
+
+        // Bound.
+        bindings.add_binding(&x, term!(1));
+        assert_eq!(bindings.variable_state(&x), VariableState::Bound(term!(1)));
+
+        bindings.add_binding(&x, term!(x.clone()));
+        assert_eq!(bindings.variable_state(&x), VariableState::Cycle(vec![x.clone()]));
+
+        // 2-cycle.
+        bindings.add_binding(&x, term!(y.clone()));
+        bindings.add_binding(&y, term!(x.clone()));
+        assert_eq!(
+            bindings.variable_state(&x),
+            VariableState::Cycle(vec![x.clone(), y.clone()])
+        );
+        assert_eq!(
+            bindings.variable_state(&y),
+            VariableState::Cycle(vec![y.clone(), x.clone()])
+        );
+
+        // 3-cycle.
+        bindings.add_binding(&x, term!(y.clone()));
+        bindings.add_binding(&y, term!(z.clone()));
+        bindings.add_binding(&z, term!(x.clone()));
+        assert_eq!(
+            bindings.variable_state(&x),
+            VariableState::Cycle(vec![x.clone(), y.clone(), z.clone()])
+        );
+        assert_eq!(
+            bindings.variable_state(&y),
+            VariableState::Cycle(vec![y.clone(), z.clone(), x.clone()])
+        );
+        assert_eq!(
+            bindings.variable_state(&z),
+            VariableState::Cycle(vec![z.clone(), x.clone(), y])
+        );
+
+        // Expression.
+        bindings.add_binding(&x, term!(op!(And)));
+        assert_eq!(bindings.variable_state(&x), VariableState::Partial(op!(And)));
+    }
+
 }
