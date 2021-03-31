7526f58160ea8b4c5f09f12b27042d221243a6cc
steve olsen <dev@steve.computer>
2021-03-31 12:02:23 -0500

    save dummy

--- languages/python/oso/tests/test_roles.py
+++ languages/python/oso/tests/test_roles.py
@@ -41,6 +41,14 @@ def test_roles():
     oso.register_roles(user=Actor, resource=Widget, resource_id="id", roles=["OWNER"])
     # role constraints?
 
+    # Permissions
+    # TODO:
+    # - define API for creating, adding/removing permissions to roles
+    # - think through UX for devs and what features this supports in the frontend
+    # - how do dynamic permissions get evaluated with `is_allowed?`
+
+    ### BRAINSTORMING
+
     {user: the_actor, role: "ADMIN", resource: the_widget, kind: "Widget"}
 
     role_config = """
@@ -60,10 +68,28 @@ def test_roles():
     """
 
     rules = """
+    # permissions on roles
+    allow(user: Actor, action, widget: Widget) if
+        role = Roles.get_user_roles(user) and
+        role.has_perm(action, widget) and
+        not widget.private;
+
+    # could also just evaluate role permissions in the library, with no hook from Polar, and introduce deny logic to Polar
+
+
+
     allow(user: Actor, "UPDATE", widget: Widget) if
         {role: "ADMIN"} in Roles.get_user_roles(user) or
         {role: "OWNER"} in Roles.get_user_roles(user, widget) or
         widget.public;
+
+    role_allow(role: {role: "OWNER", resource: resource}, _action, resource: Widget);
+
+    role_allow(role: WidgetRole, _action, resource: Widget) if
+        role.widget = resource;
+
+    role_allow(role: {role: "OWNER", resource: resource.parent}, _action, resource: Widget);
+
 
     #allow(user: Actor, "UPDATE", widget: Widget) if
     #    Roles.user_in_role(user, {role: "ADMIN"}) or
@@ -90,3 +116,28 @@ def test_roles():
     #
 
     roles = Roles()
+
+
+## Static vs dynamic
+
+### STATIC (Polar?)
+#### What Role types exist
+#### Role inheritance
+#### What permissions exist
+#### Custom logic
+
+### DYNAMIC (DB)
+#### Role instances
+#### User-role assignments
+#### Role-permission assignments
+#### Role-resource relationships
+
+## What changes a lot?
+# custom role creation
+# user-role-resource
+
+## What doesn't change a lot?
+# role-permission
+# Role levels
+# role scopes/types
+# permissions that exist
