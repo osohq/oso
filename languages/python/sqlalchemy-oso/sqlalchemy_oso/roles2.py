0d59317e5212c9266a764b6fd625dfac89db372a
Leina McDermott <leina05@gmail.com>
2021-04-09 17:48:26 -0400

    relationships in sqlalchemy???

--- languages/python/sqlalchemy-oso/sqlalchemy_oso/roles2.py
+++ languages/python/sqlalchemy-oso/sqlalchemy_oso/roles2.py
@@ -11,18 +11,12 @@ from sqlalchemy.exc import IntegrityError
 
 
 class OsoRoles:
-
     def __init__(self, sqlalchemy_base):
         self.base = sqlalchemy_base
 
-
     def enable(self, oso, sqlalchemy_base, user_model):
-        # class Test:
-        #     @staticmethod
-        #     def hello():
-        #         return "world"
+        # TODO: check that user_model is valid
 
-        # oso.register_class(Test)
         class Permission(sqlalchemy_base):
             __tablename__ = "permissions"
             id = Column(Integer, primary_key=True)
@@ -35,11 +29,19 @@ class OsoRoles:
             resource_type = Column(String)
             name = Column(String)
 
-        class RolePermission:
+        class RolePermission(sqlalchemy_base):
             __tablename__ = "role_permission"
             id = Column(Integer, primary_key=True)
             role_id = Column(Integer, ForeignKey("roles.id"))
             permission_id = Column(Integer, ForeignKey("permissions.id"))
+
+        # TODO: how do we store relationships?
+        class ParentRelationship(sqlalchemy_base):
+            id = Column(Integer, primary_key=True)
+            name = Column(String)
+            child_type = Column(String)
+            parent_type = Column(String)
+            parent_field = Column(String)
 
         # If you have from_role, you also get to_role
         # add types probably
@@ -49,9 +51,26 @@ class OsoRoles:
             from_role_id = Column(Integer, ForeignKey("roles.id"))
             to_role_id = Column(Integer, ForeignKey("roles.id"))
 
+        user_pk_type = inspect(user_model).primary_key[0].type
+        user_pk_name = inspect(user_model).primary_key[0].name
+        user_table_name = user_model.__tablename__
+
         class UserRole(sqlalchemy_base):
             __tablename__ = "user_roles"
             id = Column(Integer, primary_key=True)
-            user_id =  
-            resource: Any
-            role_id: int
+            user_id = Column(
+                user_pk_type, ForeignKey(f"{user_table_name}.{user_pk_name}")
+            )
+            # TODO: this will only work when the resource PK is a single integer
+            # We may need to have a new `UserRole` table for every role type...
+            resource_type = Column(String)
+            resource_id = Column(Integer)
+            role_id = Column(Integer)
+
+        def configure(self, resource_types, relationships, permissions, roles):
+            pass
+            # TODO: input validation
+            # self.resource_types = resource_types
+            # for name, field in relationships:
+            #     new
+
