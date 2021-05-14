0f54191b8ac2ffc8aac0c1f92caa056bf44820c1
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-05-14 18:25:27 -0400

    Revert "Default to disabling baked queries"
    This reverts commit 8e9d158926b4f8d45258e2081eeb1a672bfe965c.

--- languages/python/sqlalchemy-oso/sqlalchemy_oso/session.py
+++ languages/python/sqlalchemy-oso/sqlalchemy_oso/session.py
@@ -148,12 +148,6 @@ def authorized_sessionmaker(
     remain fixed for a given session*. This prevents authorization responses
     from changing, ensuring that the session's identity map never contains
     unauthorized objects.
-
-    NOTE: Unless ``enable_baked_queries=True`` is passed as a kwarg,
-          _baked_queries are disabled since the caching mechanism can lead to
-          authorization backdoors.
-
-    .. _baked_queries: https://docs.sqlalchemy.org/en/13/orm/extensions/baked.html
     """
     if class_ is None:
         class_ = Session
@@ -218,13 +212,7 @@ def scoped_session(
     :param kwargs: Additional keyword arguments to pass to
                    :py:func:`authorized_sessionmaker`.
 
-    NOTE: Unless ``enable_baked_queries=True`` is passed as a kwarg,
-          _baked_queries are disabled since the caching mechanism can lead to
-          authorization backdoors.
-
     .. _scoped_session: https://docs.sqlalchemy.org/en/13/orm/contextual.html
-
-    .. _baked_queries: https://docs.sqlalchemy.org/en/13/orm/extensions/baked.html
     """
     scopefunc = scopefunc or (lambda: None)
 
@@ -246,12 +234,6 @@ class AuthorizedSessionBase(object):
 
         class MySession(AuthorizedSessionBase, sqlalchemy.orm.Session):
             pass
-
-    NOTE: Unless ``enable_baked_queries=True`` is passed to the constructor,
-          _baked_queries are disabled since the caching mechanism can lead to
-          authorization backdoors.
-
-    .. _baked_queries: https://docs.sqlalchemy.org/en/13/orm/extensions/baked.html
     """
 
     def __init__(self, oso: Oso, user, checked_permissions: Permissions, **options):
@@ -290,11 +272,6 @@ class AuthorizedSessionBase(object):
         self._oso_user = user
         self._oso_checked_permissions = checked_permissions
 
-        # Unless a user explicitly enables baked queries with the understanding
-        # that it can result in an authorization backdoor, disable them.
-        if "enable_baked_queries" not in options:
-            options["enable_baked_queries"] = False
-
         super().__init__(**options)  # type: ignore
 
     @property
@@ -313,12 +290,6 @@ class AuthorizedSession(AuthorizedSessionBase, Session):
 
     Usually :py:func:`authorized_sessionmaker` is used instead of directly
     instantiating the session.
-
-    NOTE: Unless ``enable_baked_queries=True`` is passed to the constructor,
-          _baked_queries are disabled since the caching mechanism can lead to
-          authorization backdoors.
-
-    .. _baked_queries: https://docs.sqlalchemy.org/en/13/orm/extensions/baked.html
     """
 
     pass
