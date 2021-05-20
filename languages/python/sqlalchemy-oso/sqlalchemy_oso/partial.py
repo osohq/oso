677a0aacae9d6b152d8ea1521111a20264e5f308
David Hatch <david@osohq.com>
2021-05-20 11:44:08 -0400

    Add documentation for SQLAlchemy library.

--- languages/python/sqlalchemy-oso/sqlalchemy_oso/partial.py
+++ languages/python/sqlalchemy-oso/sqlalchemy_oso/partial.py
@@ -1,3 +1,70 @@
+"""Translate Oso Expressions into SQLAlchemy Expressions.
+
+This module translates between Oso Expressions that are returned from queries
+with partial variables into SQLAlchemy constraints.
+
+The entrypoint is :py:func:`partial_to_filter`. The translation is written as a
+recursive mapping operation. We do a traversal of the expression tree, using the
+``translate_*`` functions to map each node of the Oso expression tree to a
+SQLAlchemy expression.
+
+Translation functions
+=====================
+
+These functions accept as input:
+
+- ``expression``: an :py:class:`polar.expression.Expression` instance returned
+  by the query. The expression must be translated by
+  :py:func:`sqlalchemy_oso.preprocess.preprocess`.
+- ``session``: The :py:class:`sqlalchemy.orm.Session` session object to
+  translate for.
+- ``model``: The model class that this expression is constraining.
+- ``get_model``: A callable that returns a SQLAlchemy model type corresponding
+  with a Polar type tag.
+
+Expression structure
+--------------------
+
+The translation functions operate over expressions that constrain a single
+variable, named ``_this`` which corresponds to the ``model`` pararmeter.
+Constraints on a to-many relationship (expressed in Polar like ``tag in
+post.tags and tag.id = 1``) are represented as a subexpression. The Polar::
+
+    allow(_, _, post) if
+        post.id = 1 and tag in post.tags and
+        tag.id = 2 and
+        tag.is_public;
+
+Would be represented as the expression::
+
+    _this.id = 1 and (_this.id = 2 and _this.is_public= true) in post.tags
+
+- :py:func:`translate_expr`: Translate an expression.
+- :py:func:`translate_and`: Translate an and operation
+- :py:func:`translate_compare`: Translate a comparison operation (=, <, etc.)
+- :py:func:`translate_in`: Translate an in opertaion.
+- :py:func:`translate_isa`: Translate an isa.
+- :py:func:`translate_dot`: Translate a dot operation.
+
+
+Emit functions
+==============
+
+The functions :py:func:`emit_compare`, :py:func:`emit_contains`, and
+:py:func:`emit_subexpression` are used by :py:func:`translate_dot` to aid in
+producing SQLAlchemy expressions over dot operations.  More information on this
+in the :py:func:`translate_dot` documentation string.
+
+Examples in module documentation
+================================
+
+Throughout the documentation of this module, we will refer to examples
+corresponding to the models declared in ``tests/models.py``.
+
+When recursive translation is applied to an operation, the notation ``t(?)`` is
+used.
+"""
+
 import functools
 from typing import Any, Callable, Tuple
 
@@ -108,6 +175,9 @@ def check_for_roles_method(expression: Expression):
 
 
 def translate_expr(expression: Expression, session: Session, model, get_model):
+    """Translate an expression into a SQLAlchemy expression.
+
+    Accepts any type of expression. Entrypoint to the translation functions."""
     assert isinstance(expression, Expression)
     if expression.operator in COMPARISONS:
         return translate_compare(expression, session, model, get_model)
@@ -121,6 +191,12 @@ def translate_expr(expression: Expression, session: Session, model, get_model):
         raise UnsupportedError(f"Unsupported {expression}")
 
 def translate_and(expression: Expression, session: Session, model, get_model):
+    """Translate a Polar AND into a SQLAlchemy AND.
+
+    Empty and is true: () => sql.true()
+    Single argument: op1 => t(op1)
+    > 1 argument: op1 and op2 and op3 => t(op1) & t(op2) & t(op3)
+    """
     assert expression.operator == "And"
     expr = sql.true()
     for expression in expression.args:
@@ -129,8 +205,28 @@ def translate_and(expression: Expression, session: Session, model, get_model):
 
     return expr
 
-# _this.foo matches X allow(_, _, resource: Post)
 def translate_isa(expression: Expression, session: Session, model, get_model):
+    """Translate an Isa operation. (matches keyword)
+
+    Check that the field on the left hand side matches the type on the right.
+
+    ``isa`` operations with fields are not supported and throw.
+
+    If the type matches, ``sql.true()`` is returned. If the type doesn't match,
+    ``sql.false()`` is returned.
+
+    So for example::
+
+        allow(_, _, x) if x matches Tag;
+
+    would translate to sql.false() (no rows match) when ``x`` is of type Post,
+    but would translate to ``sql.true()`` when ``x`` is of type Tag.
+
+    _this matches Type => sql.true() if Type ==  model else sql.false()
+    _this.bar matches Type => sql.true() if typeof(model, "bar") == Type
+
+    Where typeof gives the type of the "bar" property of model.
+    """
     assert expression.operator == "Isa"
     left, right = expression.args
     left_path = dot_path(left)
@@ -153,10 +249,23 @@ def translate_isa(expression: Expression, session: Session, model, get_model):
     model_type = inspect(model, raiseerr=True).class_
     return sql.true() if issubclass(model_type, constraint_type) else sql.false()
 
-# =, <, >, >= , ...
-# op: = arg: [.(_this, foo), 1] => Post.foo = 1
-# session.query(Post).filter(Post.created_by.username = "user")
 def translate_compare(expression: Expression, session: Session, model, get_model):
+    """Translate a binary comparison operation.
+
+    Operators are listed in ``COMPARISONS``.
+
+    Either the left or right argument may contain a path. Paths for both
+    arguments (i.e. post.name = post.body) are not supported currently.
+
+    Also handle _this = ? where ? is an instance of the same type as _this.
+
+    _this.path.(path1)+.tail OP val => Model.path.(path1)+.has(Target.tail OP val)
+    val OP _this.path.(path1)+.tail => Model.path.(path1)+.has(Target.tail OP mirror(OP) val)
+    _this = val => model.pk1 = val.pk1 and model.pk2 = val.pk2
+
+    Where Target is the type that the dot path refers to and mirror flips an
+    operaiton.
+    """
     (left, right) = expression.args
     left_path = dot_path(left)
     right_path = dot_path(right)
@@ -202,9 +311,19 @@ def translate_compare(expression: Expression, session: Session, model, get_model
         return pk_filter
 
 
-# tag in post.tags
-# (tag.id = 1, ...) in post.tags
 def translate_in(expression, session, model, get_model):
+    """Translate the in operator.
+
+    Relationship contains at least one value that matches expr.
+    (expr) in _this.path.(path1)+ => Model.path.(path1)+.any(t(expr))
+
+    relationship at least 1 value with no constraints:
+    () in _this.path.(path1)+ => Model.path.(path1)+.any(sql.true())
+
+    relationship contains val
+    val in _this.path.(path1)+ => Model.path.(path1)+.contains(val)
+    """
+
     assert expression.operator == "In"
     left = expression.args[0]
     right = expression.args[1]
@@ -243,28 +362,31 @@ def translate_in(expression, session, model, get_model):
             path, session, model, functools.partial(emit_contains, field_name, left)
         )
 
-# tag in post.created_by and tag.id = 1 and tag.is_public = true
-# (_this.id = 1 and _this.is_public = true) in post.created_by
-# SELECT * FROM post WHERE EXISTS SELECT 1 FROM tag WHERE <emit_subexpression>
 
-# single relationship: filter(Post.created_by.has(func) )
-# multiple relationship: filter(Post.tags.any(func) )
-
-# post.created_by = 1
-# SELECT FROM post WHERE EXISTS (SELECT 1 FROM user LEFT JOIN post ON post.created_by = user.id WHERE <translate_dot>)
 def translate_dot(path: Tuple[str, ...], session: Session, model, func: EmitFunction):
+    """Translate an operation over a path.
+
+    Used to translate comparison operations over paths, and in operations.
+
+    Walks relationship properties on ``model`` using ``path``, ending by calling
+    ``func`` with ``session`` and the ``model`` of the last field as positional
+    arguments.
+
+    This results in adding an ``EXISTS (SELECT 1 FROM related_table WHERE ...)`` to
+    the expression, as documented in the SQLAlchemy documentation for ``has``
+    and ``any``. The ``...`` will either be the next segment of the dot path, or
+    the result of ``func``.
+    """
+
     if len(path) == 0:
         return func(session, model)
     else:
-        # property = getattr(Post, "created_by"), "is_multi_valued" = False, model = User
-        # property = getattr(Post, "tags"), "is_multi_valued" = True, model = User
         property, model, is_multi_valued = get_relationship(model, path[0])
         if not is_multi_valued:
             return property.has(translate_dot(path[1:], session, model, func))
         else:
             return property.any(translate_dot(path[1:], session, model, func))
 
-# allow( ) if user.id = 1;
 
 def get_relationship(model, field_name: str):
     """Get the property object for field on model. field must be a relationship field.
