638a7e7645fb6943ca1965af7f93be2cb1f36789
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-06-16 11:09:43 -0400

    Third time's the charm

--- languages/python/django-oso/tests/test_django_oso.py
+++ languages/python/django-oso/tests/test_django_oso.py
@@ -10,6 +10,7 @@ from django_oso.auth import authorize, authorize_model
 from polar import Variable, Expression
 
 from polar.exceptions import UnsupportedError
+from oso import OsoError
 from .conftest import negated_condition
 
 
