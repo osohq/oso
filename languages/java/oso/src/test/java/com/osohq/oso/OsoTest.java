bea6ce2b349cd8cd3976067e9080359450c56de4
josh rotenberg <joshrotenberg@gmail.com>
2021-06-16 20:03:04 -0700

    remove import

--- languages/java/oso/src/test/java/com/osohq/oso/OsoTest.java
+++ languages/java/oso/src/test/java/com/osohq/oso/OsoTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.net.URL;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class OsoTest {
