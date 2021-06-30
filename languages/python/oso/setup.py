ffba95cf085df5377719a141e6c7b5bfd3418bea
Tomáš Hozman <86679500+tomashozman@users.noreply.github.com>
2021-06-30 13:11:58 +0000

    Improved formatting

--- languages/python/oso/setup.py
+++ languages/python/oso/setup.py
@@ -1,13 +1,13 @@
 import codecs
 import os
-from setuptools import setup, find_packages
-from os import path
 
-here = path.abspath(path.dirname(__file__))
+from setuptools import find_packages, setup
 
-# Get the long description from the README file
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file.
 try:
-    with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
         long_description = f.read()
 except IOError:
     long_description = ""
@@ -19,14 +19,19 @@ with open("requirements.txt") as fp:
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
-    # intentionally *not* adding an encoding option to open, See:
+
+    # Intentionally *not* adding an encoding option to open, See:
     #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+
     with codecs.open(os.path.join(here, rel_path), "r") as fp:
         return fp.read()
 
 
 def get_version(rel_path):
-    """Get version from file. Copied from pip: https://github.com/pypa/pip/blob/master/setup.py#L19"""
+    """Get version from file.
+    Copied from pip: https://github.com/pypa/pip/blob/master/setup.py#L19
+    """
+
     for line in read(rel_path).splitlines():
         if line.startswith("__version__"):
             # __version__ = "0.9"
@@ -53,6 +58,7 @@ setup(
     setup_requires=["cffi>=1.0.0", "wheel"],
     cffi_modules=["polar/build.py:ffibuilder"],
     install_requires=install_requires,
+
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
@@ -61,13 +67,17 @@ setup(
     #
     # Similar to `install_requires` above, these must be valid existing
     # projects.
+
     extras_require={"flask": ["flask", "authlib"], "django": [], "jwt": ["authlib"]},
+
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     #
     # If using Python 2.6 or earlier, then these have to be included in
     # MANIFEST.in as well.
+
     package_data={"polar": ["py.typed"], "oso": ["py.typed"]},  # Optional
+
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
@@ -81,7 +91,9 @@ setup(
     #
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
+
     entry_points={"console_scripts": []},  # Optional
+
     # List additional URLs that are relevant to your project as a dict.
     #
     # This field corresponds to the "Project-URL" metadata fields:
@@ -91,5 +103,6 @@ setup(
     # issues, where the source is hosted, where to say thanks to the package
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
+
     project_urls={},  # Optional
 )
