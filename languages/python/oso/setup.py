6f76f18437ebda5930bd58c53e1994d67b03de5c
Tomáš Hozman <86679500+tomashozman@users.noreply.github.com>
2021-06-30 21:46:36 +0000

    Moved __version__ to __init__.py
    I think it works better as a package this way.

--- languages/python/oso/setup.py
+++ languages/python/oso/setup.py
@@ -43,7 +43,7 @@ def get_version(rel_path):
 
 setup(
     name="oso",
-    version=get_version("oso/oso.py"),
+    version=get_version("oso/__init__.py"),
     description="oso is an open source policy engine for authorization that's embedded in your application",
     long_description=long_description,
     long_description_content_type="text/markdown",
