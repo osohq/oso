aedbf464765ab40606054fd203ce6c0d0f477d92
steve olsen <dev@steve.computer>
2021-07-20 16:03:05 -0500

    first (non relationship) query works

--- languages/python/oso/polar/data_filtering.py
+++ languages/python/oso/polar/data_filtering.py
@@ -4,36 +4,15 @@
 # We have some hooks they need to implement so we can fetch data.
 # We have to have some top level call that initiates everything.
 
-# Should I start with the user side, in the form of a test? Yes, that seems to be a great idea.
-# And... Should that test use roles perhaps?????? Sure could.
-
-
-# So we have some relationship types or whatever that's needed to get us some info
-# We have the callback methods registered on something.
-# We have the actuial evaluation of everything.
-
-# Should the evaluation be like a different VM in the core? It sort of does the same
-# thing maybe as the normal vm where it talks in events?
-
-# Other ways to do it, maybe all the evaluation does happen in the host language but we
-# just have to implement it for each language. It's not all that much maybe?
-
-# Maybe it is the visitor pattern thing that we had before and you can fill out
-# different parts of the expression?
-
-# If the fetching is events, maybe then it all runs in the core and calls out?
-# How would you do other apis if you did it like that though?
-
-# Just going all python to start anyway so don't worry about it dawg just hack.
-
 from typing import Any, List, Dict
 from dataclasses import dataclass
 
 from .expression import Expression
+from .partial import Variable
 
 VALID_KINDS = ["parent"]
 
-
+# Used so we know what fetchers to call and how to match up constraints.
 @dataclass
 class Relationship:
     kind: str
@@ -42,42 +21,22 @@ class Relationship:
     other_field: str
 
 
-# [
-#     {
-#         "bindings": {
-#             "resource": Expression(
-#                 And,
-#                 [
-#                     Expression(Isa, [Variable("_this"), Pattern(Foo, {})]),
-#                     Expression(
-#                         Unify,
-#                         [
-#                             True,
-#                             Expression(
-#                                 Dot,
-#                                 [
-#                                     Expression(Dot, [Variable("_this"), "bar"]),
-#                                     "is_cool",
-#                                 ],
-#                             ),
-#                         ],
-#                     ),
-#                 ],
-#             )
-#         },
-#         "trace": None,
-#     }
-# ]
+# This is a first pass at what kind of thing I want to operate on.
+# I'm calling it a "FilterPlan" but what it is is basically computes
+# all the constraints into the various collections data has to be fetched from
+# and with what constraints you have to fetch it. Those constraints could come
+# from other collections. There's a dependency graph of which collections depend on
+# other ones and we can topologically sort that so we know what order to fetch data in.
+# This sorted order is provided.
 
-# There's a good chance this whole thing can go in the core and just talk with events.
-# First try to get something working I'm going to do a direct recursive interpreter.
-# Second try I'll "compile" the expressions down into just the list of things to do to filter the data.
+# That way evaluation is easy, just fetch each collection, substituting in the results as you go
+# when other collections need them.
 
-
-# Here's the thing, this is like another whole polar evaluator. What a waste of time to write a full interpreter
-# for polar twice. Can all of this stuff just be figured out in the vm while evaluating the first time? I hope so.
-
-# Really trying to just hack my way into understanding this problem.
+# This is probably too simplistic and will have to evolve as I try this out on more and more
+# valid polar but so far is a promising start. (eg there are probably many of these and ways to do
+# unions or differences or all kinds of other stuff)
+# We should be able to emit something like this right out of the core and then only have to implement
+# the fetching and collecting logic in the language library.
 
 # Id for a data set that will be fetched.
 @dataclass
@@ -111,19 +70,16 @@ class FilterPlan:
     Hopefully the thing we can get the core to spit out. It's like the plan
     for how to do the data filtering.
     """
-
     data_sets: Dict[int, Constraints]
     resolve_order: List[int]
     result_set: int
 
 
-# Only pass the constraints to the fetcher that they can handle.
-# So only field constraints (= and in) and values.
-
-# There's the preprocess step, which can hopefully go into the core.
-# Then there's this evaluate piece that actually calls fetchers to get all the data.
-
-
+# For now my fetcher functions accept a Constraints object but each Constraint value has to be grounded
+# which means they have to be actual data, not a reference to another data set.
+# This is fine because I know the dependency order so the way to evaluate this is you start with the first
+# collection, save it's results, and then for each other collection you can substitute in the results
+# before you call the fetching function.
 def ground_constraints(polar, results, filter_plan, constraints):
     # Walk the constraints substituting in any results
     # Since we walk in a dependency order we should have any results mentioned already
@@ -150,77 +106,165 @@ def filter_data(polar, filter_plan):
     return results[filter_plan.result_set]
 
 
-# Result of interpreting is gonna be like some query plan data
-# It's gonna be an order to run things in, and other things like transforms to apply?
-# I think.
+# The hardest part of this is taking the expressions in the bindings that come out of the core
+# and turning them into my new format. Eventually (once this is all figured out) this part should
+# be easy to port to rust and put into the core.
 
-# bars: Constraint(Bar, [is_cool = true]
-# foos: Constraint(Foo, [bar_id in [Result_map(bars, "id")]]
+# We want to take the results from the partial query and return our FilterPlan thing.
+# [
+#     {
+#         "bindings": {
+#             "resource": Expression(
+#                 And,
+#                 [
+#                     Expression(Isa, [Variable("_this"), Pattern(Foo, {})]),
+#                     Expression(
+#                         Unify, [True, Expression(Dot, [Variable("_this"), "is_fooey"])]
+#                     ),
+#                 ],
+#             )
+#         },
+#         "trace": None,
+#     }
+# ]
+# =>
+# FilterPlan({1: Constraints(Foo, [Constraint("Eq", "is_fooey", True)])}, [1], 1)
 
+# [
+#     {
+#         "bindings": {
+#             "resource": Expression(
+#                 And,
+#                 [
+#                     Expression(Isa, [Variable("_this"), Pattern(Foo, {})]),
+#                     Expression(
+#                         Unify,
+#                         [
+#                             True,
+#                             Expression(
+#                                 Dot,
+#                                 [
+#                                     Expression(Dot, [Variable("_this"), "bar"]),
+#                                     "is_cool",
+#                                 ],
+#                             ),
+#                         ],
+#                     ),
+#                     Expression(
+#                         Unify, [True, Expression(Dot, [Variable("_this"), "is_fooey"])]
+#                     ),
+#                 ],
+#             )
+#         },
+#         "trace": None,
+#     }
+# ]
+# =>
+# FilterPlan(
+#     {
+#         1: Constraints(
+#             Foo,
+#             [
+#                 Constraint("In", "bar_id", Attrib("id", Result(2))),
+#                 Constraint("Eq", "is_fooey", True),
+#             ],
+#         ),
+#         2: Constraints(Bar, [Constraint("Eq", "is_cool", True)]),
+#     },
+#     [2, 1],
+#     1,
+# )
 
-class Interpreter:
+class FilterPlanner:
     def __init__(self, polar, cls, variable):
         self.polar = polar
-        self.filtering_class = cls
-        self.filtering_variable = variable
+        self.cls = cls
+        self.variable = variable
 
-    def eval_exp(self, exp):
-        pass
+    def sort_dependencies(self):
+        # topologically sort all the dependencies so we have an
+        # order we can execute the fetches in
+        return [1]
 
-    def eval_unify(self, args):
-        assert len(args) == 2
-        # Could have values or expressions on either side.
-        if isinstance(args[0], Expression):
-            if isinstance(args[1], Expression):
-                raise Exception("TODO")
-            first = self.eval_exp(args[0])
-            second = args[1]
-        elif isinstance(args[1], Expression):
-            first = self.eval_exp(args[1])
-            second = args[0]
-        pass
+    def walk_dot(self, exp):
+        # Return what the unify needs to know to put this constraint
+        # on the right fetch.
+        assert exp.operator == "Dot"
+        assert len(exp.args) == 2
+        var = exp.args[0]
+        field = exp.args[1]
+        if isinstance(var, Variable):
+            # TODO: Keeping track of other variables.
+            assert var == Variable("_this")
+            assert isinstance(field, str)
+            return field
+        if isinstance(var, Expression):
+            assert var.operator == "Dot"
+            inner = self.walk_dot(var)
+            assert False, "Do this tomorrow steve"
 
-    def eval_and(self, args):
-        # Combine all fetch constraints for the and.
-        constraints = {}
-        for exp in args:
-            op = exp.operator
-            if op == "Isa":
-                pass
-            elif op == "Unify":
-                self.eval_unify(exp.args)
-            else:
-                raise Exception("TODO")
-        return {}
+    def process_exp(self, exp):
+        if exp.operator == 'And':
+            for arg in exp.args:
+                self.process_exp(arg)
+        elif exp.operator == 'Isa':
+            # Ignoring for now, probably shouldn't
+            pass
+        elif exp.operator == 'Unify':
+            assert len(exp.args) == 2
+            lhs = exp.args[0]
+            rhs = exp.args[1]
+            if isinstance(rhs, Expression):
+                # Only handle unification with values for now.
+                # TODO: stuff like _this.something = this.bar.something_else
+                # it's sort of like an additional join constraint to the defined relationship
+                assert not isinstance(lhs, Expression)
+            elif isinstance(lhs, Expression):
+                assert not isinstance(rhs, Expression)
+                lhs, rhs = rhs, lhs
+            # We are setting a constraint that the variable rhs must be equal to the value lhs
+            value = lhs
+            field = self.walk_dot(rhs)
 
-    def eval_result(self, query_result):
-        assert "bindings" in query_result
-        assert len(query_result["bindings"]) == 1
-        assert self.filtering_variable in query_result["bindings"]
-        exp = query_result["bindings"][self.filtering_variable]
-        assert isinstance(exp, Expression)
-        assert exp.operator == "And"
+            # Put the constraint on the fetcher.
+            # For now it's just always the top one until the next example
+            self.data_sets[self.sid].constraints.append(Constraint("Eq", field, value))
+        else:
+            assert False, f"Unhandled Operation {exp.operator}"
 
-        # Get all the constraints.
-        constraints = self.eval_and(exp.args)
-
-        # Fetch the data
-        # return it
-        return []
-
-    def eval(self, query_results):
+    def process_bindings(self, query_results):
+        # Making a bunch of assumptions and restrictions for now.
+        # Want something that works for simple queries that I can then expand.
         query_results = list(query_results)
         assert len(query_results) == 1
-        result_objs = []
-        for query_result in query_results:
-            objs = self.eval_result(query_result)
-            result_objs.extend(objs)
-        # TODO: No duplicates.
-        return result_objs
+        assert "bindings" in query_results[0]
+        assert len(query_results[0]["bindings"]) == 1 # Only one variable in bindings.
+        assert self.variable in query_results[0]["bindings"]
+        exp = query_results[0]["bindings"][self.variable]
+        assert isinstance(exp, Expression)
+        assert exp.operator == "And"
+        self.process_exp(exp)
 
 
-# Here is the most dumb simple interepreter I can write.
-# If this works, everything is gonna be just fine.
+    def plan(self, query_results):
+        self.next_id = 2
+        self.data_sets = {1: Constraints(self.cls, [])}
+        self.sid = 1
+        self.dependencies = []
+
+        self.process_bindings(query_results)
+
+        filter_order = self.sort_dependencies()
+
+        return FilterPlan(self.data_sets, filter_order, 1)
+
+
+def process_constraints(polar, cls, variable, query_results):
+    planner = FilterPlanner(polar, cls, variable)
+    plan = planner.plan(query_results)
+    return plan
+
+
 def evaluate(polar, cls, variable, query_results):
-    interp = Interpreter(polar, cls, variable)
-    return interp.eval(query_results)
+    plan = process_constraints(polar, cls, variable, query_results)
+    return filter_data(polar, plan)
