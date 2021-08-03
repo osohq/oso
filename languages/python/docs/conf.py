d990ef8204e4b6c2ee973deacb0c140073f87468
David Hatch <david@osohq.com>
2021-08-03 18:51:57 -0400

    Bump versions and changelog for 0.14.1.

--- languages/python/docs/conf.py
+++ languages/python/docs/conf.py
@@ -42,8 +42,8 @@ import lexer
 project = "oso"
 copyright = "2020-2021 Oso Security, Inc"
 author = "oso"
-version = "0.14"
-release = "0.14.0"
+version = "0.14.1"
+release = "0.14.1"
 
 
 # -- General configuration ---------------------------------------------------
