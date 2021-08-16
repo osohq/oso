09453ab0a00ff9bd4188dc3f596d0ca97dea15f6
Leina McDermott <leina05@gmail.com>
2021-08-16 13:19:24 -0700

    fix for TS test to pass

--- languages/js/src/Polar.test.ts
+++ languages/js/src/Polar.test.ts
@@ -900,11 +900,13 @@ describe('Oso Roles', () => {
               "edit"
           ];
 
-      parent_child(parent_org: Org, repo: Repo) if
-          repo.org = parent_org;
+      parent_child(parent_org, repo: Repo) if
+          repo.org = parent_org
+          and parent_org matches Org;
 
-      parent_child(parent_repo: Repo, issue: Issue) if
-          issue.repo = parent_repo;
+      parent_child(parent_repo, issue: Issue) if
+          issue.repo = parent_repo and
+          parent_repo matches Repo;
 
       actor_has_role_for_resource(actor, role_name, role_resource) if
           role in actor.roles and
