324985b6dfa40439ab9563a87ba2d71586aca666
Gwen Whelan <gwen@osohq.com>
2021-08-20 17:18:19 -0400

    json wrappers

--- languages/ruby/lib/oso/polar/data_filtering.rb
+++ languages/ruby/lib/oso/polar/data_filtering.rb
@@ -2,8 +2,72 @@
 
 module Oso
   module Polar
-    # Polar variable.
+    # Data filtering interface for Ruby
     module DataFiltering
+      def self.filter(polar, filter_plan, filter_plan_resolver: method(:builtin_resolve))
+        filter_plan_resolver.call(polar, filter_plan)
+      end
+
+      def self.builtin_resolve(polar, filter_plan)
+        FilterPlan.new(polar, filter_plan).builtin_resolve
+      end
+
+      # Wrapper for filter plan json.
+      # Represents a set of filter sequences that should allow the host
+      # to obtain the records satisfying a query.
+      class FilterPlan
+        include Enumerable
+        attr_reader :result_sets
+
+        def initialize(polar, parsed_json)
+          @polar = polar
+          @result_sets = parsed_json['result_sets'].map do |rset|
+            ResultSet.new polar, rset
+          end
+        end
+
+        def each(&blk)
+          result_sets.each(&blk)
+        end
+
+        def builtin_resolve # rubocop:disable Metrics/AbcSize
+          reduce([]) do |acc, rs|
+            requests = rs.requests
+            acc + rs.resolve_order.each_with_object({}) do |i, set_results|
+              req = requests[i]
+              constraints = req.constraints
+              constraints.each { |c| c.ground set_results }
+              set_results[i] = @polar.host.types[req.class_tag].fetcher[constraints]
+            end[rs.result_id]
+          end.uniq
+        end
+
+        # Represents a sequence of filters for one set of results
+        class ResultSet
+          attr_reader :requests, :resolve_order, :result_id
+
+          def initialize(polar, parsed_json)
+            @resolve_order = parsed_json['resolve_order']
+            @result_id = parsed_json['result_id']
+            @requests = parsed_json['requests'].each_with_object({}) do |req, reqs|
+              reqs[req[0].to_i] = Request.new(polar, req[1])
+            end
+          end
+
+          # Represents a filter for a result set
+          class Request
+            attr_reader :constraints, :class_tag
+
+            def initialize(polar, parsed_json)
+              @constraints = parsed_json['constraints'].map do |con|
+                Constraint.parse polar, con
+              end
+              @class_tag = parsed_json['class_tag']
+            end
+          end
+        end
+      end
+
       # Represents relationships between resources, eg. parent/child
       class Relationship
         attr_reader :kind, :other_type, :my_field, :other_field
@@ -66,48 +130,32 @@ module Oso
           item = field.nil? ? item : item.send(field)
           @check[item, val]
         end
-      end
 
-      def self.parse_constraint(polar, constraint) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-        kind = constraint['kind']
-        raise unless %w[Eq In Contains].include? kind
+        def self.parse(polar, constraint) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+          kind = constraint['kind']
+          raise unless %w[Eq In Contains].include? kind
 
-        field = constraint['field']
-        value = constraint['value']
+          field = constraint['field']
+          value = constraint['value']
 
-        value_kind = value.keys.first
-        value = value[value_kind]
+          value_kind = value.keys.first
+          value = value[value_kind]
 
-        case value_kind
-        when 'Term'
-          value = polar.host.to_ruby value
-        when 'Ref'
-          child_field = value['field']
-          result_id = value['result_id']
-          value = Ref.new field: child_field, result_id: result_id
-        when 'Field'
-          value = Field.new field: value
-        else
-          raise "Unknown value kind `#{value_kind}`"
+          case value_kind
+          when 'Term'
+            value = polar.host.to_ruby value
+          when 'Ref'
+            child_field = value['field']
+            result_id = value['result_id']
+            value = Ref.new field: child_field, result_id: result_id
+          when 'Field'
+            value = Field.new field: value
+          else
+            raise "Unknown value kind `#{value_kind}`"
+          end
+
+          new kind: kind, field: field, value: value
         end
-
-        Constraint.new kind: kind, field: field, value: value
-      end
-
-      def self.builtin_resolve(polar, filter_plan) # rubocop:disable Metrics/AbcSize
-        filter_plan['result_sets'].reduce([]) do |acc, rs|
-          requests = rs['requests']
-          acc + rs['resolve_order'].each_with_object({}) do |i, set_results|
-            req = requests[i.to_s]
-            constraints = req['constraints'].map { |con| parse_constraint(polar, con) }
-            constraints.each { |c| c.ground set_results }
-            set_results[i] = polar.host.types[req['class_tag']].fetcher[constraints]
-          end[rs['result_id']]
-        end.uniq
-      end
-
-      def self.filter(polar, filter_plan, filter_plan_resolver: method(:builtin_resolve))
-        filter_plan_resolver.call(polar, filter_plan)
       end
     end
   end
