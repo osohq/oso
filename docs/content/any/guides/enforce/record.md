3bf152eea5a5d28de790c224ba95a2638ea5dc75
Graham Kaemmer <gkaemmer@users.noreply.github.com>
2021-08-24 11:21:05 -0400

    Enforcement docs (#1092)

--- docs/content/any/guides/enforce/record.md
+++
@@ -1,9 +0,0 @@
----
-title: Record
-weight: 3
-description: >
-    The most fine-grained enforcement happens over an individual record/row in the database.
----
-
-{{% coming_soon %}}
-
--- languages/python/oso/oso/exceptions.py
+++ languages/python/oso/oso/exceptions.py
@@ -3,8 +3,29 @@ class AuthorizationError(Exception):
 
 
 class NotFoundError(AuthorizationError):
+    """
+    Thrown by the ``authorize`` method of an ``Enforcer``. This error indicates
+    that the actor is not only not allowed to perform the given action, but also
+    is not allowed to ``"read"`` the given resource.
+
+    Most of the time, your app should handle this error by returning a 404 HTTP
+    error to the client.
+
+    To control which action is used for the distinction between
+    ``NotFoundError`` and ``ForbiddenError``, you can customize the
+    ``read_action`` on your ``Enforcer``.
+    """
+
     pass
 
 
 class ForbiddenError(AuthorizationError):
+    """
+    Thrown by the Enforcer methods ``authorize``, ``authorize_field``, and
+    ``authorize_request`` when the action is not allowed.
+
+    Most of the time, your app should handle this error by returning a 403 HTTP
+    error to the client.
+    """
+
     pass
