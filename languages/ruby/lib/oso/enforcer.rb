a4e3e3fe140a2f578bb608877d2eacc781c4ba1d
Graham Kaemmer <gkaemmer@users.noreply.github.com>
2021-08-25 15:54:01 -0400

    Add yard docs

--- languages/ruby/lib/oso/enforcer.rb
+++ languages/ruby/lib/oso/enforcer.rb
@@ -3,15 +3,54 @@
 require 'set'
 
 module Oso
+  # NOTE: This is a preview feature.
+  #
+  # Exposes high-level enforcement APIs which can be used by apps to perform
+  # resource-, request-, and query-level authorization.
   class Enforcer
     attr_accessor :policy
 
+    # Create an Enforcer, which is used to enforce an Oso policy in an app.
+    #
+    # @param policy [Oso::Policy] The `Policy` instance to enforce.
+    # @param get_error [Callable] Optionally override the method used to build errors
+    #   raised by the  {#authorize} and {#authorize_request}
+    #   methods. Should be a callable that takes one argument
+    #   +is_not_found+ and returns an exception.
+    # @param read_action [Any] The action used by the {#authorize} method to
+    #   determine whether an authorization failure should
+    #   raise a {Oso::NotFoundError} or a {Oso::ForbiddenError}
     def initialize(policy, get_error: method(:default_error), read_action: 'read')
       @policy = policy
       @get_error = get_error
       @read_action = read_action
     end
 
+    # Ensure that +actor+ is allowed to perform +action+ on
+    # +resource+.
+    #
+    # If the action is permitted with an +allow+ rule in the policy, then
+    # this method returns +None+. If the action is not permitted by the
+    # policy, this method will raise an error.
+    #
+    # The error raised by this method depends on whether the actor can perform
+    # the +"read"+ action on the resource. If they cannot read the resource,
+    # then a {Oso::NotFoundError} error is raised. Otherwise, a
+    # {Oso::ForbiddenError} is raised.
+    #
+    # @param actor The actor performing the request.
+    # @param action The action the actor is attempting to perform.
+    # @param resource The resource being accessed.
+    # @param check_read [Boolean] If set to +false+, a {Oso::ForbiddenError} is
+    #   always thrown on authorization failures, regardless of whether the actor
+    #   can read the resource. Default is +true+.
+    #
+    # @raise [Oso::ForbiddenError] Raised if the actor does not have permission
+    #   to perform this action on this resource, but _does_ have +"read"+
+    #   permission on the resource.
+    # @raise [Oso::NotFoundError] Raised if the actor does not have permission
+    #   to perform this action on this resource and additionally does not have
+    #   permission to +"read"+ the resource.
     def authorize(actor, action, resource, check_read: true)
       return if policy.query_rule_once('allow', actor, action, resource)
 
@@ -24,14 +63,55 @@ module Oso
       raise @get_error.call(is_not_found)
     end
 
+    # Ensure that +actor+ is allowed to send +request+ to the server.
+    #
+    # Checks the +allow_request+ rule of a policy.
+    #
+    # If the request is permitted with an +allow_request+ rule in the
+    # policy, then this method returns nothing. Otherwise, this method raises
+    # a {Oso::ForbiddenError}.
+    #
+    # @param actor The actor performing the request.
+    # @param request An object representing the request that was sent by the
+    #   actor.
+    #
+    # @raise [Oso::ForbiddenError] Raised if the actor does not have permission
+    #   to send the request.
     def authorize_request(actor, request)
       raise @get_error.call(false) unless policy.query_rule_once('allow_request', actor, request)
     end
 
+    # Ensure that +actor+ is allowed to perform +action+ on a given
+    # +resource+'s +field+.
+    #
+    # If the action is permitted by an +allow_field+ rule in the policy,
+    # then this method returns nothing. If the action is not permitted by the
+    # policy, this method will raise a {Oso::ForbiddenError}.
+    #
+    # @param actor The actor performing the request.
+    # @param action The action the actor is attempting to perform on the
+    #   field.
+    # @param resource The resource being accessed.
+    # @param field The name of the field being accessed.
+    #
+    # @raise [Oso::ForbiddenError] Raised if the actor does not have permission
+    #   to access this field.
     def authorize_field(actor, action, resource, field)
       raise @get_error.call(false) unless policy.query_rule_once('allow_field', actor, action, resource, field)
     end
 
+    # Determine the actions +actor+ is allowed to take on +resource+.
+    #
+    # Collects all actions allowed by allow rules in the Polar policy for the
+    # given combination of actor and resource.
+    #
+    # @param actor The actor for whom to collect allowed actions
+    # @param resource The resource being accessed
+    # @param allow_wildcard Flag to determine behavior if the policy
+    #   includes a wildcard action. E.g., a rule allowing any action:
+    #   +allow(_actor, _action, _resource)+. If +true+, the method will
+    #   return +["*"]+, if +false+, the method will raise an exception.
+    # @return A list of the unique allowed actions.
     def authorized_actions(actor, resource, allow_wildcard: false) # rubocop:disable Metrics/MethodLength
       results = policy.query_rule('allow', actor, Polar::Variable.new('action'), resource)
       actions = Set.new
@@ -52,6 +132,20 @@ module Oso
       actions.to_a
     end
 
+    # Determine the fields of +resource+ on which +actor+ is allowed to
+    # perform  +action+.
+    #
+    # Uses +allow_field+ rules in the policy to find all allowed fields.
+    #
+    # @param actor The actor for whom to collect allowed fields.
+    # @param action The action being taken on the field.
+    # @param resource The resource being accessed.
+    # @param allow_wildcard Flag to determine behavior if the policy \
+    #   includes a wildcard field. E.g., a rule allowing any field: \
+    #   +allow_field(_actor, _action, _resource, _field)+. If +true+, the \
+    #   method will return +["*"]+, if +false+, the method will raise an \
+    #   exception.
+    # @returns A list of the unique allowed fields.
     def authorized_fields(actor, action, resource, allow_wildcard: false) # rubocop:disable Metrics/MethodLength
       results = policy.query_rule('allow_field', actor, action, resource, Polar::Variable.new('field'))
       fields = Set.new
--- languages/ruby/lib/oso/errors.rb
+++ languages/ruby/lib/oso/errors.rb
@@ -7,6 +7,11 @@ module Oso
   class AuthorizationError < StandardError
   end
 
+  # Thrown by the Enforcer methods +authorize+, +authorize_field+, and
+  # +authorize_request+ when the action is not allowed.
+  #
+  # Most of the time, your app should handle this error by returning a 403 HTTP
+  # error to the client.
   class ForbiddenError < AuthorizationError
     def initialize
       super(
@@ -17,6 +22,16 @@ module Oso
     end
   end
 
+  # Thrown by the +authorize+ method of an +Enforcer+. This error indicates
+  # that the actor is not only not allowed to perform the given action, but also
+  # is not allowed to +"read"+ the given resource.
+  #
+  # Most of the time, your app should handle this error by returning a 404 HTTP
+  # error to the client.
+  #
+  # To control which action is used for the distinction between
+  # +NotFoundError+ and +ForbiddenError+, you can customize the
+  # +read_action+ on your +Enforcer+.
   class NotFoundError < AuthorizationError
     def initialize
       super(
