198f1f52701f1a19590b19b5dbc22baddc03f474
Graham Kaemmer <gkaemmer@users.noreply.github.com>
2021-08-27 15:39:39 -0400

    Add javadoc comments to enforcer

--- languages/java/oso/src/main/java/com/osohq/oso/Enforcer.java
+++ languages/java/oso/src/main/java/com/osohq/oso/Enforcer.java
@@ -12,27 +12,22 @@ public class Enforcer {
   }
 
   /**
-   * Submit an `allow` query to the Polar knowledge base.
+   * Ensure that `actor` is allowed to perform `action` on `resource`.
    *
-   * <pre>{@code
-   * Oso oso = new Oso();
-   * o.loadStr("allow(\"guest\", \"get\", \"widget\");");
-   * assert o.isAllowed("guest", "get", "widget");
-   * }</pre>
+   * <p>If the action is permitted with an `allow` rule in the policy, then this method returns
+   * `None`. If the action is not permitted by the policy, this method will raise an error.
    *
-   * @param actor the actor performing the request
-   * @param action the action the actor is attempting to peform
-   * @param resource the resource being accessed
-   * @param checkRead if set to `false`, do not query the policy for the `"read"` action, and always
-   *     throw a ForbiddenException on any non-read authorization failure. Default is `true`.
-   * @return boolean
+   * <p>The error raised by this method depends on whether the actor can perform the `"read"` action
+   * on the resource. If they cannot read the resource, then a `NotFound` error is raised.
+   * Otherwise, a `ForbiddenError` is raised.
+   *
+   * @param actor The actor performing the request.
+   * @param action The action the actor is attempting to perform.
+   * @param resource The resource being accessed.
+   * @param checkRead If set to `false`, a `ForbiddenError` is always thrown on authorization
+   *     failures, regardless of whether the actor can read the resource. Default is `true`.
    * @throws Exceptions.OsoException
    */
-  public void authorize(Object actor, Object action, Object resource)
-      throws Exceptions.OsoException {
-    authorize(actor, action, resource, true);
-  }
-
   public void authorize(Object actor, Object action, Object resource, boolean checkRead)
       throws Exceptions.OsoException {
     boolean authorized = policy.queryRuleOnce("allow", actor, action, resource);
@@ -54,6 +49,23 @@ public class Enforcer {
     throw isNotFound ? new Exceptions.NotFoundException() : new Exceptions.ForbiddenException();
   }
 
+  public void authorize(Object actor, Object action, Object resource)
+      throws Exceptions.OsoException {
+    authorize(actor, action, resource, true);
+  }
+
+  /**
+   * Ensure that `actor` is allowed to send `request` to the server.
+   *
+   * <p>Checks the `allow_request` rule of a policy.
+   *
+   * <p>If the request is permitted with an `allow_request` rule in the policy, then this method
+   * returns nothing. Otherwise, this method raises a `ForbiddenError`.
+   *
+   * @param actor The actor performing the request.
+   * @param request An object representing the request that was sent by the actor.
+   * @throws Exceptions.OsoException
+   */
   public void authorizeRequest(Object actor, Object request) throws Exceptions.OsoException {
     boolean authorized = policy.queryRuleOnce("allow_request", actor, request);
     if (!authorized) {
@@ -61,6 +73,19 @@ public class Enforcer {
     }
   }
 
+  /**
+   * Ensure that `actor` is allowed to perform `action` on a given `resource`'s `field`.
+   *
+   * <p>If the action is permitted by an `allow_field` rule in the policy, then this method returns
+   * nothing. If the action is not permitted by the policy, this method will raise a
+   * `ForbiddenError`.
+   *
+   * @param actor The actor performing the request.
+   * @param action The action the actor is attempting to perform on the field.
+   * @param resource The resource being accessed.
+   * @param field The name of the field being accessed.
+   * @throws Exceptions.OsoException
+   */
   public void authorizeField(Object actor, Object action, Object resource, Object field)
       throws Exceptions.OsoException {
     boolean authorized = policy.queryRuleOnce("allow_field", actor, action, resource, field);
@@ -70,42 +95,17 @@ public class Enforcer {
   }
 
   /**
-   * Return the allowed actions for the given actor and resource, if any.
+   * Determine the actions `actor` is allowed to take on `resource`.
    *
-   * <pre>{@code
-   * Oso oso = new Oso();
-   * o.loadStr("allow(\"guest\", \"get\", \"widget\");");
-   * HashSet actions = o.authorizedActions("guest", "widget");
-   * assert actions.contains("get");
-   * }</pre>
+   * <p>Collects all actions allowed by allow rules in the Polar policy for the given combination of
+   * actor and resource.
    *
-   * @param actor the actor performing the request
-   * @param resource the resource being accessed
-   * @return HashSet<Object>
-   * @throws Exceptions.OsoException
-   */
-  public HashSet<Object> authorizedActions(Object actor, Object resource)
-      throws Exceptions.OsoException {
-    return authorizedActions(actor, resource, false);
-  }
-
-  /**
-   * Return the allowed actions for the given actor and resource, if any. Explicitly allow or
-   * disallow wildcard actions. If allowed, wildcard actions are represented as "*".
-   *
-   * <pre>{@code
-   * Oso oso = new Oso();
-   * o.loadStr("allow(_actor, _action, _resource);");
-   * HashSet actions = o.authorizedActions("guest", "widget", true);
-   * assert actions.contains("*");
-   * HashSet actions = o.authorizedActions("guest", "widget", false);
-   * // OsoException is thrown
-   * }</pre>
-   *
-   * @param actor the actor performing the request
-   * @param resource the resource being accessed
-   * @param allowWildcard whether or not to allow wildcard actions
-   * @return HashSet<Object>
+   * @param actor The actor for whom to collect allowed actions
+   * @param resource The resource being accessed
+   * @param allowWildcard Flag to determine behavior if the policy includes a wildcard action. E.g.,
+   *     a rule allowing any action: `allow(_actor, _action, _resource)`. If `true`, the method will
+   *     return `["*"]`, if `false`, the method will raise an exception.
+   * @return HashSet<Object> A list of the unique allowed actions.
    * @throws Exceptions.OsoException
    */
   public HashSet<Object> authorizedActions(Object actor, Object resource, boolean allowWildcard)
@@ -131,29 +131,23 @@ public class Enforcer {
         .collect(Collectors.toCollection(HashSet::new));
   }
 
-  /**
-   * Return the allowed fields for the given actor and resource, if any.
-   *
-   * @param actor the actor performing the request
-   * @param action the action being performed on the field
-   * @param resource the resource on which the field lives
-   * @return HashSet<Object>
-   * @throws Exceptions.OsoException
-   */
-  public HashSet<Object> authorizedFields(Object actor, Object action, Object resource)
+  public HashSet<Object> authorizedActions(Object actor, Object resource)
       throws Exceptions.OsoException {
-    return authorizedFields(actor, action, resource, false);
+    return authorizedActions(actor, resource, false);
   }
 
   /**
-   * Return the allowed fields for the given actor and resource, if any. Explicitly allow or
-   * disallow wildcard fields. If allowed, wildcard fields are represented as "*".
+   * Determine the fields of `resource` on which `actor` is allowed to perform `action`.
    *
-   * @param actor the actor performing the request
-   * @param action the action being performed on the field
-   * @param resource the resource on which the field lives
-   * @param allowWildcard whether or not to allow wildcard fields
-   * @return HashSet<Object>
+   * <p>Uses `allow_field` rules in the policy to find all allowed fields.
+   *
+   * @param actor The actor for whom to collect allowed fields.
+   * @param action The action being taken on the field.
+   * @param resource The resource being accessed.
+   * @param allowWildcard Flag to determine behavior if the policy \ includes a wildcard field.
+   *     E.g., a rule allowing any field: \ `allow_field(_actor, _action, _resource, _field)`. If
+   *     `true`, the \ method will return `["*"]`, if `false`, the method will raise an \ exception.
+   * @return HashSet<Object> A set of the unique allowed fields.
    * @throws Exceptions.OsoException
    */
   public HashSet<Object> authorizedFields(
@@ -181,5 +175,10 @@ public class Enforcer {
               }
             })
         .collect(Collectors.toCollection(HashSet::new));
+  }
+
+  public HashSet<Object> authorizedFields(Object actor, Object action, Object resource)
+      throws Exceptions.OsoException {
+    return authorizedFields(actor, action, resource, false);
   }
 }
