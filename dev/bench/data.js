cc40d69409a1d8bf4818fbc0b8d2f525e640660d
github-action-benchmark <github@users.noreply.github.com>
2021-09-03 18:53:26 +0000

    add Rust Benchmark (cargo) benchmark result for e4dad87f295e2865d8c4c4609f06ea292c6549df

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1630693219746,
+  "lastUpdate": 1630695205999,
   "repoUrl": "https://github.com/osohq/oso",
   "entries": {
     "Rust Benchmark": [
@@ -85899,6 +85899,240 @@ window.BENCHMARK_DATA = {
             "name": "partial_rule_depth/100",
             "value": 69666942,
             "range": "± 2291600",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "17556281+gj@users.noreply.github.com",
+            "name": "Gabe Jackson",
+            "username": "gj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e4dad87f295e2865d8c4c4609f06ea292c6549df",
+          "message": "Actor/Resource type registration (#1114)\n\n* Working entity registration\r\n\r\n* Update gitclub submodule\r\n\r\n* Slightly easier-to-read error messages for test failures\r\n\r\n* Rename namespace -> resource block\r\n\r\n* Rename implication -> shorthand rule\r\n\r\n* Rename sugar module -> resource_block\r\n\r\n* Missed a couple granules\r\n\r\n* Split up imports by type\r\n\r\n* Better 'unions'\r\n\r\n* Better checking for if 'Actor' or 'Resource' are actually 'unions'\r\n\r\n* Union types cannot be registered as constants\r\n\r\n* Wow that's ugly\r\n\r\n* Add todo\r\n\r\n* Don't use @polar_class decorator on 'Actor'\r\n\r\n* Factor out KB::check_rule_instance_is_subclass_of_prototype_instance method",
+          "timestamp": "2021-09-03T14:41:12-04:00",
+          "tree_id": "316278b964ef5967b9efdf8ee545bd92e805d1ab",
+          "url": "https://github.com/osohq/oso/commit/e4dad87f295e2865d8c4c4609f06ea292c6549df"
+        },
+        "date": 1630695203467,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "rust_get_attribute",
+            "value": 50575,
+            "range": "± 6964",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "n_plus_one/100",
+            "value": 2349142,
+            "range": "± 5867",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "n_plus_one/500",
+            "value": 11047317,
+            "range": "± 18819",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "n_plus_one/1000",
+            "value": 22017900,
+            "range": "± 61189",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "unify_once",
+            "value": 861,
+            "range": "± 26",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "unify_twice",
+            "value": 2100,
+            "range": "± 36",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "many_rules",
+            "value": 55442,
+            "range": "± 2073",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/5",
+            "value": 499818,
+            "range": "± 8772",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "prime/3",
+            "value": 19384,
+            "range": "± 665",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "prime/23",
+            "value": 19546,
+            "range": "± 640",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "prime/43",
+            "value": 19541,
+            "range": "± 675",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "prime/83",
+            "value": 17323,
+            "range": "± 552",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "prime/255",
+            "value": 17382,
+            "range": "± 681",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "indexed/100",
+            "value": 5477,
+            "range": "± 606",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "indexed/500",
+            "value": 7521,
+            "range": "± 1768",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "indexed/1000",
+            "value": 9059,
+            "range": "± 887",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "indexed/10000",
+            "value": 26750,
+            "range": "± 1343",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "not",
+            "value": 6129,
+            "range": "± 95",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "double_not",
+            "value": 12704,
+            "range": "± 170",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "De_Morgan_not",
+            "value": 7988,
+            "range": "± 133",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "load_policy",
+            "value": 764385,
+            "range": "± 827",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_and/1",
+            "value": 36331,
+            "range": "± 1374",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_and/5",
+            "value": 125267,
+            "range": "± 3663",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_and/10",
+            "value": 236340,
+            "range": "± 5589",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_and/20",
+            "value": 489373,
+            "range": "± 5986",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_and/40",
+            "value": 1063451,
+            "range": "± 9501",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_and/80",
+            "value": 2090049,
+            "range": "± 7851",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_and/100",
+            "value": 3095342,
+            "range": "± 10589",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_rule_depth/1",
+            "value": 112919,
+            "range": "± 3806",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_rule_depth/5",
+            "value": 348519,
+            "range": "± 6310",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_rule_depth/10",
+            "value": 734624,
+            "range": "± 12739",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_rule_depth/20",
+            "value": 1981988,
+            "range": "± 9748",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_rule_depth/40",
+            "value": 6927799,
+            "range": "± 52773",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_rule_depth/80",
+            "value": 39820571,
+            "range": "± 244438",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "partial_rule_depth/100",
+            "value": 71777766,
+            "range": "± 474342",
             "unit": "ns/iter"
           }
         ]
