77c0f5bdf4ce2ae524715dc00dbaa48437ce7e6b
Graham Kaemmer <gkaemmer@users.noreply.github.com>
2021-09-03 09:30:26 -0400

    Export Relationship class from top-level package

--- languages/js/src/index.ts
+++ languages/js/src/index.ts
@@ -1,3 +1,4 @@
 export { Oso } from './Oso';
 export { Variable } from './Variable';
 export { AuthorizationError, ForbiddenError, NotFoundError } from './errors';
+export { Relationship } from './dataFiltering';
