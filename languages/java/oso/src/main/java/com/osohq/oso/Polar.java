fca49616a0134da4a7394c06414921bcfb37d515
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-09-03 18:54:06 -0400

    make -C languages/java fmt

--- languages/java/oso/src/main/java/com/osohq/oso/Polar.java
+++ languages/java/oso/src/main/java/com/osohq/oso/Polar.java
@@ -9,14 +9,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import org.json.JSONArray;
 
 public class Polar {
   private Ffi.Polar ffiPolar;
