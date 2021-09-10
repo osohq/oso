7a35b2f688fd72a4a2509cc8b6f731fbac89747d
Gwen Whelan <gwen@osohq.com>
2021-09-10 16:28:52 -0400

    less futureproof

--- languages/ruby/lib/oso.rb
+++ languages/ruby/lib/oso.rb
@@ -7,7 +7,7 @@ require 'oso/version'
 
 # Top-level namespace for Oso authorization library.
 module Oso
-  def self.new(*args, **kwargs, &blk)
-    ::Oso::Oso.new(*args, **kwargs, &blk)
+  def self.new(not_found_error: NotFoundError, forbidden_error: ForbiddenError, read_action: 'read')
+    ::Oso::Oso.new(not_found_error: not_found_error, forbidden_error: forbidden_error, read_action: read_action)
   end
 end
