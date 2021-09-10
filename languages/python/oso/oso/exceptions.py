252bb1a3581c2e3f1b9427bd7e3ce0760e55cded
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-09-09 22:02:04 -0400

    Flip NotFound & Forbidden error messages

--- languages/python/oso/oso/exceptions.py
+++ languages/python/oso/oso/exceptions.py
@@ -21,9 +21,9 @@ class NotFoundError(AuthorizationError):
 
     def __init__(self):
         super().__init__(
-            "Oso ForbiddenError -- The requested action was not allowed for the "
-            "given resource. You should handle this error by returning a 403 error "
-            "to the client."
+            "Oso NotFoundError -- The current user does not have permission to read "
+            "the given resource. You should handle this error by returning a 404 "
+            "error to the client."
         )
 
 
@@ -38,7 +38,7 @@ class ForbiddenError(AuthorizationError):
 
     def __init__(self):
         super().__init__(
-            "Oso NotFoundError -- The current user does not have permission to read "
-            "the given resource. You should handle this error by returning a 404 "
-            "error to the client."
+            "Oso ForbiddenError -- The requested action was not allowed for the "
+            "given resource. You should handle this error by returning a 403 error "
+            "to the client."
         )
