608fb2a46b11dec2317e1d426f4c6b1281a901d7
David Hatch <david@osohq.com>
2021-09-13 23:20:16 -0400

    Go add to your app.

---
+++ docs/examples/add-to-your-application/go/routes.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
+
+func InitApp() *fiber.App {
+    oso := initOso()
+	app := fiber.New()
+
+
+    // docs: begin-show-route
+	app.Get("/repo/:repoName", func(c *fiber.Ctx) error {
+		c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+		repoName := c.Params("repoName")
+		repository := GetRepositoryByName(repoName)
+		err := oso.Authorize(GetCurrentUser(), "read", repository)
+		if err == nil {
+			return c.Status(200).SendString(fmt.Sprintf("<h1>A Repo</h1><p>Welcome to repo %s</p>", repository.Name))
+		} else {
+			return c.Status(404).SendString("<h1>Whoops!</h1><p>That repo was not found</p>")
+		}
+	})
+    // docs: end-show-route
+
+    return app
+}
+
+func main() {
+    app := InitApp()
+	if err := app.Listen(":5000"); err != nil {
+		log.Fatalf("Failed to start: %s", err)
+	}
+}
