652f3e943ca5802b2f4730dc963fcb22a0b3169c
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-09-20 15:39:16 -0400

    Keep using GTS I guess

--- languages/js/.eslintrc.js
+++ languages/js/.eslintrc.js
@@ -1,11 +1,4 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['./node_modules/gts/'],
 };
