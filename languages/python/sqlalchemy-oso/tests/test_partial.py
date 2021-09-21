09b81f52c2494f537cd4deff61fafde4d9cd3a0f
gw <gwen@osohq.com>
2021-09-21 17:49:58 -0400

    Keep unify constraints on dot lookups (#1209)
    * keep unify constraints on dot lookups
    
    * rm debug log
    
    * remove more debug logging
    
    * Apply suggestions from code review
    
    Co-authored-by: Gabe Jackson <17556281+gj@users.noreply.github.com>
    
    * partial test
    
    * fmt
    
    * lint
    
    Co-authored-by: Gabe Jackson <17556281+gj@users.noreply.github.com>

---
+++ languages/python/sqlalchemy-oso/tests/test_partial.py
@@ -0,0 +1,19 @@
+from polar import Variable
+from sqlalchemy.orm import Session
+from sqlalchemy_oso.partial import partial_to_filter
+from .models import User
+
+
+def test_partial_to_query_filter(oso, engine):
+    oso.load_str('ok(_: User{username:"gwen"});')
+    session = Session(bind=engine)
+    gwen = User(username="gwen")
+    session.add(gwen)
+    steve = User(username="steve")
+    session.add(steve)
+    result = oso.query_rule("ok", Variable("actor"), accept_expression=True)
+
+    partial = next(result)["bindings"]["actor"]
+    filter = partial_to_filter(partial, session, User, oso.get_class)
+    q = list(session.query(User).filter(filter))
+    assert q == [gwen]
