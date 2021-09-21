f96e98e58fa7ad4e08d15c539fd81dcdb70b12a7
Sam Scott <sam@osohq.com>
2021-09-21 12:14:02 -0400

    EVEN MORE FORMATTING?!?

--- languages/js/src/Polar.test.ts
+++ languages/js/src/Polar.test.ts
@@ -947,10 +947,10 @@ describe('Oso Roles', () => {
   });
 
   test('rule types correctly check subclasses', async () => {
-    class Foo { }
-    class Bar extends Foo { }
-    class Baz extends Bar { }
-    class Bad { }
+    class Foo {}
+    class Bar extends Foo {}
+    class Baz extends Bar {}
+    class Bad {}
 
     // NOTE: keep this order of registering classes--confirms that MROs are added at the correct time
     const p = new Polar();
