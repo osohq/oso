682ceaddba7ad50e7cf5ebc18bd9924ed9d1e787
Patrick O'Doherty <p@trickod.com>
2021-10-06 14:37:09 -0700

    simplify error message building for missing actor block

--- polar-core/src/kb.rs
+++ polar-core/src/kb.rs
@@ -265,18 +265,15 @@ impl KnowledgeBase {
                             }
                         }
                         if !success {
+                            let mut err = format!("Rule specializer {} on parameter {} must be a member of rule type specializer {}", rule_instance.tag,index, rule_type_instance.tag);
                             if rule_type_instance.tag == sym!("Actor") {
-                                return Ok(RuleParamMatch::False(format!("Rule specializer {} on parameter {} must be a member of rule type specializer {}
-
+                                err.push_str(&format!("
 \tPerhaps you meant to add an actor block to the top of your policy, like this:
 
-\t  actor {} {{}}",
-                                rule_instance.tag, index, rule_type_instance.tag, rule_instance.tag)));
-
-                            } else {
-                                return Ok(RuleParamMatch::False(format!("Rule specializer {} on parameter {} must be a member of rule type specializer {}", rule_instance.tag,index, rule_type_instance.tag)));
+\t  actor {} {{}}", rule_instance.tag));
                             }
 
+                            return Ok(RuleParamMatch::False(err));
                         }
                     }
                     if !self.param_fields_match(&rule_type_instance.fields, &rule_instance.fields) {
