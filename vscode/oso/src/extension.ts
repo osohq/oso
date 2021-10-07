bf0dfb02d233c02bbe6c6b42a8001badbf2b6ecb
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-10-07 11:50:35 -0400

    More comments

--- vscode/oso/src/extension.ts
+++ vscode/oso/src/extension.ts
@@ -7,10 +7,22 @@ import {
   TextEditor,
 } from 'vscode';
 
+// TODO(gj): think about what it would take to support `load_str()` via
+// https://code.visualstudio.com/api/language-extensions/embedded-languages
+
+// TODO(gj): do we need to maintain state for all (potentially dirty) Polar
+// files in the current workspace?
+
 function updateStatus(status: StatusBarItem, filenames: string[]) {
-  status.text = `[${filenames.map(f => f.split('/').pop()).join(', ')}]`;
+  status.text = `${workspace.name || '(none)'} -> [${filenames
+    .map(f => f.split('/').pop())
+    .join(', ')}]`;
 }
 
+// TODO(gj): maybe just punt on non-workspace use cases entirely for now? At
+// least until progress is made on
+// https://github.com/Microsoft/vscode/issues/15178 so we have a less hacky way
+// to list all open editors (instead of just the currently visible ones).
 function updateStatusFromVisibleEditors(
   status: StatusBarItem,
   es: TextEditor[]
@@ -23,8 +35,13 @@ function updateStatusFromVisibleEditors(
 // TODO(gj): what about when a workspace is open and you open a new file/editor
 // (via command-N) that may or may not ultimately be saved in a folder that
 // exists in the current workspace?
+//
+// NOTE(gj): punting on the above at least until progress is made on
+// https://github.com/Microsoft/vscode/issues/15178 so we have a less hacky way
+// to list all open editors (instead of just the currently visible ones).
 async function updateStatusFromWorkspace(status: StatusBarItem): Promise<void> {
   const polarFiles = await workspace.findFiles('**/*.polar');
+  console.log('polarFiles', polarFiles);
   const filenames = polarFiles.map(f => f.fsPath);
   updateStatus(status, filenames);
 }
@@ -54,6 +71,14 @@ export async function activate(context: ExtensionContext): Promise<void> {
     updateStatusFromVisibleEditors(status, window.visibleTextEditors);
   } else {
     // In a workspace.
+
+    // TODO(gj): need to track files separately per workspace folder; currently
+    // tracking all files across the entire workspace. Use case is opening
+    // osohq/gitclub in one workspace folder and osohq/oso in another.
+
+    // TODO(gj): what happens when someone opens the osohq/oso folder and there
+    // are a ton of different Polar files in different subfolders that should
+    // *not* be considered part of the same policy?
 
     const watcher = workspace.createFileSystemWatcher('**/*.polar');
     context.subscriptions.push(watcher);
