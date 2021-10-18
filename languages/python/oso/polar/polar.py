21e2817d43d8a75ed2a94720ffb0beda66f13d46
gw <gwen@osohq.com>
2021-10-18 15:18:55 -0400

    Data Filtering QA (#1238)
    * isa_with_path test
    
    * fields
    
    * more tests
    
    * more tests
    
    * test reorg
    
    * lint
    
    * orm test parity
    
    * ok enough python tests
    
    * ruby parity
    
    * more parity tests
    
    * space
    
    * lil progress??
    
    * dependent filters
    
    * pred table
    
    * dependent constraints in ruby
    
    * double relation field cmp test
    
    * js parity
    
    * fmt/lint
    
    * cippy
    
    * linted
    
    * Red.result_id is always defined
    
    * list types
    
    * rust polish
    
    * omg mypy
    
    * maybe it has something to do with those type: ignore comments
    
    * fmt
    
    * oops we want to log that
    
    * out of new ideas at this point
    
    * oops new kwarg
    
    * unimpl err test
    
    * nested attr test
    
    * restore added constraint check
    
    * xfails
    
    * xit failing test
    
    * better dot fail error msg
    
    * rm print
    
    * position
    
    * fmt
    
    * clippy
    
    * py fmt
    
    * fmt ... again
    
    Co-authored-by: Stephen Olsen <dev@steve.computer>
    Co-authored-by: David Hatch <david@osohq.com>

--- languages/python/oso/polar/polar.py
+++ languages/python/oso/polar/polar.py
@@ -230,7 +230,7 @@ class Polar:
         cls,
         *,
         name=None,
-        types=None,
+        fields=None,
         build_query=None,
         exec_query=None,
         combine_query=None
@@ -241,7 +241,7 @@ class Polar:
         :param name:
             Optionally specify the name for the class inside of Polar. Defaults
             to `cls.__name__`
-        :param types:
+        :param fields:
             Optional dict mapping field names to types or Relation objects for
             data filtering.
         :param build_query:
@@ -256,7 +256,7 @@ class Polar:
         cls_name = self.host.cache_class(
             cls,
             name=name,
-            fields=types,
+            fields=fields,
             build_query=build_query,
             exec_query=exec_query,
             combine_query=combine_query,
---
+++ languages/python/oso/tests/data_filtering/helpers.py
@@ -0,0 +1,31 @@
+from oso import Oso
+from functools import reduce
+
+
+class DfTestOso(Oso):
+    def check_authz(self, actor, action, resource, expected):
+        assert unord_eq(self.authorized_resources(actor, action, resource), expected)
+        for re in expected:
+            assert self.is_allowed(actor, action, re)
+
+
+def unord_eq(a, b):
+    b = list(b)
+    for x in a:
+        try:
+            b.remove(x)
+        except ValueError:
+            return False
+    return not b
+
+
+def filter_array(array):
+    def go(constraints):
+        check = reduce(
+            lambda f, g: lambda x: f(x) and g(x),
+            [c.check for c in constraints],
+            lambda _: True,
+        )
+        return [x for x in array if check(x)]
+
+    return go
