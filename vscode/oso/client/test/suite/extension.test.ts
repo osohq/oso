0d9060f1eba9a0396ab0c14e2b70df088c4ca460
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-10-20 01:43:16 -0400

    Windows isn't happy with mocha.beforeEach

--- vscode/oso/client/test/suite/extension.test.ts
+++ vscode/oso/client/test/suite/extension.test.ts
@@ -1,6 +1,5 @@
 import * as assert from 'assert';
 
-import { beforeEach } from 'mocha';
 import { Diagnostic, languages, Position, Range, Uri, workspace } from 'vscode';
 
 // TODO(gj): This is temporary and will be replaced when we have real
@@ -18,31 +17,32 @@ function check([uri, diagnostics]: [Uri, Diagnostic[]], expected: string) {
   );
 }
 
-suite('Diagnostics', () => {
-  // Spin until workspace is fully loaded. There might be a race condition
-  // where we call `languages.getDiagnostics()` before the extension has loaded
-  // all of the Polar files and emitted a diagnostic for each.
-  beforeEach(async () => {
-    for (;;) {
-      const uris = await workspace.findFiles('*');
-      switch (uris.length) {
-        case 0:
-        case 1:
-        case 2:
-          console.log(
-            'uris =>',
-            uris.map(u => [...u.toString().split('/')].pop())
-          );
-          continue;
-        case 3:
-          return;
-        default:
-          throw new Error();
-      }
+// Spin until workspace is fully loaded. There might be a race condition
+// where we call `languages.getDiagnostics()` before the extension has loaded
+// all of the Polar files and emitted a diagnostic for each.
+async function waitForWorkspaceToLoad() {
+  for (;;) {
+    const uris = await workspace.findFiles('*');
+    switch (uris.length) {
+      case 0:
+      case 1:
+      case 2:
+        console.log(
+          'uris =>',
+          uris.map(u => [...u.toString().split('/')].pop())
+        );
+        continue;
+      case 3:
+        return;
+      default:
+        throw new Error();
     }
-  });
+  }
+}
 
-  test('We receive a diagnostic for each Polar file in the workspace', () => {
+suite('Diagnostics', () => {
+  test('We receive a diagnostic for each Polar file in the workspace', async () => {
+    await waitForWorkspaceToLoad();
     const diagnostics = languages.getDiagnostics();
     assert.strictEqual(diagnostics.length, 2);
     check(diagnostics[0], 'apple.polar');
