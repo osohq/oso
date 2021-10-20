824f3a0a62a468fbc7ec2dd2d47fb3015157e2c8
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-10-20 11:08:10 -0400

    Add TODO

--- vscode/oso/client/test/suite/extension.test.ts
+++ vscode/oso/client/test/suite/extension.test.ts
@@ -2,6 +2,8 @@ import * as assert from 'assert';
 
 import { Diagnostic, languages, Position, Range, Uri } from 'vscode';
 
+// TODO(gj): This is temporary and will be replaced when we have real
+// diagnostics.
 function check([uri, diagnostics]: [Uri, Diagnostic[]], expected: string) {
   const filename = [...uri.toString().split('/')].pop();
   assert.strictEqual(filename, expected);
