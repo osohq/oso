67e410b75b03e7da36bf9026c9edb871d3361b63
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-10-21 00:15:26 -0400

    Update test

--- vscode/oso/client/test/suite/extension.test.ts
+++ vscode/oso/client/test/suite/extension.test.ts
@@ -1,24 +1,6 @@
 import * as assert from 'assert';
 
-import { Diagnostic, languages, Position, Range, Uri, workspace } from 'vscode';
-
-// TODO(gj): This is temporary and will be replaced when we have real
-// diagnostics.
-function check([uri, diagnostics]: [Uri, Diagnostic[]], expected: string) {
-  const filename = [...uri.toString().split('/')].pop();
-  assert.strictEqual(filename, expected);
-
-  assert.strictEqual(diagnostics.length, 1);
-  assert.strictEqual(diagnostics[0].message.trim(), filename);
-  assert(
-    diagnostics[0].range.isEqual(
-      new Range(
-        new Position(0, 0),
-        new Position(0, diagnostics[0].message.length)
-      )
-    )
-  );
-}
+import { Diagnostic, languages, Uri, workspace } from 'vscode';
 
 // Helper that waits for `n` diagnostics to appear and then returns them.
 async function getDiagnostics(n: number): Promise<[Uri, Diagnostic[]][]> {
@@ -34,9 +16,16 @@ async function getDiagnostics(n: number): Promise<[Uri, Diagnostic[]][]> {
 
 suite('Diagnostics', () => {
   test('We receive a diagnostic for each Polar file in the workspace', async () => {
-    const polarFileCount = (await workspace.findFiles('*.polar')).length;
-    const diagnostics = await getDiagnostics(polarFileCount);
-    check(diagnostics[0], 'apple.polar');
-    check(diagnostics[1], 'banana.polar');
+    const polarFiles = await workspace.findFiles('*.polar');
+    const diagnostics = await getDiagnostics(polarFiles.length);
+    for (const [uri, [diagnostic]] of diagnostics) {
+      const { line, character } = diagnostic.range.start;
+      assert.strictEqual(
+        diagnostic.message,
+        `hit the end of the file unexpectedly. Did you forget a semi-colon at line ${
+          line + 1
+        }, column ${character + 1} in file ${uri.toString()}`
+      );
+    }
   });
 });
