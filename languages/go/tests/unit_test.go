4e4e72b95565c725dd04d7285692ee3b53e66960
Sam Scott <sam@osohq.com>
2021-10-29 09:36:43 -0400

    Update Go polar_types by running `sam/polar-crossgen`.

--- languages/go/tests/unit_test.go
+++ languages/go/tests/unit_test.go
@@ -436,7 +436,7 @@ func TestFailingALot(t *testing.T) {
 
 		if err = <-errors; err != nil {
 			if !strings.Contains(err.Error(), "'1' object has no attribute 'Foo'") {
-				t.Errorf("Expected Polar runtime error, got: {}", err)
+				t.Errorf("Expected Polar runtime error, got: %s", err)
 			}
 		} else {
 			t.Fatal("oops")
