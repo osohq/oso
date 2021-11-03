cb7c06ae0d2e49b7b9df77b75a0b617b9e83bf14
Stephen Olsen <dev@steve.computer>
2021-11-03 13:08:30 -0500

    test python 3.10 (#1350)
    Co-authored-by: Sam Scott <sam@osohq.com>

--- scripts/test_musl.sh
+++ scripts/test_musl.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 apk add build-base libffi-dev
 cd oso
-pip install oso==$OSO_VERSION -f musl-wheel --no-deps --no-index
-pip install oso==$OSO_VERSION -f musl-wheel
+pip install oso==$OSO_VERSION -f wheel --no-deps --no-index
+pip install oso==$OSO_VERSION -f wheel
 cd test
 python test.py
 echo "tests passed"
