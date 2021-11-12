536ad6dbb97aa825f09f85d9f443ebd6e59bb891
Sam Scott <sam@osohq.com>
2021-11-12 18:54:34 -0500

    Use mutex instead of refcell, since something weird is happening.

--- polar-c-api/src/lib.rs
+++ polar-c-api/src/lib.rs
@@ -2,11 +2,11 @@ pub use polar_core::polar::Polar;
 pub use polar_core::query::Query;
 use polar_core::{error, terms};
 
-use std::cell::RefCell;
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::panic::{catch_unwind, AssertUnwindSafe};
 use std::ptr::{null, null_mut};
+use std::sync::{Arc, Mutex};
 
 /// Get a reference to an object from a pointer
 macro_rules! ffi_ref {
@@ -50,18 +50,18 @@ macro_rules! ffi_try {
 }
 
 thread_local! {
-    static LAST_ERROR: RefCell<Option<Box<error::PolarError>>> = RefCell::new(None);
+    static LAST_ERROR: Mutex<Option<Box<error::PolarError>>> = Default::default();
 }
 
 fn set_error(e: error::PolarError) -> i32 {
-    LAST_ERROR.with(|prev| *prev.borrow_mut() = Some(Box::new(e)));
+    LAST_ERROR.with(|prev| *prev.lock().unwrap() = Some(Box::new(e)));
     POLAR_FAILURE
 }
 
 #[no_mangle]
 pub extern "C" fn polar_get_error() -> *const c_char {
     ffi_try!({
-        let err = LAST_ERROR.with(|prev| prev.borrow_mut().take());
+        let err = LAST_ERROR.with(|prev| prev.lock().unwrap().take());
         if let Some(e) = err {
             let error_json = serde_json::to_string(&e).unwrap();
             CString::new(error_json)
