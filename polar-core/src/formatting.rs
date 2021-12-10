0d5ca4bf0205983b714d06595fb259efa724ac90
Patrick O'Doherty <p@trickod.com>
2021-12-09 17:28:23 -0800

    INFO result logs with serialized bindings
    log query each result along with its bindings.
    
    NB: we cannot implement fmt::Display for Bindings as it is a HashMap<Symbol, Term>

--- polar-core/src/formatting.rs
+++ polar-core/src/formatting.rs
@@ -353,6 +353,7 @@ pub mod display {
             }
         }
     }
+
     impl fmt::Display for Declaration {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             match self {
