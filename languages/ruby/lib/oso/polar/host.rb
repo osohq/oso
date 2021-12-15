333232753b16ecc990417c2b01e2ce7fbf85beb1
Patrick O'Doherty <p@trickod.com>
2021-12-14 16:17:34 -0800

    Ruby spec & lint fixes

--- languages/ruby/lib/oso/polar/host.rb
+++ languages/ruby/lib/oso/polar/host.rb
@@ -323,8 +323,15 @@ module Oso
 
                   # only pass class_repr for registered types
                   class_repr = nil
-                  class_repr = value.class.to_s if types.has_key?(value.class)
-                  { 'ExternalInstance' => { 'instance_id' => cache_instance(value, id: instance_id), 'repr' => nil, 'class_repr' => class_repr } }
+                  class_repr = value.class.to_s if types.key?(value.class)
+
+                  {
+                    'ExternalInstance' => {
+                      'instance_id' => cache_instance(value, id: instance_id),
+                      'repr' => nil,
+                      'class_repr' => class_repr
+                    }
+                  }
                 end
         { 'value' => value }
       end
