772fe87bbecc10bbeab1d601b98978c254dbaffd
Gwen Whelan <gwen@osohq.com>
2021-12-17 11:02:56 -0500

    more tests

--- polar-core/src/normalize.rs
+++ polar-core/src/normalize.rs
@@ -16,12 +16,12 @@ impl Term {
                 // as deep as possible
                 And => {
                     assert_eq!(args.len(), 2);
-                    let args = vec![args[0].clone().negated(), args[1].clone().negated()];
+                    let args = vec![args[0].negated(), args[1].negated()];
                     self.clone_with_value(Expression(Operation { operator: Or, args }))
                 }
                 Or => {
                     assert_eq!(args.len(), 2);
-                    let args = vec![args[0].clone().negated(), args[1].clone().negated()];
+                    let args = vec![args[0].negated(), args[1].negated()];
                     self.clone_with_value(Expression(Operation {
                         operator: And,
                         args,
@@ -42,11 +42,11 @@ impl Term {
                     // empty -> boolean
                     0 => self.clone_with_value(Value::Boolean(*operator == And)),
                     // one -> lift it out
-                    1 => args[0].clone().btnf(),
+                    1 => args[0].btnf(),
                     // else fold
                     _ => args
                         .iter()
-                        .map(|a| a.clone().btnf())
+                        .map(|a| a.btnf())
                         .reduce(|m, x| {
                             self.clone_with_value(Value::Expression(Operation {
                                 operator: *operator,
@@ -79,12 +79,15 @@ impl Term {
         }
     }
 
-    fn lhs(&self) -> &Self {
-        &self.value().as_expression().unwrap().args[0]
+    fn _hs(&self, n: usize) -> Self {
+        self.value().as_expression().unwrap().args[n].clone()
     }
 
-    fn rhs(&self) -> &Self {
-        &self.value().as_expression().unwrap().args[1]
+    fn lhs(&self) -> Self {
+        self._hs(0)
+    }
+    fn rhs(&self) -> Self {
+        self._hs(1)
     }
 
     /// normalize an expression by fully distributing logical connectives. the
@@ -100,26 +103,29 @@ impl Term {
         c2: fn(Term, Term) -> Term,
     ) -> Self {
         use Value::*;
+
         match self.value().as_expression() {
             Err(_) => self.clone(),
             Ok(Operation { operator, args }) => {
-                let operator = *operator;
                 let args: Vec<_> = args.iter().map(|t| t.distribute(p1, c1, p2, c2)).collect();
 
                 if p1(self) && p2(&args[0]) {
                     c2(
-                        c1(args[0].lhs().clone(), args[1].clone()),
-                        c1(args[0].rhs().clone(), args[1].clone()),
+                        c1(args[0].lhs(), args[1].clone()),
+                        c1(args[0].rhs(), args[1].clone()),
                     )
                     .distribute(p1, c1, p2, c2)
                 } else if p1(self) && p2(&args[1]) {
                     c2(
-                        c1(args[0].clone(), args[1].lhs().clone()),
-                        c1(args[0].clone(), args[1].rhs().clone()),
+                        c1(args[0].clone(), args[1].lhs()),
+                        c1(args[0].clone(), args[1].rhs()),
                     )
                     .distribute(p1, c1, p2, c2)
                 } else {
-                    self.clone_with_value(Expression(Operation { operator, args }))
+                    self.clone_with_value(Expression(Operation {
+                        operator: *operator,
+                        args,
+                    }))
                 }
             }
         }
@@ -173,54 +179,141 @@ fn orp(l: &Term) -> bool {
 mod test {
     use super::*;
 
-    #[test]
-    fn test_nnf() {
-        let ex = or_(
+    fn ex1() -> Term {
+        or_(
             not_(var!("p")),
             and_(var!("q"), not_(and_(not_(var!("r")), var!("s")))),
-        );
+        )
+    }
 
-        assert_eq!(ex.nnf(), ex.nnf().nnf());
+    fn ex2() -> Term {
+        or_(
+            and_(var!("q"), var!("r")),
+            and_(not_(var!("q")), or_(var!("a"), var!("b"))),
+        )
+    }
 
+    fn ex3() -> Term {
+        and_(
+            or_(var!("a"), var!("b")),
+            and_(
+                or_(var!("c"), var!("d")),
+                and_(or_(var!("e"), var!("f")), or_(var!("g"), var!("h"))),
+            ),
+        )
+    }
+
+    #[test]
+    fn test_nnf() {
+        let ex = ex1();
         let nnf = or_(
             not_(var!("p")),
             and_(var!("q"), or_(var!("r"), not_(var!("s")))),
         );
 
+        assert_eq!(ex.nnf(), ex.nnf().nnf()); // should be idempotent
         assert_eq!(nnf, ex.nnf());
     }
 
     #[test]
-    fn test_dnf() {
-        let ex = or_(
-            not_(var!("p")),
-            and_(var!("q"), not_(and_(not_(var!("r")), var!("s")))),
-        );
-
-        assert_eq!(ex.dnf(), ex.dnf().dnf());
-
-        let dnf = or_(
-            not_(var!("p")),
-            or_(and_(var!("q"), var!("r")), and_(var!("q"), not_(var!("s")))),
-        );
-
-        assert_eq!(dnf, ex.dnf());
-    }
-
-    #[test]
     fn test_cnf() {
-        let ex = or_(
-            not_(var!("p")),
-            and_(var!("q"), not_(and_(not_(var!("r")), var!("s")))),
-        );
-
-        assert_eq!(ex.cnf(), ex.cnf().cnf());
-
+        let ex = ex1();
         let cnf = and_(
             or_(not_(var!("p")), var!("q")),
             or_(not_(var!("p")), or_(var!("r"), not_(var!("s")))),
         );
 
-        assert_eq!(cnf.to_polar(), ex.cnf().to_polar());
+        assert_eq!(ex.cnf(), ex.cnf().cnf());
+        assert_eq!(cnf, ex.cnf());
+
+        let ex = ex2();
+        let cnf = and_(
+            and_(
+                or_(var!("q"), not_(var!("q"))),
+                or_(var!("q"), or_(var!("a"), var!("b"))),
+            ),
+            and_(
+                or_(var!("r"), not_(var!("q"))),
+                or_(var!("r"), or_(var!("a"), var!("b"))),
+            ),
+        );
+
+        assert_eq!(ex.cnf(), ex.cnf().cnf());
+        assert_eq!(cnf, ex.cnf());
+    }
+
+    #[test]
+    fn test_dnf() {
+        let ex = ex1();
+        let dnf = or_(
+            not_(var!("p")),
+            or_(and_(var!("q"), var!("r")), and_(var!("q"), not_(var!("s")))),
+        );
+
+        assert_eq!(ex.dnf(), ex.dnf().dnf());
+        assert_eq!(dnf, ex.dnf());
+
+        let ex = ex2();
+        let dnf = or_(
+            and_(var!("q"), var!("r")),
+            or_(
+                and_(not_(var!("q")), var!("a")),
+                and_(not_(var!("q")), var!("b")),
+            ),
+        );
+
+        assert_eq!(ex.dnf(), ex.dnf().dnf());
+        assert_eq!(dnf, ex.dnf());
+
+        let ex = ex3();
+        let dnf = or_(
+            or_(
+                or_(
+                    or_(
+                        and_(var!("a"), and_(var!("c"), and_(var!("e"), var!("g")))),
+                        and_(var!("a"), and_(var!("c"), and_(var!("e"), var!("h")))),
+                    ),
+                    or_(
+                        and_(var!("a"), and_(var!("c"), and_(var!("f"), var!("g")))),
+                        and_(var!("a"), and_(var!("c"), and_(var!("f"), var!("h")))),
+                    ),
+                ),
+                or_(
+                    or_(
+                        and_(var!("a"), and_(var!("d"), and_(var!("e"), var!("g")))),
+                        and_(var!("a"), and_(var!("d"), and_(var!("e"), var!("h")))),
+                    ),
+                    or_(
+                        and_(var!("a"), and_(var!("d"), and_(var!("f"), var!("g")))),
+                        and_(var!("a"), and_(var!("d"), and_(var!("f"), var!("h")))),
+                    ),
+                ),
+            ),
+            or_(
+                or_(
+                    or_(
+                        and_(var!("b"), and_(var!("c"), and_(var!("e"), var!("g")))),
+                        and_(var!("b"), and_(var!("c"), and_(var!("e"), var!("h")))),
+                    ),
+                    or_(
+                        and_(var!("b"), and_(var!("c"), and_(var!("f"), var!("g")))),
+                        and_(var!("b"), and_(var!("c"), and_(var!("f"), var!("h")))),
+                    ),
+                ),
+                or_(
+                    or_(
+                        and_(var!("b"), and_(var!("d"), and_(var!("e"), var!("g")))),
+                        and_(var!("b"), and_(var!("d"), and_(var!("e"), var!("h")))),
+                    ),
+                    or_(
+                        and_(var!("b"), and_(var!("d"), and_(var!("f"), var!("g")))),
+                        and_(var!("b"), and_(var!("d"), and_(var!("f"), var!("h")))),
+                    ),
+                ),
+            ),
+        );
+
+        assert_eq!(ex.dnf(), ex.dnf().dnf());
+        assert_eq!(dnf, ex.dnf());
     }
 }
