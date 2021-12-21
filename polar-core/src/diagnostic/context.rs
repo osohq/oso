87b13ab313a68df095f6e1bef5ddb6aa176609f1
Patrick O'Doherty <p@trickod.com>
2021-12-21 10:33:53 -0800

    Beginning of discrete log levels in VM (#1437)
    * Beginning of discrete LogLevels in VM
    
    * cut new `LogLevel` enum which will replace the `self.log` bool used to denote
    logging output level
    * extend `log_with` with a log level argument denoting the priority of the msg
    * add INFO output with development related feedback (query status, applicable rules, warn if no matches)

--- polar-core/src/diagnostic/context.rs
+++ polar-core/src/diagnostic/context.rs
@@ -46,14 +46,23 @@ fn pos_to_loc(src: &str, row: usize, column: usize) -> usize {
 
 impl fmt::Display for Context {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.source_file_and_line())?;
         let Position { row, column } = self.range.start;
-        write!(f, " at line {}, column {}", row + 1, column + 1)?;
-        if let Some(ref filename) = self.source.filename {
-            write!(f, " of file {}", filename)?;
-        }
         let loc = pos_to_loc(&self.source.src, row, column);
         let lines = source_lines(&self.source, loc, 0).replace('\n', "\n\t");
         writeln!(f, ":\n\t{}", lines)?;
         Ok(())
+    }
+}
+
+impl Context {
+    pub(crate) fn source_file_and_line(&self) -> String {
+        let mut f = String::new();
+        let Position { row, column } = self.range.start;
+        f += &format!(" at line {}, column {}", row + 1, column + 1);
+        if let Some(ref filename) = self.source.filename {
+            f += &format!(" of file {}", filename);
+        }
+        f
     }
 }
