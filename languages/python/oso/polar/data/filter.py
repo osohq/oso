72612fa23e25b5eee853872ad6a45126199a36fa
Gwen Whelan <gwen@osohq.com>
2021-12-21 15:41:05 -0500

    docstrings

--- languages/python/oso/polar/data/filter.py
+++ languages/python/oso/polar/data/filter.py
@@ -1,4 +1,6 @@
 class DataFilter:
+    """An object representing an abstract query over a particular data type"""
+
     def __init__(self, model, relations, conditions, types):
         self.model = model
         self.relations = relations
@@ -20,12 +22,20 @@ class DataFilter:
 
 
 class Projection:
+    """
+    An object representing a named property (`field`) of a particular data type (`source`).
+    `field` may be `None`, which user code must translate to a field (usually the primary key
+    column in a database) that uniquely identifies the record.
+    """
+
     def __init__(self, source, field):
         self.source = source
         self.field = field
 
 
 class Relation:
+    """An object representing a named relation between two data types"""
+
     def __init__(self, left, name, right):
         self.left = left
         self.name = name
@@ -38,6 +48,14 @@ class Relation:
 
 
 class Condition:
+    """
+    An object representing a WHERE condition on a query.
+
+    `cmp` is an equality or inequality operator.
+
+    `left` and `right` may be Projections or literal data.
+    """
+
     def __init__(self, left, cmp, right):
         self.left = left
         self.cmp = cmp
