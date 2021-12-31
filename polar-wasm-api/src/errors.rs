94e52b5a1a79c8a2f4cfa3400eb231c2f4daaf08
Gabe Jackson <17556281+gj@users.noreply.github.com>
2021-12-31 15:00:55 -0500

    Update polar-wasm-api Error type

--- polar-wasm-api/src/errors.rs
+++ polar-wasm-api/src/errors.rs
@@ -2,28 +2,22 @@ use wasm_bindgen::JsValue;
 
 use polar_core::error::{FormattedPolarError, OperationalError, PolarError};
 
-pub struct Error {
-    pub kind: String,
-    inner: FormattedPolarError,
-}
+pub(crate) struct Error(FormattedPolarError);
 
-pub fn serialization_error(msg: String) -> JsValue {
-    Error::from(PolarError::from(OperationalError::Serialization { msg })).into()
+pub(crate) fn serialization_error(msg: String) -> JsValue {
+    Error(PolarError::from(OperationalError::Serialization { msg }).into()).into()
 }
 
 impl From<PolarError> for Error {
-    fn from(err: PolarError) -> Self {
-        Self {
-            kind: err.kind(),
-            inner: err.into(),
-        }
+    fn from(other: PolarError) -> Self {
+        Self(other.into())
     }
 }
 
 impl From<Error> for js_sys::Error {
     fn from(err: Error) -> Self {
-        let e = Self::new(&err.inner.formatted);
-        e.set_name(&err.kind);
+        let e = Self::new(&err.0.message);
+        e.set_name(&err.0.kind);
         e
     }
 }
