9212971470daed11310d7e7aed7d9469954bc856
Sam Scott <sam@osohq.com>
2022-04-28 10:06:30 -0500

    Keep using the `go mod tidy` hammer.

--- docs/examples/quickstart/test_quickstarts.rb
+++ docs/examples/quickstart/test_quickstarts.rb
@@ -9,25 +9,23 @@ require 'bundler'
 CURL_ERROR = "curl: (7) Failed to connect to localhost port 5000: Connection refused\n"
 CURL_EMPTY = "curl: (52) Empty reply from server\n"
 
-
 quickstarts = {
-  go: { setup: 'go build', server: './oso-go-quickstart' },
+  go: { setup: 'go mod tidy && go build', server: './oso-go-quickstart' },
   # TODO: add local_setup that tests against local Oso install for java
   java: { setup: 'make install', server: 'make run' },
   # TODO: add local_setup that tests against local Oso install for nodejs
   nodejs: { setup: 'npm i', server: 'npm run dev' },
   python: { setup: 'pip install --upgrade -r requirements.txt', server: 'FLASK_APP=app.server python -m flask run' },
-  ruby: { setup: 'bundle', server: 'bundle exec ruby server.rb' },
+  ruby: { setup: 'bundle', server: 'bundle exec ruby server.rb' }
   # rust: { setup: 'cargo build', server: 'cargo run' }
 }
 
-if ARGV.include? "local"
-  quickstarts[:go][:setup] = "make -C ../../../../languages/go copy_lib && go mod edit -replace github.com/osohq/go-oso=../../../../languages/go && go build"
-  quickstarts[:python][:setup] = "pip install -e ../../../../languages/python/oso && pip install --upgrade -r requirements.txt"
-  quickstarts[:ruby][:setup] = "BUNDLE_GEMFILE=../Gemfile-local bundle"
-  quickstarts[:ruby][:server] = "BUNDLE_GEMFILE=../Gemfile-local bundle exec ruby server.rb"
+if ARGV.include? 'local'
+  quickstarts[:go][:setup] = 'make -C ../../../../languages/go copy_lib && go mod edit -replace github.com/osohq/go-oso=../../../../languages/go && go mod tidy && go build'
+  quickstarts[:python][:setup] = 'pip install -e ../../../../languages/python/oso && pip install --upgrade -r requirements.txt'
+  quickstarts[:ruby][:setup] = 'BUNDLE_GEMFILE=../Gemfile-local bundle'
+  quickstarts[:ruby][:server] = 'BUNDLE_GEMFILE=../Gemfile-local bundle exec ruby server.rb'
 end
-
 
 def start_server(server, repo)
   server = spawn server
@@ -68,29 +66,27 @@ quickstarts.each do |lang, qs|
       raise "Setup step failed for #{lang.upcase}:\n#{setup_output}" unless $CHILD_STATUS.exitstatus.zero?
 
       Timeout.timeout 15 do
-        begin
-          ensure_port_5000_is_open
+        ensure_port_5000_is_open
 
-          puts "#{prefix} Starting server..."
-          server, received = start_server qs[:server], "gmail"
-          puts "#{prefix} Checking that /repo/gmail returns a 200..."
-          expected = 'Welcome to repo gmail'
-          unless received.include?(expected)
-            raise "#{lang.upcase} failure\n\texpected: #{expected.inspect}\n\treceived: #{received.inspect}\n"
-          end
-
-          puts "#{prefix} Checking that /repo/react returns a 404..."
-          received = `curl -sS localhost:5000/repo/react 2>&1`
-          expected = 'Repo named react was not found'
-          unless received.include?(expected)
-            raise "#{lang.upcase} failure\n\texpected: #{expected.inspect}\n\treceived: #{received.inspect}\n"
-          end
-
-          puts "#{prefix} Success!"
-        ensure
-          kill_server server unless server.nil?
-          ensure_port_5000_is_open
+        puts "#{prefix} Starting server..."
+        server, received = start_server qs[:server], 'gmail'
+        puts "#{prefix} Checking that /repo/gmail returns a 200..."
+        expected = 'Welcome to repo gmail'
+        unless received.include?(expected)
+          raise "#{lang.upcase} failure\n\texpected: #{expected.inspect}\n\treceived: #{received.inspect}\n"
         end
+
+        puts "#{prefix} Checking that /repo/react returns a 404..."
+        received = `curl -sS localhost:5000/repo/react 2>&1`
+        expected = 'Repo named react was not found'
+        unless received.include?(expected)
+          raise "#{lang.upcase} failure\n\texpected: #{expected.inspect}\n\treceived: #{received.inspect}\n"
+        end
+
+        puts "#{prefix} Success!"
+      ensure
+        kill_server server unless server.nil?
+        ensure_port_5000_is_open
       end
     end
   end
