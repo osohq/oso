0717b003a571fae5f77845d303e5162e97f7976b
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2022-07-19 16:10:32 -0400

    refactor: add explicit exports to oso/polar

--- languages/python/oso/polar/__init__.py
+++ languages/python/oso/polar/__init__.py
@@ -5,3 +5,17 @@ from .predicate import Predicate
 from .expression import Expression, Pattern
 from .data_filtering import Relation
 from .data import DataFilter, Condition, Projection
+
+__all__ = [
+    "Condition",
+    "DataFilter",
+    "Expression",
+    "Pattern",
+    "Polar",
+    "Predicate",
+    "Projection",
+    "Query",
+    "QueryResult",
+    "Relation",
+    "Variable",
+]
--- languages/python/oso/polar/data/__init__.py
+++ languages/python/oso/polar/data/__init__.py
@@ -1,2 +1,10 @@
 from .adapter import DataAdapter
 from .filter import DataFilter, Projection, Relation, Condition
+
+__all__ = [
+    "Condition",
+    "DataAdapter",
+    "DataFilter",
+    "Projection",
+    "Relation",
+]
