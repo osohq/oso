7a868a6be0713fa54ad07790d1d70d5fed5a1ad2
Sam Scott <sam@osohq.com>
2022-10-11 15:33:32 -0500

    Revert changes to repl

--- languages/js/bin/repl.js
+++ languages/js/bin/repl.js
@@ -1,17 +1,5 @@
 #!/usr/bin/env node
 
 const { Oso } = require('../dist/src/Oso'); // eslint-disable-line node/no-missing-require
-const { Dict } = require('../dist/src/types');
 
-const oso = new Oso();
-
-class Foo { }
-
-oso.registerClass(Foo, {
-    isaCheck: (instance) => instance instanceof Object && instance.typeName && instance.typeName == "Foo"
-});
-oso.registerConstant({ typeName: "Foo" }, "foo");
-oso.registerConstant(new Dict({ x: 1 }), "d")
-oso.registerConstant({ x: 1 }, "d2")
-
-oso.repl(process.argv.slice(2));
+new Oso().repl(process.argv.slice(2));
