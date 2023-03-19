76f7c460eada07d7a32d86f582b4dda9f0de7d25
Mike D <5084545+devmonkey22@users.noreply.github.com>
2023-03-19 18:18:02 -0400

    Update django-oso to use automatic AppConfig discovery for Django 3.2+ (#1648)
    Update django-oso to use automatic AppConfig discovery for Django 3.2+ which avoids `RemovedInDjango41Warning: 'django_oso' defines default_app_config = 'django_oso.apps.DjangoOsoConfig'. Django now detects this configuration automatically. You can remove default_app_config.` warning. See https://docs.djangoproject.com/en/3.2/releases/3.2/#automatic-appconfig-discovery
    
    Co-authored-by: Gabe Jackson <17556281+gj@users.noreply.github.com>

--- languages/python/django-oso/django_oso/__init__.py
+++ languages/python/django-oso/django_oso/__init__.py
@@ -1,9 +1,11 @@
+import django
 import oso
 
 from .oso import Oso
 
 __version__ = "0.26.2"
 
-default_app_config = "django_oso.apps.DjangoOsoConfig"
+if django.VERSION < (3, 2):
+    default_app_config = "django_oso.apps.DjangoOsoConfig"
 
 __all__ = ["Oso", "oso"]
